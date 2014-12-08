8a3574bb8444df6b26a9a1644a6d735ac9d099d3
Basil Suter <basil.suter@zephir.ch>
2014-12-08 09:57:03 +0100

    luya basic functions

---
+++ src/Luya.php
@@ -0,0 +1,34 @@
+<?php
+namespace luya;
+
+use yii;
+
+/**
+ * Luya
+ * 
+ * Provides basic functionality to wrap yii functions and make life more easy.
+ */
+final class Luya
+{
+    /**
+     * Get yii parameter
+     * 
+     * @param string $param
+     * @return array
+     */
+    public static function getParams($param)
+    {
+        return (isset(yii::$app->params[$param])) ? yii::$app->params[$param] : [];
+    }
+    
+    /**
+     * Set new yii parameter
+     * 
+     * @param string $param
+     * @param array $value
+     */
+    public static function setParams($param, $value)
+    {
+        yii::$app->params[$param] = $value;
+    }
+}
