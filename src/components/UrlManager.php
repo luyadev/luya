59763b0edd41a160cd9d523f321438eb9b0cc4c4
Basil Suter <basil.suter@zephir.ch>
2014-12-10 14:51:36 +0100

    note

--- src/components/UrlManager.php
+++ src/components/UrlManager.php
@@ -1,6 +1,14 @@
 <?php
 namespace luya\components;
 
+/**
+ * if its not necesary to directly access the modules via /{module} we could
+ * implement this solution:
+ * http://www.yiiframework.com/doc-2.0/guide-runtime-routing.html#adding-rules-dynamically
+ * 
+ * @author nadar
+ *
+ */
 class UrlManager extends \yii\web\UrlManager
 {
     public $enablePrettyUrl = true;
