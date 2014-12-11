388704fda1ad7b142f475f77e4532c48957c852b
Basil Suter <basil.suter@zephir.ch>
2014-12-11 16:32:06 +0100

    added urlPrefixComposition

--- src/Module.php
+++ src/Module.php
@@ -3,5 +3,12 @@ namespace luya;
 
 class Module extends \luya\base\Module
 {
-    
+    /**
+     * Url matching prefix, which is used for all the modules (e.g. an e-store requireds a language
+     * as the cms needs this informations too. After proccessing this informations, they will be removed
+     * from the url for further proccessing.
+     * 
+     * @var string
+     */
+    public $urlPrefixComposition = '<langId:[a-z]{2}>';
 }
