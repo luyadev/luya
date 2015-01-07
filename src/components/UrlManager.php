0a515e1320db27f3195d0a955fe02c6cd9cbd0fc
nadar <basil.suter@zephir.ch>
2015-01-07 14:38:55 +0100

    removed default behavior, removed clearRules functions (use addRules[], true/false)

--- src/components/UrlManager.php
+++ src/components/UrlManager.php
@@ -14,13 +14,4 @@ class UrlManager extends \yii\web\UrlManager
     public $enablePrettyUrl = true;
 
     public $showScriptName = false;
-
-    public $rules = [
-        ['class' => 'luya\components\UrlRule'],
-    ];
-
-    public function clearRules()
-    {
-        $this->rules = [];
-    }
 }
