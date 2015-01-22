7f5f160d35e05bed1a858c2a1095774db55c4fa6
nadar <basil.suter@zephir.ch>
2015-01-22 15:54:42 +0100

    [*] added nav_item_type options method

--- modules/cmsadmin/base/NavItemType.php
+++ modules/cmsadmin/base/NavItemType.php
@@ -6,4 +6,14 @@ abstract class NavItemType extends \yii\db\ActiveRecord
     abstract public function getContent();
 
     abstract public function getHeaders();
+    
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
+    
+    public function getOption($key)
+    {
+        return (isset($this->options[$key])) ? $this->options[$key] : false;
+    }
 }
