607ecad26089e3b06af54fee4eb0f514c96e26b6
nadar <basil.suter@zephir.ch>
2015-01-22 16:46:04 +0100

    [*] changed twig cms path to @app/views/cmslayouts

--- modules/cmsadmin/models/NavItemPage.php
+++ modules/cmsadmin/models/NavItemPage.php
@@ -27,7 +27,7 @@ class NavItemPage extends \cmsadmin\base\NavItemType
 
     public function getContent()
     {
-        $loader = new \Twig_Loader_Filesystem(yii::getAlias('@cms').'/views');
+        $loader = new \Twig_Loader_Filesystem(\yii::getAlias('@app/views/cmslayouts/'));
         $twig = new \Twig_Environment($loader, ['autoescape' => false]);
 
         $insertion = [];
