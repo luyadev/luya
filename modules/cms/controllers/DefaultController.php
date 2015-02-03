aafeb4f491d2e2122abe23ebdb9c7e2231800225
nadar <basil.suter@zephir.ch>
2015-02-03 11:58:38 +0100

    [+] moved page collection stuff into cms controller, not yet cleaned up!

--- modules/cms/controllers/DefaultController.php
+++ modules/cms/controllers/DefaultController.php
@@ -4,27 +4,128 @@ namespace cms\controllers;
 
 use Yii;
 
-class DefaultController extends \luya\base\Controller
+class DefaultController extends \luya\base\PageController
 {
     public $useModuleViewPath = true;
 
-    public function actionIndex()
-    {
-        $langShortCode = Yii::$app->get('collection')->lang->shortCode;
+    public $pageTitle = 'default none!';
 
+    private $_lang = null;
+
+    private $_context = null;
+
+    public function init()
+    {
+        parent::init();
+        $this->links();
+        $this->_context = $this;
+        $this->_lang = yii::$app->collection->lang->shortCode;
+    }
+
+    private function links()
+    {
         $links = new \cms\collection\Links();
         $links->activeLink = $_GET['path'];
-        //$links->setLangId(yii::$app->collection->lang->shortCode);
         $links->start();
+        yii::$app->collection->links = $links;
+    }
 
-        Yii::$app->get('collection')->links = $links;
+    public function actionIndex()
+    {
+        $linksObject = \Yii::$app->collection->links;
 
-        $page = new \cms\collection\Page();
+        $urls = $linksObject->getAll();
 
-        Yii::$app->get('collection')->page = $page;
+        $fullUrl = $linksObject->getActiveLink();
 
-        return $this->render("index", [
-            'page' => $page
+        /* above collection based */
+
+        if (empty($fullUrl)) {
+            echo "FIND HOME! 404!";
+            exit;
+        }
+
+        $parts = explode("/", $fullUrl);
+
+        $parts[] = '__FIRST_REMOVAL'; // @todo remove
+
+        $activeUrl = $this->findActive($urls, $parts);
+
+        if (!$activeUrl) {
+            // URL NOT FOUND! REDIRECT TO HOME
+            echo "<h1>404<h1><h3>Url \"$fullUrl\" not found</h3>";
+            exit;
+        }
+
+        $linkItem = $linksObject->getLink($activeUrl);
+
+        $pageContent = $this->getPageContent($linkItem['id'], [
+            'restString' => substr($fullUrl, strlen($linkItem['url']) + 1)       // negativPath
         ]);
+
+        return $this->render('index', [
+            'pageContent' => $pageContent
+        ]);
+    }
+
+    /* PRIVS */
+
+    private function getPageContent($navId, $options = [])
+    {
+        // @TODO is $linkItem['id'] a unifyed system or does it only matches cause Links is set via cms\collection\Links?
+        $object = \cmsadmin\models\Nav::findOne($navId);
+
+        // @TODO Language ID
+        $navItemId = $object->getItem(1)->id;
+
+        // @TODO LANGUAGE PARAM SHOULD BE APPLYIED HERE!
+        $itemType = \cmsadmin\models\NavItem::findOne($navItemId);
+
+        $this->pageTitle = $itemType->title;
+
+        $item = $itemType->getType();
+
+        $item->setOptions($options);
+
+        $content = $item->getContent();
+
+        $this->setContext($item->getContext());
+
+        return $content;
+    }
+
+    private function setContext($context)
+    {
+        if (empty($context)) {
+            return;
+        }
+
+        foreach ($context as $prop => $value) {
+            $this->$prop = $value;
+        }
+    }
+
+    private function getContext()
+    {
+        return $this->_context;
+    }
+
+    private function findDefaultPage()
+    {
+        $cat = (new \yii\db\Query())->select(['id', 'default_nav_id'])->from("cms_cat")->where(['is_default' => 1])->one();
+
+        return $cat['default_nav_id'];
+    }
+
+    private function findActive($urls, $parts)
+    {
+        while (array_pop($parts)) {
+            $match = implode("/", $parts);
+            if (array_key_exists($match, $urls)) {
+                return $match;
+            }
+        }
+
+        return false;
     }
 }
