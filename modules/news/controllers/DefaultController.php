655b391da6a462cd69403c3b83502194c6759832
nadar <basil.suter@zephir.ch>
2015-02-05 15:09:05 +0100

    [+] added basic news module

---
+++ modules/news/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace news\controllers;
+
+use admin\base\actionIndex;
+
+class DefaultController extends \luya\base\PageController
+{
+    public function actionIndex()
+    {
+        $model = \newsadmin\models\Article::className();
+        
+        return $this->render('index', [
+            'model' => $model        
+        ]);
+    }
+}
