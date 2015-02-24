7166ae060db6a230ad2a97e1368c7ec14f345fb9
nadar <git@nadar.io>
2015-02-24 11:03:06 +0100

    [+] added ng controler split to overwrite a local ng config for a controller (not have to use model ng config)

--- modules/admin/ngrest/base/Controller.php
+++ modules/admin/ngrest/base/Controller.php
@@ -4,17 +4,34 @@ namespace admin\ngrest\base;
 class Controller extends \admin\base\Controller
 {
     public $modelClass = null;
+    
+    private $_model = null;
 
     public function getModelClass()
     {
         return $this->modelClass;
     }
+    
+    public function getModelObject()
+    {
+        if ($this->_model !== null) {
+            return $this->_model;
+        }
+        
+        $class = $this->getModelClass();
+        
+        $this->_model = new $class();
+        return $this->_model;
+    }
+    
+    public function getNgRestConfig()
+    {
+        return $this->getModelObject()->getNgRestConfig();
+    }
 
     public function actionIndex()
     {
-        $class = $this->getModelClass();
-        $model = new $class();
-        $ngrest = new \admin\ngrest\NgRest($model->getNgRestConfig());
+        $ngrest = new \admin\ngrest\NgRest($this->getNgRestConfig());
 
         return $ngrest->render(new \admin\ngrest\render\RenderCrud());
     }
