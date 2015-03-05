1e8a351abf4d1f01c9a22d5c8695fad3e72e3bfd
Martin Petrasch <martin.petrasch@zephir.ch>
2015-03-05 17:07:15 +0100

    added checkbox plugin

---
+++ modules/admin/ngrest/plugins/Checkbox.php
@@ -0,0 +1,36 @@
+<?php
+namespace admin\ngrest\plugins;
+
+use admin\ngrest\PluginAbstract;
+
+class Checkbox extends PluginAbstract
+{
+    public function renderList($doc)
+    {
+        $elmn = $doc->createElement("span", "{{item.".$this->name."}}");
+        $doc->appendChild($elmn);
+
+        return $doc;
+    }
+
+    public function renderCreate($doc)
+    {
+        $elmn = $doc->createElement("input");
+        $elmn->setAttribute("id", $this->id);
+        $elmn->setIdAttribute("id", true);
+        $elmn->setAttribute("ng-model", $this->ngModel);
+        $elmn->setAttribute("ng-true-value", "1");
+        $elmn->setAttribute("ng-false-value", "0");
+        $elmn->setAttribute("value", 1);
+        $elmn->setAttribute("class", "form__input");
+        $elmn->setAttribute("type", "checkbox");
+        $doc->appendChild($elmn);
+
+        return $doc;
+    }
+
+    public function renderUpdate($doc)
+    {
+        return $this->renderCreate($doc);
+    }
+}
