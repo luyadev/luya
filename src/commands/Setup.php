0dcb0004c991618dcac3444ed5e9770262ab224b
nadar <git@nadar.io>
2015-03-23 15:14:12 +0100

    setup command todo/idea

---
+++ src/commands/Setup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace luya\commands;
+
+/**
+ * @todo remove all database entries from migrations scripts
+ * @todo create a setup (first init) proccess to fill basic tables (like rights, blocks, cms) when creating a new system, probably based on executions files???
+ * @author nadar
+ */
+class SetupController extends \yii\console\controllers
+{
+    
+}
