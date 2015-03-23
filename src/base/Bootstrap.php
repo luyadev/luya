836d84ab4da185ca9cecab462da499840a42d5ea
nadar <git@nadar.io>
2015-03-23 12:45:59 +0100

    added exec command, added bootstrap base

---
+++ src/base/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+namespace luya\base;
+
+/**
+ * create basic bootstraping which is allowed for console and web components
+ * @author nadar
+ *
+ */
+class Bootstrap implements \yii\base\BootstrapInterface
+{
+    
+}
