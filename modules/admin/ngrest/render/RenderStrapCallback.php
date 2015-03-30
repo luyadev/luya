dc3e83bf5407b8a1ca216f116acc669b329fb5a8
nadar <git@nadar.io>
2015-03-30 13:51:06 +0200

    fixed render strap callback bug where not required arguments have been treated as required arguments.

--- modules/admin/ngrest/render/RenderStrapCallback.php
+++ modules/admin/ngrest/render/RenderStrapCallback.php
@@ -1,13 +1,14 @@
 <?php
 namespace admin\ngrest\render;
 
+use Yii;
 use admin\ngrest\RenderAbstract;
 use admin\ngrest\RenderInterface;
 
 /**
- * @todo complet rewrite of this class - what is the best practive to acces data in the view? define all functiosn sindie here? re-create methods from config object?
- *  $this->config() $this....
- *
+ * @todo complet rewrite of this class - what is the best practive to acces data in the view? define all functions inside here? re-create methods from config object? $this->config() $this....
+ * @todo change get params to yii2 request->get(..., null);
+ * @todo sanitize post (\yii\helpers\HtmlPurifier::process(...)
  * @author nadar
  */
 class RenderStrapCallback extends RenderAbstract implements RenderInterface
@@ -19,17 +20,19 @@ class RenderStrapCallback extends RenderAbstract implements RenderInterface
 
         $function = "callback".ucfirst($_GET['strapCallback']);
 
-        $args = $_POST; // SANITIZE THIS INPUT ? yii?
+        $args = Yii::$app->request->post();
 
         $reflection = new \ReflectionMethod($obj, $function);
 
         $methodArgs = [];
-        foreach ($reflection->getParameters() as $param) {
-            if (!array_key_exists($param->name, $args)) {
-                throw new \Exception("the provided argument does not exists in the method args list");
+        
+        if ($reflection->getNumberOfRequiredParameters() > 0) {
+            foreach ($reflection->getParameters() as $param) {
+                if (!array_key_exists($param->name, $args)) {
+                    throw new \Exception("the provided argument '".$param->name."' does not exists in the provided arguments list.");
+                }
+                $methodArgs[] = $args[$param->name];
             }
-
-            $methodArgs[] = $args[$param->name];
         }
 
         $response = call_user_func_array(array($obj, $function), $methodArgs);
