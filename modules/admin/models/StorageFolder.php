d4fa6907f98d84400306ccc8d4f4d7e5990ae9c2
nadar <git@nadar.io>
2015-04-13 13:09:11 +0200

    added storage folder model

---
+++ modules/admin/models/StorageFolder.php
@@ -0,0 +1,18 @@
+<?php
+namespace admin\models;
+
+class StorageFolder extends \yii\db\ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'admin_storage_folder';
+    }
+
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['parent_id', 'timestamp_create'], 'safe'],
+        ];
+    }
+}
