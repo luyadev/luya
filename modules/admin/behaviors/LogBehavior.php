c6704225ffcb7385a67f9401d8efee5810187a3f
nadar <git@nadar.io>
2015-04-23 16:10:01 +0200

    fixed session bug when using exec commands

--- modules/admin/behaviors/LogBehavior.php
+++ modules/admin/behaviors/LogBehavior.php
@@ -27,27 +27,31 @@ class LogBehavior extends \yii\base\Behavior
     
     public function eventAfterInsert($event)
     {
-        \yii::$app->db->createCommand()->insert('admin_ngrest_log', [
-            "user_id" => \admin\Module::getAdminUserData()->id,
-            "timestamp_create" => time(),
-            "route" => $this->route,
-            "api" => $this->api,
-            "is_insert" => 1,
-            "is_update" => 0,
-            "attributes_json" => json_encode($event->sender->getAttributes()),    
-        ])->execute();
+        if(\yii::$app instanceof \yii\web\Application) {
+            \yii::$app->db->createCommand()->insert('admin_ngrest_log', [
+                "user_id" => \admin\Module::getAdminUserData()->id,
+                "timestamp_create" => time(),
+                "route" => $this->route,
+                "api" => $this->api,
+                "is_insert" => 1,
+                "is_update" => 0,
+                "attributes_json" => json_encode($event->sender->getAttributes()),    
+            ])->execute();
+        }
     }
     
     public function eventAfterUpdate($event)
     {
-        \yii::$app->db->createCommand()->insert('admin_ngrest_log', [
-            "user_id" => 1,
-            "timestamp_create" => time(),
-            "route" => $this->route,
-            "api" => $this->api,
-            "is_insert" => 0,
-            "is_update" => 1,
-            "attributes_json" => json_encode($event->sender->getAttributes()),
-        ])->execute();
+        if(\yii::$app instanceof \yii\web\Application) {
+            \yii::$app->db->createCommand()->insert('admin_ngrest_log', [
+                "user_id" => 1,
+                "timestamp_create" => time(),
+                "route" => $this->route,
+                "api" => $this->api,
+                "is_insert" => 0,
+                "is_update" => 1,
+                "attributes_json" => json_encode($event->sender->getAttributes()),
+            ])->execute();
+        }
     }
 }
