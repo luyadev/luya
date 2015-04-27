bd606c9dbbfff1c38fd2a23923b398105664527b
nadar <git@nadar.io>
2015-04-27 13:10:27 +0200

    created example admin filter

---
+++ modules/admin/filters/SmallThumbnail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace admin\filters;
+
+class SmallThumbnail extends \admin\base\Filter
+{    
+    public function identifier()
+    {
+        return 'small-thumbnail';
+    }
+    
+    public function name()
+    {
+        return 'kleines Thumbnail';
+    }
+    
+    public function chain()
+    {
+        return [
+            [self::EFFECT_THUMBNAIL, [
+                'width' => 100,
+                'height' => 100,
+            ]],
+        ];
+    }
+}
