5193c1fc7b3eae8d6d59e22215a36111a003578a
nadar <git@nadar.io>
2015-05-04 15:41:06 +0200

    register gallery strap, added album_image ref table

---
+++ modules/galleryadmin/migrations/m150504_132138_gallery_album_image.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150504_132138_gallery_album_image extends Migration
+{
+    public function up()
+    {
+        $this->createTable("gallery_album_image", [
+            "image_id" => "int(11) NOT NULL default 0",
+            "album_id" => "int(11) NOT NULL default 0",
+        ]);
+    }
+
+    public function down()
+    {
+        echo "m150504_132138_gallery_album_image cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
