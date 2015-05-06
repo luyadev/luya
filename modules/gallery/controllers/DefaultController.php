7e5a833633f95533dd37070bdc9ade8a370d41bc
nadar <git@nadar.io>
2015-05-06 15:57:25 +0200

    added page title and meta description

--- modules/gallery/controllers/DefaultController.php
+++ modules/gallery/controllers/DefaultController.php
@@ -13,8 +13,17 @@ class DefaultController extends \luya\base\PageController
     
     public function actionDetail($id)
     {
+        $model = \galleryadmin\models\Album::find()->where(['id' => $id])->one();
+        
+        $this->view->registerMetaTag([
+            'name' => 'description',
+            'content' => $model->description
+        ]);
+        
+        $this->pageTitle = $model->title;
+        
         return $this->render('detail', [
-            'model' => \galleryadmin\models\Album::find()->where(['id' => $id])->one(), 
+            'model' => $model, 
         ]);
     }
 }
