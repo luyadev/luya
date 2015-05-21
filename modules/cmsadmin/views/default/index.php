130f36061faee246231292387b16228054c7bbe7
nadar <git@nadar.io>
2015-05-21 11:12:41 +0200

    changed create page form to materializecss

--- modules/cmsadmin/views/default/index.php
+++ modules/cmsadmin/views/default/index.php
@@ -8,56 +8,46 @@
 </script>
 
 <script type="text/ng-template" id="createform.html">
-<div ng-switch on="data.nav_item_type">
-<div style="padding:20px; background-color:#F0F0F0; margin:20px;">
-<table>
-    <tr>
-        <td>Titel</td>
-        <td><input type="text" ng-model="data.title" placeholder="Hallo Welt" /></td>
-    </tr>
-    <tr>
-        <td>Rewrite</td>
-        <td><input type="text" ng-model="data.rewrite" placeholder="hallo-welt" /></td>
-    </tr>
-    <tr>
-        <td>Cat Id</td>
-        <td>
-            <div ng-if="!data.nav_id">
-                <select ng-model="data.cat_id" ng-options="item.id as item.name for item in cat" />
-            </div>
-            <div ng-if="data.nav_id">
-                create page from nav_id: {{data.nav_id}}
-            </div>
-        </td>
-    </tr>
-    <!-- people should not create pages for other languages as long as there is not a page in the cms original menu
-    <tr>
-        <td>Lang Id</td>
-        <td>
-            <select ng-model="data.lang_id" ng-options="item.id as item.name for item in lang" />
-        </td>
-    </tr>
-    -->
-    <tr>
-        <td>Parent Nav Id</td>
-        <td>
-            <div ng-if="!data.nav_id">
-                <select ng-model="data.parent_nav_id">
-                    <option value="0">[ROOT]</option>
-                    <option ng-repeat="nav in navitems" value="{{nav.id}}">{{nav.title}}</option>
-                </select>
-            </div>
-        </td>
-    </tr>
-    <tr>
-        <td>Content Nav-Item-Type Id</td>
-        <td>
-            <input type="radio" ng-model="data.nav_item_type" value="1"> Page<br />
-            <input type="radio" ng-model="data.nav_item_type" value="2"> Module<br />
-            <input type="radio" ng-model="data.nav_item_type" value="3"> Redirect<br />
-        </td>
-    </tr>
-</table>
+<div ng-switch on="data.nav_item_type" class="card-panel">
+<h5>Neue Seite hinzufügen</h5>
+<div class="row">
+    <div class="input-field col s6">
+        <input type="text" ng-model="data.title" />
+        <label>Seiten Titel</label>
+    </div>
+    <div class="input-field col s6">
+        <input type="text" ng-model="data.rewrite" />
+        <label>Rewrite</label>
+    </div>
+</div>
+<div class="row" ng-show="!data.nav_id">
+    <div class="col s12">
+        <label>Kategorrie</label>
+        <select class="browser-default" ng-model="data.cat_id" ng-options="item.id as item.name for item in cat" />
+    </div>
+</div>
+<div class="row">
+    <div class="col s12">
+        <label>Sprache</label>        
+        <select class="browser-default" ng-model="data.lang_id" ng-options="item.id as item.name for item in lang" />
+    </div>
+</div>
+<div class="row">
+    <div class="col s12">
+        <label>Navigations Punkt von</label>
+        <select class="browser-default" ng-model="data.parent_nav_id">
+            <option value="0">[Oberstes Level]</option>
+            <option ng-repeat="nav in navitems" value="{{nav.id}}">{{nav.title}}</option>
+        </select>
+        
+    </div>
+</div>
+<div class="row">
+    <div class="col s12">
+        <p><input type="radio" ng-model="data.nav_item_type" value="1" id="t1"><label for="t1">Seite</label></p>
+        <p><input type="radio" ng-model="data.nav_item_type" value="2" id="t2"><label for="t2">Module</label></p>
+        <p><input type="radio" ng-model="data.nav_item_type" value="3" id="t3"><label for="t3">Weiterleitung</label></p>
+    </div>
 </div>
 <div ng-switch-when="1">
     <create-form-page data="data"></create-form-page>
@@ -76,36 +66,39 @@
 </div>
 
 </div>
-
 </script>
 
 <script type="text/ng-template" id="createformpage.html">
-<div style="padding:20px; border:2px solid #F0F0F0; margin:20px;">
-<table>
-<tr>
-    <td>Layout</td>
-    <td><select ng-model="data.layout_id" ng-options="lts.id as lts.name for lts in layouts"></select></td>
-</tr>
-</table>
+<div class="row">
+    <div class="col s12">
+        <label>Layout</label>
+        <select class="browser-default" ng-model="data.layout_id" ng-options="lts.id as lts.name for lts in layouts"></select>
+    </div>
 </div>
-<button ng-click="save()">SAVE</button>
+<div class="row">
+    <div class="col s12">
+        <button type="button" class="btn" ng-click="save()">Neue Seite Speichern</button>
+    </div>
+</div>
 </script>
 
 <script type="text/ng-template" id="createformmodule.html">
-<div style="padding:20px; border:2px solid #CCC; margin:20px;">
-<table>
-<tr>
-    <td>Module Name (Yii2-ID)</td>
-    <td><input type="text" ng-model="data.module_name" /></td>
-</tr>
-</table>
+<div class="row">
+    <div class="col s12 input-field">
+        <input type="text" ng-model="data.module_name" />
+        <label>Module Name (Yii-ID)</label>
+    </div>
 </div>
-<button ng-click="save()">SAVE</button>
+<div class="row">
+    <div class="col s12">
+        <button type="button" class="btn" ng-click="save()">Neue Seite Speichern</button>
+    </div>
+</div>
 </script>
+
 <a class="btn-floating btn-large waves-effect waves-light red" style="position:absolute; margin-left:334px; margin-top:20px;" ui-sref="custom.cmsadd"><i class="mdi-content-add"></i></a>
 <div class="row">
       <div class="col s12 m4 l2">
-            
             <div ng-controller="CmsMenuTreeController">
                 <div ng-repeat="catitem in menu" class="card-panel blue lighten-5">
                     <h5 style="text-transform: uppercase">{{catitem.name}}</h5>
