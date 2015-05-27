97a734ea3e64f4003d8755ac3510cce108f6f6a2
Marc Stampfli <marc.stampfli@zephir.ch>
2015-05-27 09:28:38 +0200

    Moved treeview styles to cmsadmin assets;

--- modules/admin/views/default/dashboard.php
+++ modules/admin/views/default/dashboard.php
@@ -1,4 +1,8 @@
 <div style="padding:50px; margin:50px; text-align:center; background-color:#F0F0F0;">
-	<h1>Klicken Sie oben auf die Icons um im Luya-Admin zu navigieren.</h1>
+	<h1>Herzlich Willkommen</h1>
+    <p>
+        Nutzen Sie die Hauptnavigation um zwischen Modulen zu wechseln.<br />
+        Mithilfe der Seitennavigation können Sie sich die Kategorien des Moduls ansehen.
+    </p>
     <p>http://luya.io</p>
 </div>
