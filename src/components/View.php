10a74f61062ad5276767e4b8626bac53d3b2a7db
nadar <git@nadar.io>
2015-06-04 16:23:37 +0200

    added view helper method url().

--- src/components/View.php
+++ src/components/View.php
@@ -3,16 +3,30 @@
 namespace luya\components;
 
 use Yii;
+use luya\helpers\Url;
 
 class View extends \yii\web\View
 {
     /**
      * @todo verify there is already a yii-way solution
      *
-     * @param unknown $assetName
+     * @param string $assetName
+     * @return string
      */
     public function getAssetUrl($assetName)
     {
         return $this->assetBundles[$assetName]->baseUrl;
+    }
+    
+    /**
+     * Helper method for convenience
+     * 
+     * @param string $route
+     * @param array $params
+     * @return string
+     */
+    public function url($route, array $params = [])
+    {
+        return Url::to($route, $params);
     }
 }
