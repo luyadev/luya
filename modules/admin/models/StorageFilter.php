aa3eef43ef42081b8762fd7e7d6ecc0dd8c444f0
nadar <git@nadar.io>
2015-06-04 13:21:05 +0200

    removed storage filter ActiveWindow

--- modules/admin/models/StorageFilter.php
+++ modules/admin/models/StorageFilter.php
@@ -85,7 +85,7 @@ class StorageFilter extends \admin\ngrest\base\Model
 
     public function ngRestConfig($config)
     {
-        $config->activeWindow->register(new \admin\aws\FilterEffectChain(), 'Chain');
+        //$config->aw->register(new \admin\aws\FilterEffectChain(), 'Chain');
 
         $config->list->field('name', 'Name')->text()->required();
         $config->list->field('identifier', 'Identifier')->text()->required();
