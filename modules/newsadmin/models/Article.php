640844e826074e6eaa5a7934eec34a27bbf63035
nadar <git@nadar.io>
2015-06-11 11:09:10 +0200

    fixed: Unable to remove field 'id' from 'update' config. The field does not exists.

--- modules/newsadmin/models/Article.php
+++ modules/newsadmin/models/Article.php
@@ -78,7 +78,7 @@ class Article extends \admin\ngrest\base\Model
         $config->update->field('file_list', 'Datei Liste')->fileArray();
         $config->update->extraField('tags', 'Tags')->checkboxRelation(\newsadmin\models\Tag::className(), 'news_article_tag', 'article_id', 'tag_id');
         
-        $config->create->copyFrom('update', ['id']);
+        $config->create->copyFrom('update');
 
         return $config;
     }
