b475d593a4a5a6d165e05bfb59bb26e436cc82b4
nadar <git@nadar.io>
2015-06-11 10:48:22 +0200

    ngrest crud config update

--- modules/newsadmin/models/Article.php
+++ modules/newsadmin/models/Article.php
@@ -36,6 +36,7 @@ class Article extends \admin\ngrest\base\Model
         $this->update_user_id = \admin\Module::getAdminUserData()->id;
         $this->timestamp_create = time();
         $this->timestamp_update = time();
+        $this->timestamp_display_from = time();
     }
 
     public function getDetailUrl($contextNavItemId = null)
@@ -64,20 +65,20 @@ class Article extends \admin\ngrest\base\Model
     {
         $config->list->field('cat_id', 'Kategorie')->selectClass('\newsadmin\models\Cat', 'id', 'title');
         $config->list->field('title', 'Titel')->text()->required();
-        $config->list->field('timestamp_display_from', 'News anzeigen ab')->date();
-        $config->list->field('timestamp_display_until', 'News anzeigen bis')->date();
+        //$config->list->field('timestamp_display_from', 'News anzeigen ab')->date();
+       // $config->list->field('timestamp_display_until', 'News anzeigen bis')->date();
         
         $config->update->field('cat_id', 'Kategorie')->selectClass('\newsadmin\models\Cat', 'id', 'title');
         $config->update->field('title', 'Titel')->text()->required();
         $config->update->field('text', 'Text')->textarea()->required();
-        $config->update->field('timestamp_display_from', 'News anzeigen ab')->date();
-        $config->update->field('timestamp_display_until', 'News anzeigen bis')->date();
+        //$config->update->field('timestamp_display_from', 'News anzeigen ab')->date();
+        //$config->update->field('timestamp_display_until', 'News anzeigen bis')->date();
         $config->update->field('image_id', 'Bild')->image()->required();
         $config->update->field('image_list', 'Bild Liste')->imageArray();
         $config->update->field('file_list', 'Datei Liste')->fileArray();
         $config->update->extraField('tags', 'Tags')->checkboxRelation(\newsadmin\models\Tag::className(), 'news_article_tag', 'article_id', 'tag_id');
-
-        $config->create->copyFrom('list', ['id']);
+        
+        $config->create->copyFrom('update', ['id']);
 
         return $config;
     }
