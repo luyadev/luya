dac401afb516980757b47593f137b19e94509438
nadar <git@nadar.io>
2015-06-11 10:35:48 +0200

    added catData to alben controller view

--- modules/gallery/controllers/AlbenController.php
+++ modules/gallery/controllers/AlbenController.php
@@ -2,12 +2,16 @@
 
 namespace gallery\controllers;
 
+use \galleryadmin\models\Album;
+use \galleryadmin\models\Cat;
+
 class AlbenController extends \luya\base\PageController
 {
     public function actionIndex($catId)
     {
         return $this->render('index', [
-            'albenData' => \galleryadmin\models\Album::find()->where(['cat_id' => $catId])->all(),
+            'catData' => Cat::findOne($catId),
+            'albenData' => Album::find()->where(['cat_id' => $catId])->all(),
         ]);
     }
 }
