f2e81e8a7c214e9832d0237d089516d363127322
nadar <git@nadar.io>
2015-06-15 11:04:33 +0200

    changed dashboard log layout, limited last data to 20 items.

--- modules/admin/views/template/default.php
+++ modules/admin/views/template/default.php
@@ -13,16 +13,21 @@
     <div class="luya-container__main">
         <div class="row">
             <div class="col s12" ui-view>
-                <h1>Dashboard</h1>
-                <div ng-repeat="item in dashboard">
-                    <div class="card-panel teal">
-                        <h5>{{ item.menu.alias }}</h5>
-                        <p ng-repeat="log in item.data">{{ log.firstname }} {{ log.lastname}} hat einen Datensatz
-                            <strong ng-if="log.is_update == 1">bearbeitet</strong>
-                            <strong ng-if="log.is_insert == 1">hinzugefügt</strong>
-                            am {{ log.timestamp_create * 1000 | date:"dd.MM.yyyy ' um ' HH:mm" }} Uhr.
-                        </p>
+                <div class="row">
+                    
+                    <div ng-repeat="item in dashboard">
+                        <div class="col s6">
+                            <div class="card-panel  blue lighten-5">
+                                <h5>{{ item.menu.alias }}</h5>
+                                <p ng-repeat="log in item.data"><i>{{ log.firstname }} {{ log.lastname}}</i> hat einen Datensatz
+                                    <strong ng-if="log.is_update == 1">bearbeitet</strong>
+                                    <strong ng-if="log.is_insert == 1">hinzugefügt</strong>
+                                    am {{ log.timestamp_create * 1000 | date:"dd.MM.yyyy ' um ' HH:mm" }} Uhr.
+                                </p>
+                            </div>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
