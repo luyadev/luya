4373f2aaccfe45ada72fe22634572046aa01028a
nadar <git@nadar.io>
2015-06-15 09:20:26 +0200

    fixed issue #44

--- src/commands/CommandController.php
+++ src/commands/CommandController.php
@@ -12,6 +12,10 @@ class CommandController extends \yii\console\Controller
     public function actionIndex($module, $route = 'default')
     {
         $moduleObject = Yii::$app->getModule($module);
+        if (!$moduleObject) {
+            echo "The module '$module' does not exist.";
+            exit(0);
+        }
         $moduleObject->controllerNamespace = $module .'\commands';
         echo $moduleObject->runAction($route);
         echo PHP_EOL;
