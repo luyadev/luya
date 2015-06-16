1becd8f85a1b1a4c168db79c27ccb690e56d8277
Marc Stampfli <marc.stampfli@zephir.ch>
2015-06-16 14:17:01 +0200

    Clone blocks on drag

--- modules/cmsadmin/views/page/update.php
+++ modules/cmsadmin/views/page/update.php
@@ -210,7 +210,7 @@
                 <div class="col s12">
                     <div class="blockholder__group" ng-repeat="item in DroppableBlocksService.blocks">
                         <b class="blockholder__group-title">{{item.group.name}}</b>
-                        <div class="blockholder__block" ng-repeat="block in item.blocks" data-drag="true" jqyoui-draggable="{placeholder: 'keep', index : {{$index}}, onStart : 'onStart', onStop : 'onStop'}" ng-model="item.blocks" data-jqyoui-options="{revert: false, helper : 'original'}">
+                        <div class="blockholder__block" ng-repeat="block in item.blocks" data-drag="true" jqyoui-draggable="{placeholder: 'keep', index : {{$index}}, onStart : 'onStart', onStop : 'onStop'}" ng-model="item.blocks" data-jqyoui-options="{revert: false, helper : 'clone'}">
                             <i class="mdi-editor-format-align-left"></i> {{ block.name}}
                         </div>
                     </div>
