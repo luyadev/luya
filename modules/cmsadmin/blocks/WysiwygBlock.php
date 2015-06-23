8cc48c236ee6c55a69f6f1f83563e39f3b869563
Martin Petrasch <martin.petrasch@zephir.ch>
2015-06-23 15:08:03 +0200

    changed icon and empty message/behavior of wysiwygBlock

--- modules/cmsadmin/blocks/WysiwygBlock.php
+++ modules/cmsadmin/blocks/WysiwygBlock.php
@@ -6,25 +6,30 @@ class WysiwygBlock extends \cmsadmin\base\Block
 {
     public function name()
     {
-        return 'WYSIWYG';
+        return 'Texteditor';
     }
-    
+
+    public function icon()
+    {
+        return "mdi-editor-format-color-text";
+    }
+
     public function config()
     {
         return [
             'vars' => [
-                ['var' => 'wysiwyg', 'label' => 'Inhalt', 'type' => 'zaa-wysiwyg']
+                ['var' => 'content', 'label' => 'Inhalt', 'type' => 'zaa-wysiwyg']
             ]
         ];
     }
-    
+
     public function twigFrontend()
     {
-        return '{{ vars.wysiwyg }}';
+        return '{% if vars.content is not empty %}{{ vars.content }}{% endif %}';
     }
-    
+
     public function twigAdmin()
     {
-        return '{{ vars.wysiwyg }}';
+        return '{% if vars.content is empty %}<span class="block__empty-text">Es wurde noch kein Text eingegeben.</span>{% else %}{{ vars.content }}{% endif %}';
     }
 }
