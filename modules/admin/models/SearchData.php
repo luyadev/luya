15b5fb314c611446fbd7795ec66096457cb2d2ca
Nadar <git@nadar.io>
2015-06-26 11:01:17 +0200

    added search model #131

---
+++ modules/admin/models/SearchData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace admin\models;
+
+use Yii;
+
+class SearchData extends \yii\db\ActiveRecord
+{
+    public function init()
+    {
+        parent::init();
+        $this->on(self::EVENT_BEFORE_VALIDATE, [$this, 'onBeforeValidate']);
+    }
+    public static function tableName()
+    {
+        return 'admin_search_data';
+    }
+    
+    public function rules()
+    {
+        return [
+            [['query', 'num_rows', 'user_id'], 'required'],
+        ];
+    }
+    
+    public function onBeforeValidate()
+    {
+        $this->timestamp_create = time();
+        $this->user_id = Yii::$app->adminuser->getId();
+    }
+}
