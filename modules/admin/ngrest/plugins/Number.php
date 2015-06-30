34a88c3466f96cf3c475523ca5df072e0002d50f
nadar <git@nadar.io>
2015-06-30 09:30:51 +0200

    ngrest placeholder integration closes #143

--- modules/admin/ngrest/plugins/Number.php
+++ modules/admin/ngrest/plugins/Number.php
@@ -4,6 +4,13 @@ namespace admin\ngrest\plugins;
 
 class Number extends \admin\ngrest\base\Plugin
 {
+    public $placeholder = null;
+    
+    public function __construct($placeholder = null)
+    {
+        $this->placeholder = $placeholder;
+    }
+    
     public function renderList($doc)
     {
         $doc->appendChild($doc->createElement('span', '{{item.'.$this->name.'}}'));
@@ -18,6 +25,7 @@ class Number extends \admin\ngrest\base\Plugin
         $elmn->setAttribute('model', $this->ngModel);
         $elmn->setAttribute('label', $this->alias);
         $elmn->setAttribute('grid', $this->gridCols);
+        $elmn->setAttribute('placeholder', $this->placeholder);
         $doc->appendChild($elmn);
 
         return $doc;
--- modules/admin/ngrest/plugins/Textarea.php
+++ modules/admin/ngrest/plugins/Textarea.php
@@ -4,6 +4,13 @@ namespace admin\ngrest\plugins;
 
 class Textarea extends \admin\ngrest\base\Plugin
 {
+    public $placeholder = null;
+    
+    public function __construct($placeholder = null)
+    {
+        $this->placeholder = $placeholder;
+    }
+    
     public function renderList($doc)
     {
         $elmn = $doc->createElement('span', '{{item.'.$this->name.'}}');
@@ -20,6 +27,7 @@ class Textarea extends \admin\ngrest\base\Plugin
         $elmn->setAttribute('model', $this->ngModel);
         $elmn->setAttribute('label', $this->alias);
         $elmn->setAttribute('grid', $this->gridCols);
+        $elmn->setAttribute('placeholder', $this->placeholder);
         $doc->appendChild($elmn);
         return $doc;
     }
