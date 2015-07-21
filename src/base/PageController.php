4cf6839e2dd5f0600f47c8b5ca2b52724d37217b
nadar <git@nadar.io>
2015-07-21 12:54:17 +0200

    fixed exception class use

--- src/base/PageController.php
+++ src/base/PageController.php
@@ -2,7 +2,7 @@
 
 namespace luya\base;
 
-throw new Exception("PageController is not in use anymore. Extend your controller from '\luya\baseController'.");
+throw new \Exception("PageController is not in use anymore. Extend your controller from '\luya\baseController'.");
 
 /*
 class PageController extends \luya\base\Controller implements \yii\base\ViewContextInterface
