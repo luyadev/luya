53a6ffd02107c395e92443daa2d3ba3176cff413
nadar <git@nadar.io>
2015-07-27 16:19:38 +0200

    added phpdoc link info

--- modules/crawler/Module.php
+++ modules/crawler/Module.php
@@ -2,6 +2,12 @@
 
 namespace crawler;
 
+/**
+ * 
+ * @link https://github.com/FriendsOfPHP/Goutte
+ * @link http://api.symfony.com/2.7/Symfony/Component/DomCrawler.html
+ * @author nadar
+ */
 class Module extends \luya\base\Module
 {
     public $baseUrl = null;   
