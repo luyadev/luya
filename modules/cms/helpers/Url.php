a76eaf08f9022cf44c3ff69f760a4fb96c03e3a0
nadar <git@nadar.io>
2015-07-30 10:47:13 +0200

    added cms\helpers\Url

---
+++ modules/cms/helpers/Url.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace cms\helpers;
+
+use Yii;
+use cmsadmin\models\NavItem;
+
+class Url
+{
+    /**
+     * if the module could not be found (not registered in the cms) the method returns the provided module name.
+     * 
+     * returns only ONE even if there are more! not good structure inside the cms?
+     * 
+     * @param string $moduleName
+     * @return url
+     */
+    public static function toModule($moduleName)
+    {
+        $model = NavItem::find()->leftJoin('cms_nav_item_module', 'nav_item_type_id=cms_nav_item_module.id')->where(['nav_item_type' => 2, 'cms_nav_item_module.module_name' => $moduleName])->asArray()->one();
+        
+        if ($model) {
+            $link = Yii::$app->links->findOneByArguments(['id' => $model['id']]);
+            if ($link) {
+                return $link['full_url'];
+            }
+        }
+        
+        return $moduleName;
+    }
+}
