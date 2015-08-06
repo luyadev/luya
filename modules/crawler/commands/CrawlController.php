e968e4654706aa482829fc08230a5d0f23eaaf7c
nadar <git@nadar.io>
2015-08-06 09:47:39 +0200

    remove vars

--- modules/crawler/commands/CrawlController.php
+++ modules/crawler/commands/CrawlController.php
@@ -15,17 +15,8 @@ class CrawlController extends \luya\base\Command
 {
     public function actionIndex()
     {
-        $url = 'http://kunstmuesumbasel.ch';
         $client = new Client();
         $pageCrawler = new CrawlPage(['client' => $client]);
         $container = new CrawlContainer(['baseUrl' => $this->module->baseUrl, 'pageCrawler' => $pageCrawler]);
-        
-        /*
-        $client = new Client();
-        $obj = new CrawlPage(['client' => $client, 'pageUrl' => 'http://localhost/luya/envs/dev/public_html/de/module-test']);
-        
-        var_dump($obj->getContent());
-        print_r($obj->getLinks());
-        */
     }
 }
