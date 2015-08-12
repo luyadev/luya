8fe03e5b058e59f0b6a4cd2cfc5cf82e2cd46eb3
nadar <git@nadar.io>
2015-08-12 11:25:39 +0200

    added url rule docs

--- src/base/UrlRule.php
+++ src/base/UrlRule.php
@@ -2,8 +2,15 @@
 
 namespace luya\base;
 
-/* must be sbtarct
- *
+/**
+ * Can't set the UrlRule as abstract cause its used as base class for all UrlRules, this
+ * is a behavior of the UrlManger where is set like this:
+ * 
+ * ```
+ * public $ruleConfig = ['class' => '\luya\base\UrlRule'];
+ * ```
+ * 
+ * @author nadar
  */
 class UrlRule extends \yii\web\UrlRule
 {
