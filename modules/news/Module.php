3fa9a544081fc6c52118e69f9cfc958285d485c4
nadar <git@nadar.io>
2015-08-13 17:05:47 +0200

    changed url rule for news

--- modules/news/Module.php
+++ modules/news/Module.php
@@ -5,6 +5,6 @@ namespace news;
 class Module extends \luya\base\Module
 {
     public $urlRules = [
-        ['pattern' => 'news/detail/<id:\d+>/<title:[a-zA-Z0-9\-]+>/', 'route' => 'news/default/detail'],
+        ['pattern' => 'news/<id:\d+>/<title:[a-zA-Z0-9\-]+>/', 'route' => 'news/default/detail'],
     ];
 }
