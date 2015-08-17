555edf7c3c46d0a7bfc7dc446f42fe92628abcaf
nadar <git@nadar.io>
2015-08-17 12:03:37 +0200

    added base command output formatting listening to application mute state.

--- src/base/Command.php
+++ src/base/Command.php
@@ -2,6 +2,34 @@
 
 namespace luya\base;
 
+use Yii;
+use yii\helpers\Console;
+
 abstract class Command extends \yii\console\Controller
 {
+    public function isMuted()
+    {
+        return Yii::$app->mute;
+    }
+    
+    protected function output($message, $color = false)
+    {
+        if (!$this->isMuted()) {
+            echo $this->ansiFormat("\r".$message."\n", $color);
+        }
+    }
+    
+    public function outputError($message)
+    {
+        $this->output($message, Console::FG_RED);
+        
+        return false;
+    }
+    
+    public function outputSuccess($message)
+    {
+        $this->output($message, Console::FG_GREEN);
+        
+        return true;
+    }
 }
