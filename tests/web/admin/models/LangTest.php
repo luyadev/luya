8c7b53fef7f5eed6aea6b9d701b7872aa29974dc
nadar <git@nadar.io>
2015-08-26 11:16:10 +0200

    added admin model lang test.

---
+++ tests/web/admin/models/LangTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\web\admin\models;
+
+class LangTest extends \tests\web\BaseModel
+{
+    public function getModel()
+    {
+        return new \admin\models\Lang();
+    }
+    
+    public function getParams()
+    {
+        return [
+            'name',
+            'short_code',
+        ];
+    }
+}
