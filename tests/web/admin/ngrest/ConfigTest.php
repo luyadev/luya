0106619307fa1b51071ac98be94caaedcc78f0cf
nadar <git@nadar.io>
2015-08-26 12:42:55 +0200

    added admin ngrest config test

---
+++ tests/web/admin/ngrest/ConfigTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests\web\admin\ngrest;
+
+class ConfigTest extends \tests\web\Base
+{
+    /**
+     * @expectedException Exception
+     */
+    public function testSetConfigException()
+    {
+        $cfg = new \admin\ngrest\Config(['apiEndpoint' => 'rest-url', 'primaryKey' => 'id']);
+        $cfg->setConfig(['foo' => 'bar']);
+        $cfg->setConfig(['not' => 'valid']); // will throw exception: Cant set config if config is not empty
+    }
+    
+    public function testAddFieldIfExists()
+    {
+        $cfg = new \admin\ngrest\Config(['apiEndpoint' => 'rest-url', 'primaryKey' => 'id']);
+        $this->assertEquals(true, $cfg->addField('list', 'foo'));
+        $this->assertEquals(false, $cfg->addField('list', 'foo'));
+    }
+}
