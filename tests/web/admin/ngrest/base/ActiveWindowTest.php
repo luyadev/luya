6da71619d96fed0f2e2765289d41323e346d3255
nadar <git@nadar.io>
2015-08-26 13:19:33 +0200

    added set config test

--- tests/web/admin/ngrest/base/ActiveWindowTest.php
+++ tests/web/admin/ngrest/base/ActiveWindowTest.php
@@ -12,5 +12,9 @@ class ActiveWindowTest extends \tests\web\Base
         
         $this->assertEquals('bar', $aw->render('index', ['foo' => 'bar']));
         $this->assertEquals('index', $aw->index());
+        
+        $aw->setConfig(['id' => 1]);
+        
+        $this->assertArrayHasKey('id', $aw->config);
     }   
 }
