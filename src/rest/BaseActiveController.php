4215526f03c282eb2989d0bd5b5198fb3ec3e61c
nadar <git@nadar.io>
2015-08-27 14:38:52 +0200

    added phpdocs

--- src/rest/BaseActiveController.php
+++ src/rest/BaseActiveController.php
@@ -10,9 +10,21 @@ abstract class BaseActiveController extends \yii\rest\ActiveController implement
 
     public $updateScenario = 'restupdate';
 
+    /**
+     * If a user should not get access to the rest api, an ForbittendHttpException
+     * must be thrown:
+     * 
+     * ```php
+     * throw new \yii\web\ForbiddenHttpException
+     * ```
+     * 
+     * To make access restrictions checks in your Rest controller you have to override
+     * this method, otherwise there is no access check.
+     * 
+     * @see \yii\rest\ActiveController::checkAccess()
+     */
     public function checkAccess($action, $model = null, $params = [])
     {
         // override
-        //throw new \yii\web\ForbiddenHttpException();
     }
 }
