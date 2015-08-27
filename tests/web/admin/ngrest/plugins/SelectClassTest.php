25a281854db0ac08664aaa40a7b96fff1a0cee19
nadar <git@nadar.io>
2015-08-27 14:38:59 +0200

    added tests

---
+++ tests/web/admin/ngrest/plugins/SelectClassTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\web\admin\ngrest\plugins;
+
+/**
+ * @todo test against real config data.
+ * 
+ * @author nadar
+ */
+class SelectTest extends \tests\web\BasePlugin
+{
+    public function testPlugin()
+    {
+        $text = new \admin\ngrest\plugins\SelectClass('\\tests\\data\\models\\UserModel', 'id', 'firstname');
+        $this->assertEquals("<span>{{item.}}</span>", $this->renderListHtml($text));
+        $this->assertEquals('<zaa-select fieldid="" fieldname="" model="" label="" grid="" initvalue="" options="service..selectdata"></zaa-select>', $this->renderCreateHtml($text));
+        $this->assertEquals('<zaa-select fieldid="" fieldname="" model="" label="" grid="" initvalue="" options="service..selectdata"></zaa-select>', $this->renderUpdateHtml($text));
+    }
+}
