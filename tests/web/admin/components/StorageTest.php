48ec5359542161bfe7e3f7f66664b798cdc51cfe
nadar <git@nadar.io>
2015-08-27 11:26:47 +0200

    added storage getter tests

---
+++ tests/web/admin/components/StorageTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tests\web\admin\components;
+
+use Yii;
+
+class StorageTest extends \tests\web\Base
+{
+    public function testGetFile()
+    {
+        $this->assertEquals(true, is_object(Yii::$app->storage->file));
+    }
+    
+    public function testGetFilter()
+    {
+        $this->assertEquals(true, is_object(Yii::$app->storage->filter));
+    }
+    
+    public function testGetEffect()
+    {
+        $this->assertEquals(true, is_object(Yii::$app->storage->effect));
+    }
+    
+    public function testGetImage()
+    {
+        $this->assertEquals(true, is_object(Yii::$app->storage->image));
+    }
+    
+    public function testGetFolder()
+    {
+        $this->assertEquals(true, is_object(Yii::$app->storage->folder));
+    }
+}
