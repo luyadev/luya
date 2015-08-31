530ca3c71b21c5268e09607f80478d6c5a47494e
nadar <git@nadar.io>
2015-08-31 16:03:03 +0200

    bugfix

--- modules/cmsadmin/importers/BlockImporter.php
+++ modules/cmsadmin/importers/BlockImporter.php
@@ -21,7 +21,7 @@ class BlockImporter extends \luya\base\Importer
                     'system_block' => 0,
                     'class' => $ns,
                 ]);
-                $block->insert();$
+                $block->insert();
                 $this->getImporter()->addLog('block', $ns . ' new block has been added to database.');
             } else {
                 $this->getImporter()->addLog('block', $ns . ' already in the database.');
