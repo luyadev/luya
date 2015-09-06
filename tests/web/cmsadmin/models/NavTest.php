ef75a762634c878acbe098f96272c65f1bf2455a
Nadar <git@nadar.io>
2015-09-06 17:57:15 +0200

    added basic nav tests

---
+++ tests/web/cmsadmin/models/NavTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\web\cmsadmin\models;
+
+use \cmsadmin\models\Nav;
+
+class NavTest extends \tests\web\Base
+{
+    public function testFindContent()
+    {
+        $this->assertEquals(false, Nav::findContent(0));
+        $this->assertEquals(null, Nav::findContent(1));
+    }
+    
+    /*
+    public function testCreatePage()
+    {
+        $model = new Nav();
+        $response = $model->createPage(0, 1, 1, 'foo-title', 'foo-rewrite', 1);
+        
+        var_dump($response);
+    }
+    */
+}
