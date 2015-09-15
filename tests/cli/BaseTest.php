0144eb6a910ecf2d47c6d3ec908a675d030f9406
nadar <git@nadar.io>
2015-09-15 11:23:59 +0200

    fixed base cli test clover issue

--- tests/cli/BaseTest.php
+++ tests/cli/BaseTest.php
@@ -4,10 +4,22 @@ namespace tests\cli;
 
 use Yii;
 
+/**
+ * tryed to fix
+ * 
+ * Starting test 'tests\cli\BaseTest::testApp'.
+ * PHP Notice 'yii\base\ErrorException' with message 'Array to string conversion'
+ * in /home/travis/build/zephir/luya/vendor/phpunit/php-token-stream/src/Token/Stream.php:438
+ *
+ * with replacing instanceof method with assertEquals
+ * @author nadar
+ *
+ */
 class BaseTest extends \tests\cli\Base
 {
     public function testApp()
     {
-        $this->assertInstanceOf('luya\cli\Application', Yii::$app);
+        $this->assertEquals(true, Yii::$app instanceof \luya\cli\Application);
+        // $this->assertInstanceOf('luya\cli\Application', Yii::$app);
     }
 }
