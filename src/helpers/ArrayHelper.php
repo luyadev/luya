2efba3c2882071a03345af4ce495ec698487585b
nadar <git@nadar.io>
2015-09-29 14:39:42 +0200

    added array helper unshift method to add assoc arrays at the begin of an array.

--- src/helpers/ArrayHelper.php
+++ src/helpers/ArrayHelper.php
@@ -8,4 +8,11 @@ class ArrayHelper extends \yii\helpers\BaseArrayHelper
     {
         return json_decode(json_encode($array), false);
     }
+        
+    public static function arrayUnshiftAssoc(&$arr, $key, $val)
+    {
+        $arr = array_reverse($arr, true);
+        $arr[$key] = $val;
+        return array_reverse($arr, true);
+    }
 }
