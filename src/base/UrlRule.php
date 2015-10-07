c87cbd4f3b45897ff98b078abdfb8a68fe69ff9b
nadar <git@nadar.io>
2015-10-07 15:32:10 +0200

    added url rule docs

--- src/base/UrlRule.php
+++ src/base/UrlRule.php
@@ -3,10 +3,12 @@
 namespace luya\base;
 
 /**
+ * Base urlRule class used as ruleConfig in UrlManager Componenet
+ * 
  * Can't set the UrlRule as abstract cause its used as base class for all UrlRules, this
  * is a behavior of the UrlManger where is set like this:.
  * 
- * ```
+ * ```php
  * public $ruleConfig = ['class' => '\luya\base\UrlRule'];
  * ```
  * 
@@ -14,5 +16,11 @@ namespace luya\base;
  */
 class UrlRule extends \yii\web\UrlRule
 {
+    /**
+     * @var array Composition rules are multi lingual rule definitions match against different
+     * languages from composition component. This variable will be assigned from the modules urlRule
+     * variable and while foreaching the urlRules the composition values for each rule will be stored
+     * in this variable to retrieve the informations later on.
+     */
     public $composition = [];
 }
