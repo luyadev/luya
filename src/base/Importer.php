159e27ae9e831b6684e6d2b2c9be4bc6ba472a1b
nadar <git@nadar.io>
2015-10-07 13:23:48 +0200

    added docs

--- src/base/Importer.php
+++ src/base/Importer.php
@@ -23,17 +23,35 @@ namespace luya\base;
  */
 abstract class Importer
 {
+    /**
+     * @var mixed|array Read only property contains the importer object.
+     */
     private $_importer = null;
 
+    /**
+     * Class constructor containing the importer object from where its called.
+     * 
+     * @param \luya\commands\ImportController $importer Import Object `\luya\commands\ImportController`.
+     */
     public function __construct(\luya\commands\ImportController $importer)
     {
         $this->_importer = $importer;
     }
 
+    /**
+     * Returns the import object to use the importers methods.
+     * 
+     * @return object Import Object
+     */
     public function getImporter()
     {
         return $this->_importer;
     }
 
+    /**
+     * Each Importer Class must contain a run method.
+     * 
+     * @return void
+     */
     abstract public function run();
 }
