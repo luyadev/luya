10924da74f3a77176bc013c3cc6c932c7acf2e44
nadar <git@nadar.io>
2015-10-07 12:21:16 +0200

    added docs

--- src/base/Bootstrap.php
+++ src/base/Bootstrap.php
@@ -3,32 +3,42 @@
 namespace luya\base;
 
 use Yii;
+use yii\base\Object;
 
 /**
- * create basic bootstraping which is allowed for console and web components.
+ * Base class for luya bootsrapping proccess.
  *
  * @author nadar
  */
 abstract class Bootstrap implements \yii\base\BootstrapInterface
 {
-    public $_modules = null;
+    /**
+     * @var string|array Readonly variable contains all module Objects.
+     */
+    private $_modules = null;
 
-    public $app = null;
-
+    /**
+     * Boostrap method will be invoken by Yii Application bootrapping proccess containing
+     * the Application ($app) Object to get/set data.
+     * 
+     * @param object $app Luya Application `luya\base\Application`.
+     * @return void
+     */
     public function bootstrap($app)
     {
-        $this->setModules($app);
+        $this->extractModules($app);
         $this->beforeRun($app);
         $this->registerComponents($app);
         $this->run($app);
     }
-
-    public function hasModule($module)
-    {
-        return array_key_exists($module, $this->_modules);
-    }
-
-    public function setModules($app)
+    
+    /**
+     * Extract and load all modules from the Application-Object.
+     *
+     * @param object $app Luya Application `luya\base\Application`.
+     * @return void
+     */
+    public function extractModules($app)
     {
         if ($this->_modules === null) {
             foreach ($app->getModules() as $id => $obj) {
@@ -42,11 +52,35 @@ abstract class Bootstrap implements \yii\base\BootstrapInterface
         }
     }
 
+    /**
+     * Check if a Module exists in the module list `getModules()`.
+     * 
+     * @param string $module The name of the Module
+     * @return boolean
+     */
+    public function hasModule($module)
+    {
+        return array_key_exists($module, $this->_modules);
+    }
+
+    /**
+     * Return all modules prepared by `extractModules()` method.
+     * 
+     * @return array An array containg all modules where the key is the module name and 
+     * the value is the Module Object `luya\base\Module`.
+     */
     public function getModules()
     {
         return $this->_modules;
     }
 
+    /**
+     * Register all components from the modules `registerComponents()` method to the
+     * Applcation.
+     * 
+     * @param object $app Luya Appliation `\luya\base\Application`.
+     * @return void;
+     */
     private function registerComponents($app)
     {
         foreach ($this->getModules() as $id => $module) {
@@ -60,7 +94,19 @@ abstract class Bootstrap implements \yii\base\BootstrapInterface
         }
     }
 
+    /**
+     * This method will be invoke before the `run()` method.
+     * 
+     * @param object $app Luya Application `luya\base\Application`
+     * @return void
+     */
     abstract public function beforeRun($app);
 
+    /**
+     * This method will be invoke after the `beforeRun()` method.
+     * 
+     * @param object $app Luya Application `luya\base\Application`
+     * @return void
+     */
     abstract public function run($app);
 }
