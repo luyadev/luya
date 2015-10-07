3740141cd4dc8cac2b75536b222d6c75df83c776
nadar <git@nadar.io>
2015-10-07 10:28:02 +0200

    added phpdocs

--- src/base/Command.php
+++ src/base/Command.php
@@ -5,13 +5,30 @@ namespace luya\base;
 use Yii;
 use yii\helpers\Console;
 
+/**
+ * Base class for all LUYA commands.
+ * 
+ * @author nadar
+ */
 abstract class Command extends \yii\console\Controller
 {
+    /**
+     * Helper method to see if the current Application is muted or not. If the Application is muted, no output will displayed.
+     * 
+     * @return boolean
+     */
     public function isMuted()
     {
         return Yii::$app->mute;
     }
 
+    /**
+     * Helper method for writting console application output, include before and after wrappers.
+     * 
+     * @param string $message The message which is displayed
+     * @param string $color A color from \yii\helpers\Console::FG_GREEN;
+     * @param return void
+     */
     protected function output($message, $color = false)
     {
         if (!$this->isMuted()) {
@@ -19,6 +36,12 @@ abstract class Command extends \yii\console\Controller
         }
     }
 
+    /**
+     * Helper method to stop the console command with an error message, outputError returns exit code 1.
+     * 
+     * @param string $message The message which should be displayed.
+     * @return number Exit code 1
+     */
     public function outputError($message)
     {
         $this->output($message, Console::FG_RED);
@@ -26,6 +49,12 @@ abstract class Command extends \yii\console\Controller
         return 1;
     }
 
+    /**
+     * Helper method to stop the console command with a success message, outputSuccess returns exit code 0.
+     * 
+     * @param string $message The message which sould be displayed
+     * @return number Exit code 0
+     */
     public function outputSuccess($message)
     {
         $this->output($message, Console::FG_GREEN);
--- src/base/Controller.php
+++ src/base/Controller.php
@@ -8,29 +8,25 @@ namespace luya\base;
 abstract class Controller extends \yii\web\Controller
 {
     /**
-     * Use the default behaviour of Yii. This will result in loading the templates inside the Modules.
-     *
-     * @var bool
+     * @var boolean Use the default behaviour of Yii. This will result in loading the templates inside the Modules.
      */
     public $useModuleViewPath = false;
 
     /**
-     * skips defined assets from the module base, you can not skip assets which are registered in the local asset variable. To Skip
+     * @var array skips defined assets from the module base, you can not skip assets which are registered in the local asset variable. To Skip
      * all the assets from the module ($this->module->assets) you can use skipAssets = ["*"];.
-     *
-     * @var array
      */
     public $skipModuleAssets = [];
 
     /**
-     * controller specific asset class to register in the view.
-     *
-     * @var array The class name of the Asset (e.g. "\admin\asset\BowerAsset")
+     * @var array Defined assets where should be assigned into the view for this controller. The class name of the Asset (e.g. "\admin\asset\BowerAsset")
      */
     public $assets = [];
 
     /**
-     * Yii initializer.
+     * Yii initializer. Find assets to register, and add them into the view if they are not ignore by $skipModuleAssets.
+     * 
+     * @return void
      */
     public function init()
     {
@@ -49,7 +45,7 @@ abstract class Controller extends \yii\web\Controller
     }
 
     /**
-     * Override the default yii controller getViewPath method. To define the template folders in where
+     * Override the default Yii controller getViewPath method. To define the template folders in where
      * the templates are located. Why? Basically some modules needs to put theyr templates inside of the client
      * repository.
      *
@@ -71,10 +67,11 @@ abstract class Controller extends \yii\web\Controller
     }
 
     /**
-     * if we are acting in the module context and the layout is empty we only should renderPartial the content.
+     * If we are acting in the module context and the layout is empty we only should renderPartial the content.
      *
-     * @param unknown_type $view
-     * @param unknown_type $params
+     * @param string $view The name of the view file (e.g. index)
+     * @param array $params The params to assign into the value for key is the variable and value the content.
+     * @return string
      */
     public function render($view, $params = [])
     {
@@ -85,11 +82,52 @@ abstract class Controller extends \yii\web\Controller
         return parent::render($view, $params);
     }
 
+    /**
+     * Returns the path for layout files when using `renderLayout()` method. Those module layouts are located in @app/views
+     * 
+     * @return string The path to the layout for the current Module.
+     */
     public function getModuleLayoutViewPath()
     {
         return '@app/views/'.$this->module->id.'/';
     }
 
+    /**
+     * Luya implementation of layouts for controllers. The method will return a view file wrapped by a custom module layout. 
+     * For example you have a e-store module with a header which returns the basket you can use the module layout in all the actions
+     * to retrieve the same header. Example e-store controller class:
+     * 
+     * ```php
+     * class EstoreController extends \luya\base\Controller
+     * {
+     *     public function actionIndex()
+     *     {
+     *         return $this->renderLayout('index', ['content' => 'This is my index content in variabel $content.']);
+     *     }
+     *     
+     *     public function actionBasket()
+     *     {
+     *          return $this->renderLayout('basket', ['otherVariable' => 'This is a variable for the basket view file in variable $otherVariable.']);
+     *     }
+     * }
+     * ```
+     * 
+     * The example layout file which is located in `@app/views/module/layout` could look something like this:
+     * 
+     * ```php
+     * <ul>
+     *  <li>E-Store Frontpage</li>
+     *  <li>Basket</li>
+     * </ul>
+     * <div id="estore-wrapper">
+     *      <?= $content; ?>
+     * </div>
+     * ```
+     * 
+     * @param string $view The name of the view file
+     * @param array $params The params to assign into the view file.
+     * @return string Rendered template wrapped by the module layout file.
+     */
     public function renderLayout($view, $params = [])
     {
         $content = $this->view->renderFile($this->getViewPath().'/'.$view.'.php', $params, $this);
