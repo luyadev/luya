5d821ee674962d827bbc1f7dae0a4312cb91bb37
nadar <git@nadar.io>
2015-10-08 10:16:45 +0200

    added account is protected area property.

---
+++ modules/account/properties/IsProtectedAreaProperty.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace account\properties;
+
+use Yii;
+use luya\helpers\Url;
+
+class IsProtectedAreaProperty extends \admin\base\Property
+{
+    public function init()
+    {
+        $this->on(self::EVENT_BEFORE_RENDER, [$this, 'eventBeforeRender']);
+    }
+    
+    public function eventBeforeRender($event)
+    {
+        if (Yii::$app->getModule('account')->getUserIdentity()->isGuest) {
+            $event->isValid = false;
+            Yii::$app->response->redirect(Url::to('account/default/index'));
+        }
+    }
+    
+    public function varName()
+    {
+        return 'isProtectedArea';
+    }
+    
+    public function label()
+    {
+        return 'Diese Seite schützen';
+    }
+    
+    public function type()
+    {
+        return 'zaa-select';
+    }
+    
+    public function options()
+    {
+        return [
+            ['value' => 0, 'label' => 'Nein'],
+            ['value' => 1, 'label' => 'Ja'],
+        ];
+    }
+}
