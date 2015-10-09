a0aeda3094ed1bc83bcb2a3304e3b798544a3df8
Nadar <git@nadar.io>
2015-10-09 14:30:28 +0200

    added ability to remove users

--- modules/admin/apis/UserController.php
+++ modules/admin/apis/UserController.php
@@ -4,14 +4,5 @@ namespace admin\apis;
 
 class UserController extends \admin\ngrest\base\Api
 {
-    public function actions()
-    {
-        $actions = parent::actions();
-
-        unset($actions['delete']);
-
-        return $actions;
-    }
-
     public $modelClass = 'admin\models\User';
 }
