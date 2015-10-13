573b5fe88d5c0b80574e5aa9ac93edaa9a0a291a
nadar <git@nadar.io>
2015-10-13 10:17:51 +0200

    removed not null integrity check

--- modules/admin/migrations/m150304_152238_admin_storage_file.php
+++ modules/admin/migrations/m150304_152238_admin_storage_file.php
@@ -19,8 +19,8 @@ class m150304_152238_admin_storage_file extends Migration
             'hash_file' => Schema::TYPE_STRING,
             'hash_name' => Schema::TYPE_STRING,
             'upload_timestamp' => 'int(11) NOT NULL default 0',
-            'file_size' => 'int(11) NOT NULL default 0', // in bytes
-            'upload_user_id' => 'int(11) NOT NULL default 0',
+            'file_size' => 'int(11) default 0', // in bytes
+            'upload_user_id' => 'int(11) default 0',
             'is_deleted' => 'tinyint(1) NOT NULL default 0',
         ]);
     }
