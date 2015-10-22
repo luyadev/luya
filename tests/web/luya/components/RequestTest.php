1217610c6d39c58425126155609d5bb25d2c63d6
Martin Petrasch <martin.petrasch@zephir.ch>
2015-10-22 16:07:40 +0200

    added unit test to compontents/requests

---
+++ tests/web/luya/components/RequestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace tests\web\luya\components;
+
+use Yii;
+use luya\components\Request;
+
+class RequestTest extends \tests\web\Base
+{
+    public function testIsAdmin()
+    {
+        $request = new Request();
+        $this->assertEquals(false, $request->isAdmin());
+
+        $request->forceWebRequest = true;
+        $request->pathInfo = "admin/test/";
+        $this->assertEquals(true, $request->isAdmin());
+
+        $request->forceWebRequest = true;
+        $request->pathInfo = "test/admin/test/";
+        $this->assertEquals(false, $request->isAdmin());
+
+        $request->forceWebRequest = true;
+        $request->pathInfo = "admin/ÜÄß";
+        $this->assertEquals(true, $request->isAdmin());
+
+        $request->forceWebRequest = true;
+        $request->pathInfo = "de/admin/test/";
+        $this->assertEquals(true, $request->isAdmin());
+
+        // changing language code in composition pattern
+        Yii::$app->composition->pattern = "<langShortCode:[0-9]{1}>";
+
+        $request->forceWebRequest = true;
+        $request->pathInfo = "1/admin/test/";
+        $this->assertEquals(true, $request->isAdmin());
+
+    }
+}
