a654c0dee821f2a0b60797be57cd77e624eb2bc9
Martin Petrasch <martin.petrasch@zephir.ch>
2015-10-22 16:07:12 +0200

    added php doc to request, small code refinements

--- src/components/Request.php
+++ src/components/Request.php
@@ -4,10 +4,24 @@ namespace luya\components;
 
 use Yii;
 
+/**
+ * Luya Request Component to provide a differentiation for frontend/admin context
+ *
+ * @author nadar
+ */
 class Request extends \yii\web\Request
 {
+    /**
+     * @var boolean force web request to enable unit tests with simulated web requests
+     */
     public $forceWebRequest = false;
 
+    /**
+     * Resolve the current url request and check if admin context.
+     *
+     * @return boolean if admin context available?
+     *
+     */
     public function isAdmin()
     {
         if ($this->getIsConsoleRequest() && !$this->forceWebRequest) {
@@ -16,10 +30,7 @@ class Request extends \yii\web\Request
 
         $resolver = Yii::$app->composition->getResolvedPathInfo($this);
 
-        $pathInfo = $resolver['route'];
-
-        $parts = explode('/', $pathInfo);
-        $first = reset($parts);
+        $first = reset(explode('/', $resolver['route']));
 
         if ($first == 'admin') {
             return true;
