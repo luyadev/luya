ed27c63bb3532d2866214347f5202c64a8b4def9
Martin Petrasch <martin.petrasch@zephir.ch>
2015-10-26 18:01:49 +0100

    Added color plugin to ngrest/plguins this fixes #400

---
+++ modules/admin/ngrest/plugins/Color.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace admin\ngrest\plugins;
+
+class Color extends \admin\ngrest\base\Plugin
+{
+    public function renderList($doc)
+    {
+        $element = $doc->createElement('span','&nbsp;');
+        $element->setAttribute('style', 'font-size:14.5px; padding-left:30px; background-color : #{{item.'.$this->name.'}}');
+        $doc->appendChild($element);
+
+        return $doc;
+    }
+
+    public function renderCreate($doc)
+    {
+        throw new \Exception('Error: there is no create implementation for ColorPlugin yet!');
+    }
+
+    public function renderUpdate($doc)
+    {
+        throw new \Exception('Error: there is no update implementation for ColorPlugin yet!');
+    }
+}
