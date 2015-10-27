61e1604173d144323840487cfe895f35587b6f75
nadar <git@nadar.io>
2015-10-27 13:49:22 +0100

    display if cat is empty in page tree.

--- modules/cmsadmin/views/default/index.php
+++ modules/cmsadmin/views/default/index.php
@@ -201,6 +201,7 @@
                 <div class="treeview" ng-repeat="catitem in menu" ng-class="{ 'treeview--drag-active' : showDrag }">
                     <h5 class="treeview__title" ng-click="toggleCat(catitem.id)"><i class="material-icons" ng-if="toggleIsHidden(catitem.id)">keyboard_arrow_up</i><i class="material-icons" ng-if="!toggleIsHidden(catitem.id)">keyboard_arrow_down</i> {{catitem.name}}</h5>
 
+                    <p ng-show="catitem.__items.length == 0"><em>Keine Seiten</em></p>
                     <ul class="treeview__list" ng-hide="toggleIsHidden(catitem.id)">
                         <li class="treeview__item" ng-repeat="data in catitem.__items" ng-include="'reverse.html'"></li>
                     </ul>
