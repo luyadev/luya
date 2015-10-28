0854833ec512a7286a4da6febaade40d097c4751
nadar <git@nadar.io>
2015-10-28 11:41:48 +0100

    moved back to php 5.5 version

--- src/Boot.php
+++ src/Boot.php
@@ -2,7 +2,7 @@
 
 namespace luya;
 
-if (!version_compare(PHP_VERSION, '5.6.0', '>=')) {
+if (!version_compare(PHP_VERSION, '5.5.0', '>=')) {
     trigger_error('Some functions of luya need php version 5.6.0 or higher! You are currently using Version: '.PHP_VERSION, E_USER_ERROR);
 }
 
