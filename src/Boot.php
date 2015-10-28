f847598f7eb6acb2fa411c109ed72b8b1d26ad60
nadar <git@nadar.io>
2015-10-28 09:04:03 +0100

    luya update to php version 5.6

--- src/Boot.php
+++ src/Boot.php
@@ -2,8 +2,8 @@
 
 namespace luya;
 
-if (!version_compare(PHP_VERSION, '5.4.0', '>=')) {
-    trigger_error('Some functions of luya need php version 5.4.0 or higher! You are currently using Version: '.PHP_VERSION, E_USER_ERROR);
+if (!version_compare(PHP_VERSION, '5.6.0', '>=')) {
+    trigger_error('Some functions of luya need php version 5.6.0 or higher! You are currently using Version: '.PHP_VERSION, E_USER_ERROR);
 }
 
 class Boot extends \luya\base\Boot
