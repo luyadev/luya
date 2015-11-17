7500acbb36c71ff23711adb81c931368d698edc9
nadar <git@nadar.io>
2015-11-17 13:03:49 +0100

    removed storage dump

--- envs/dev/views/layouts/main.php
+++ envs/dev/views/layouts/main.php
@@ -1,10 +1,3 @@
-<?php
-    $i = (new \admin\storage\ImageQuery())->all();
-    
-    foreach($i as $m) {
-        echo $m->source . PHP_EOL;
-    }
-?>
 <html>
     <head>
         <title>Luya &mdash; <?= $this->title; ?></title>
