6066aef60612150d592f55d7453280c1c5a8cfdf
nadar <git@nadar.io>
2015-11-25 12:28:38 +0100

    added base doc

--- src/web/AssetManager.php
+++ src/web/AssetManager.php
@@ -2,6 +2,12 @@
 
 namespace luya\web;
 
+/**
+ * Wrapper class of yii\web\AssetManager to add more functionality for assets in future. 
+ * Specialy for compressing assets.
+ *
+ * @author nadar
+ */
 class AssetManager extends \yii\web\AssetManager
 {
 }
