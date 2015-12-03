f613cb0ed6b73fabc565dd9ffe02554fa1a64c9e
nadar <git@nadar.io>
2015-12-03 14:44:22 +0100

    luya kickstarter layout update

--- envs/dev/views/layouts/main.php
+++ envs/dev/views/layouts/main.php
@@ -12,17 +12,36 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>Luya Website</h1>
+                        <h1><?= Yii::$app->siteTitle; ?></h1>
                     </div>
                     <div class="col-md-6">
                         <div class="git pull-right">
-                            <a href="https://github.com/zephir/luya" target="_blank" alt="Luya on GitHub" title="Luya on GitHub"><i class="fa fa-github fa-4x"></i></a>
+                            <a href="https://github.com/zephir/luya" target="_blank" alt="Luya on GitHub" title="Luya on GitHub"><i class="fa fa-github fa-2x"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
+        
+        <!-- You can delete this line in your template! as this is only a luya information. -->
+        <div style="background: #9ccc65; margin-top:20px;" class="container" id="yes">
+            <div>
+                <p style="padding:10px 10px; margin:0px;"><?= \luya\Module::t('luya', 'kickstarter_success'); ?></p>
+            </div>
+            <div>
+                <p style="padding:10px 10px; margin:0px; font-weight:bold;"><?= \luya\Module::t('luya', 'kickstarter_admin_link', ['link' => \luya\helpers\Url::toInternal(['admin/default/index']), true]); ?></p>
+            </div>
+        </div>
         <div class="container" id="content">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <? foreach(Yii::$app->menu->current->teardown as $item): ?>
+                    <li><a href="<?= $item->link; ?>"><?= $item->title; ?></a>
+                    <? endforeach; ?>
+                </ol>
+            </div>
+        
             <div class="row">
                 <div class="col-md-3"  id="nav">
                     <ul>
@@ -57,7 +76,7 @@
         </div>
         <div id="footer">
             <div class="container divider">
-                <h4>&copy <?= date("Y"); ?> by Luya</h4>
+                <h4>&copy <?= date("Y"); ?> by <i>You</i> &amp; <i>Luya</i></h4>
             </div>
         </div>
     <?php $this->endBody() ?>
--- envs/kickstarter/views/layouts/main.php
+++ envs/kickstarter/views/layouts/main.php
@@ -1,13 +1,9 @@
-<?php
-use yii\helpers\Url;
-$this->beginPage();
-?>
+<? $this->beginPage(); ?>
 <html>
     <head>
         <title>Luya &mdash; <?= $this->title; ?></title>
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <base href="<?= Url::base(true); ?>/" />
         <?php $this->head() ?>
     </head>
     <body>
@@ -16,18 +12,36 @@ $this->beginPage();
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>Luya Website</h1>
-                        <h2><?= Url::base(true); ?></h2>
+                        <h1><?= Yii::$app->siteTitle; ?></h1>
                     </div>
                     <div class="col-md-6">
                         <div class="git pull-right">
-                            <a href="https://github.com/zephir/luya" target="_blank" alt="Luya on GitHub" title="Luya on GitHub"><i class="fa fa-github fa-4x"></i></a>
+                            <a href="https://github.com/zephir/luya" target="_blank" alt="Luya on GitHub" title="Luya on GitHub"><i class="fa fa-github fa-2x"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
+        
+        <!-- You can delete this line in your template! as this is only a luya information. -->
+        <div style="background: #9ccc65; margin-top:20px;" class="container" id="yes">
+            <div>
+                <p style="padding:10px 10px; margin:0px;"><?= \luya\Module::t('luya', 'kickstarter_success'); ?></p>
+            </div>
+            <div>
+                <p style="padding:10px 10px; margin:0px; font-weight:bold;"><?= \luya\Module::t('luya', 'kickstarter_admin_link', ['link' => \luya\helpers\Url::toInternal(['admin/default/index']), true]); ?></p>
+            </div>
+        </div>
         <div class="container" id="content">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <? foreach(Yii::$app->menu->current->teardown as $item): ?>
+                    <li><a href="<?= $item->link; ?>"><?= $item->title; ?></a>
+                    <? endforeach; ?>
+                </ol>
+            </div>
+        
             <div class="row">
                 <div class="col-md-3"  id="nav">
                     <ul>
@@ -62,7 +76,7 @@ $this->beginPage();
         </div>
         <div id="footer">
             <div class="container divider">
-                <h4>&copy <?= date("Y"); ?> by Luya</h4>
+                <h4>&copy <?= date("Y"); ?> by <i>You</i> &amp; <i>Luya</i></h4>
             </div>
         </div>
     <?php $this->endBody() ?>
