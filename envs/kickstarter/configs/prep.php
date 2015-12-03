8c3bccdbdac4b0a8553dd63d0462e6adcc240b19
nadar <git@nadar.io>
2015-12-03 15:35:52 +0100

    added kickstarter default config informationes

--- envs/kickstarter/configs/prep.php
+++ envs/kickstarter/configs/prep.php
@@ -1,8 +1,8 @@
 <?php
 
-/**
- * the project specific configurations, your working host specific configurations must be located in the local.php config.
- */
+// enable or disable the debugging, if thos values are deleted YII_DEBUG is false and YII_ENV is prod.
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
 $config = [
     'id' => 'myproject', // For best interoperability it is recommended that you use only alphanumeric characters when specifying an application ID
@@ -26,6 +26,8 @@ $config = [
         ]
     ],
     'components' => [
+        // add your smtp connection to the mail component to send mails (which is required for secure login), you can test your
+        // mail component with the luya console command ./vendor/bin/luya health/mailer.
     	'mail' => [
     		'host' => null,
     		'username' => null,
@@ -36,30 +38,39 @@ $config = [
     	'errorHandler' => [
     		/*
     		 * ATTENTION:
-    		 * To help us improve our Software you can enable this property to send all Exceptions directly to the luya developer team. The follwing informations will be transfered:
+    		 * To help us improve our Software you can enable (true) this property to send all Exceptions directly to the luya developer team. The follwing informations will be transfered:
     		 * - $_GET, $_POST, $_SESSION & $_SERVER
     		 * - Exception Object (inlcuding stack trace, line, linenr, message, file)
     		 */
             'transferException' => false,
-        ]
+        ],
+        // composition component handles all language issues
+        'composition' => [
+            'hidden' => true, // you will not have languages in your url (most case for pages which are not multi lingual)
+            'default' => ['langShortCode' => 'en'], // the default language for the composition should match your default language shortCode in the langauge table.
+        ],
+        // when you are enabling the cache, luya will cache cms blocks and speed up the system in different arrays
+        // this should only be activated in the prod.php (productive) config.
+        /*
+        'cache' => [
+            'class' => 'yii\caching\FileCache', // choose between different caching systems, see yii2 documentation.
+        ],
+        */
     ]
 ];
 
-
-// enable or disable the debugging
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-// if you want to use the debug and gii modules of yii, add them to your composer.json in the require section:
-// "yiisoft/yii2-gii" : "*"
-// "yiisoft/yii2-debug" : "*"
+/**
+ * if you want to use the debug and gii modules of yii, add them to your composer.json in the require section:
+ * "yiisoft/yii2-gii" : "*"
+ * "yiisoft/yii2-debug" : "*"
+ */
+/*
 if (YII_DEBUG) {
-	/*
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
-    */
 }
+*/
 
 return \yii\helpers\ArrayHelper::merge($config, require('local.php'));
