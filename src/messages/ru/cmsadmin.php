20f7722eaf1e54deb089de58d185ae31c21f091b
Андрей <aasvetlichniy@gmail.com>
2015-12-04 13:49:21 +0400

    implemented language closes #628

---
+++ src/messages/ru/cmsadmin.php
@@ -0,0 +1,240 @@
+<?php
+
+return [
+    // Blocks
+
+    // AudioBlock
+    'block_audio_name' => 'Аудио',
+    'block_audio_help_soundurl' => 'Вставьте embed code из soundcloud.com сюда.',
+
+    // DevBlock
+    'block_dev_name' => 'Дизайн-тестер',
+
+    // FileListBlock
+    'block_file_list_name' => 'Список файлов',
+    'block_file_list_files_label' => 'Файлы',
+    'block_file_list_files_showtype_label' => 'Отобразить тип файла?',
+    'block_file_list_showtype_yes' => "Да",
+    'block_file_list_showtype_no' => "Нет",
+
+    // FormBlock
+    'block_form_name' => 'Форма',
+    'block_form_default_name_label' => 'Название',
+    'block_form_default_name_placeholder' => 'ФИО',
+    'block_form_default_name_error' => 'Пожалуйста введите свое имя',
+    'block_form_default_email_label' => 'Email',
+    'block_form_default_email_placeholder' => 'example@example.com',
+    'block_form_default_email_error' => 'Введите Email',
+    'block_form_default_message_label' => 'Сообщение',
+    'block_form_default_message_error' => 'Введите сообщение',
+    'block_form_default_send_label' => 'Отправить',
+    'block_form_default_send_error' => 'К сожалению, произошла ошибка при отправке сообщения.',
+    'block_form_default_send_success' => 'Спасибо за ваш запрос.',
+    'block_form_default_mail_subject' => 'Связаться',
+
+    // HtmlBlock
+    'block_html_html_label' => 'HTML код',
+    'block_html_no_content' => 'HTML код пока не был добавлен.',
+
+    // ImageBlock
+    'block_image_name' => 'Изображение',
+    'block_image_imageid_label' => 'Загрузка изображений',
+    'block_image_caption_label' => 'Подпись изображения',
+    'block_image_no_image' => 'Изображения пока не были загружены.',
+
+    // ImageTextBlock
+    'block_image_text_name' => 'Текст с изображением',
+    'block_image_text_text_label' => 'Текст',
+    'block_image_text_imageid_label' => 'Загрузка изображений',
+    'block_image_text_imageposition_label' => 'Позиция изображения',
+    'block_image_text_imageposition_left' => 'Слева',
+    'block_image_text_imageposition_right' => 'Справа',
+    'block_image_text_heading_label' => 'Заголовок',
+    'block_image_text_headingtype_label' => 'Размер',
+    'block_image_text_margin_label' => 'Расстояние между изображениями',
+    'block_image_text_margin_pixel' => 'Пикселей',
+    'block_image_text_texttype_label' => 'Тип текста',
+    'block_image_text_texttype_normal' => 'Текст',
+    'block_image_text_texttype_markdown' => 'Markdown текст',
+    'block_image_text_btnlabel_label' => 'Название кнопки',
+    'block_image_text_btnhref_label' => 'Ссылка на кнопке',
+    'block_image_text_targetblank_label' => 'Открыть ссылку в новом окне',
+    'block_image_text_help_texttype' => 'Тип текста указывает только на текс а не на заголовок.',
+    'block_image_text_no_image' => 'Изображения пока не загружены.',
+    'block_image_text_no_text' => 'Текст пока не был вставлен',
+
+    // LayoutBlock
+    'block_layout_name' => 'Шаблон',
+    'block_layout_width_label' => 'Размер первой колонки (значение от 1 до 12)',
+    'block_layout_placeholders_left' => 'Слева',
+    'block_layout_placeholders_right' => 'Справа',
+
+    // LineBlock
+    'block_line_name' => 'Разделительная линия',
+    'block_line_linespace_label' => 'Межстрочный интервал',
+    'block_line_linespace_space' => 'Расстояние(верх/низ)',
+    'block_line_linestyle_label' => 'Стиль линии',
+    'block_line_linestyle_dotted' => 'Точки',
+    'block_line_linestyle_dashed' => 'Пунктир',
+    'block_line_linestyle_solid' => 'Непрерывный',
+    'block_line_linewidth_label' => 'Ширина линии',
+    'block_line_linecolor_label' => 'Цвет линии',
+
+    // LinkButtonBlock
+    'block_link_button_name' => 'Кнопка-ссылка',
+    'block_link_button_btnlabel_label' => 'Название кнопки',
+    'block_link_button_btnhref_label' => 'Ссылка',
+
+    // ListBlock
+    'block_list_name' => 'Список',
+    'block_list_elements_label' => 'Элементы',
+    'block_list_listtype_label' => 'Тип',
+    'block_list_listtype_ul' => 'Список',
+    'block_list_listtype_ol' => 'Нумерация',
+    'block_list_no_content' => 'Пока что нет контента.',
+
+    // MapBlock
+    'block_map_name' => 'Карта',
+    'block_map_address_label' => 'Адрес',
+    'block_map_zoom_label' => 'Увеличить',
+    'block_map_zoom_entire' => 'Полный масштаб',
+    'block_map_zoom_world' => 'Мир',
+    'block_map_zoom_continent' => 'Континент',
+    'block_map_zoom_country' => 'Страна',
+    'block_map_zoom_city' => 'Город',
+    'block_map_zoom_district' => 'Район',
+    'block_map_zoom_street' => 'Улица',
+    'block_map_zoom_house' => 'Дом',
+    'block_map_maptype_label' => 'Тип карты',
+    'block_map_maptype_roadmap' => 'Тип улицы',
+    'block_map_maptype_satellitemap' => 'Спутник',
+    'block_map_maptype_hybrid' => 'Габрид',
+    'block_map_no_content' => 'Адреса еще не введены.',
+
+    // Module Block
+    'block_module_name' => 'Модуль',
+    'block_module_modulename_label' => 'Название модуля',
+    'block_module_modulecontroller_label' => 'Название контроллера (без суффикса)',
+    'block_module_moduleaction_label' => 'Название действия (без суффикса)',
+    'block_module_moduleactionargs_label' => 'Аргумент (json: {"var":"value"})',
+    'block_module_no_module' => 'Модуль еще не создан.',
+    'block_module_integration' => 'Индеграция модуля',
+
+    // QuoteBlock
+    'block_quote_name' => 'Цитата',
+    'block_quote_content_label' => 'Текст',
+    'block_quote_no_content' => 'Цитата еще не создана.',
+
+    // SpacingBlock
+    'block_spacing_name' => 'Расстояние',
+    'block_spacing_spacing_label' =>'Расстояние',
+
+    // TableBlock
+    'block_table_name' => 'Таблица',
+    'block_table_table_label' => 'Текст',
+    'block_table_header_label' => 'Использовать первую строчку как заголовок',
+    'block_table_stripe_label' => 'Использовать другой цвет для каждой второй строки',
+    'block_table_border_label' => 'Граница таблицы',
+    'block_table_equaldistance_label' => 'Использовать равное расстояние для столбцов',
+    'block_table_help_table' => 'Вы должны добавить колонку и строку, прежде чем добавить содержимое в таблицу.',
+    'block_table_no_table' => 'Таблица пока пустая.',
+
+    // TextBlock
+    'block_text_name' => 'Текст',
+    'block_text_content_label' => 'Текст',
+    'block_text_texttype_label' =>'Тип текста',
+    'block_text_texttype_normal' => 'Дополнительный текст',
+    'block_text_texttype_markdown' => 'Markdown текст',
+    'block_text_no_content' => 'Текст еще не добавлен.',
+
+    // TitleBlock
+    'block_title_name' => 'Заголовок',
+    'block_title_content_label' => 'Название',
+    'block_title_headingtype_label' => 'Размер',
+    'block_title_headingtype_heading' => 'Заголовок',
+    'block_title_no_content' => 'Заголовок еще не задан.',
+
+    // VideoBlock
+    'block_video_name' => 'Видео',
+    'block_video_url_label' => 'URL видео',
+    'block_video_controls_label' => 'Скрыть интерфейс управления?',
+    'block_video_help_url' => 'Поддерживается YouTube и Vimeo Url.',
+    'block_video_help_controls' => 'Эта опция поддерживается только YouTube.',
+    'block_video_no_video' => 'Не вставлен валидный URL.',
+
+    // WysiwygBlock
+    'block_wysiwyg_name' => 'Редактор',
+    'block_wysiwyg_content_label' => 'Контент',
+    'block_wysiwyg_help_content' => 'Кликните на первую строчку чтобы добавить данные',
+    'block_wysiwyg_no_content' => 'Текст пока не доабавлен.',
+    
+    // views/default/index
+
+    'view_index_add_type' => 'Тип страницы',
+    'view_index_type_page' => 'Страница',
+    'view_index_type_module' => 'Модуль',
+    'view_index_type_redirect' => 'Переадресация',
+    'view_index_as_draft' => 'Как в шаблоне',
+    'view_index_as_draft_help' => 'Вы хотитеопределить новую страницу в качестве шаблона?',
+    'view_index_no' => 'Нет',
+    'view_index_yes' => 'Да',
+    'view_index_page_title' => 'Название страницы',
+    'view_index_page_alias' => 'Алиас',
+    'view_index_page_meta_description' => 'Описание (Описание для поисковиков)',
+    'view_index_page_nav_container' => 'Раздел навигации',
+    'view_index_page_parent_page' => 'Родительская страница',
+    'view_index_page_success' => 'Страница успешно создана!',
+    'view_index_page_parent_root' => '[Основной уровень]',
+    'view_index_page_use_draft' => 'Использовать шаблон?',
+    'view_index_page_select_draft' => 'Хотите выбрать шаблон?',
+    'view_index_page_layout' => 'Выбор шаблона',
+    'view_index_page_btn_save' => 'Сохранить новую страницу',
+    'view_index_module_select' => 'Название модуля (Yii-ID)',
+    'view_index_redirect_type' => 'Тип переадресации',
+    'view_index_redirect_internal' => 'Внутреняя страница',
+    'view_index_redirect_external' => 'Внешний URL-адрес',
+    'view_index_redirect_internal_select' => 'Выберите внутреннюю страницу на которую следует направить пользователя.',
+    'view_index_redirect_external_link' => 'Внешняя ссылка',
+    'view_index_redirect_external_link_help' => 'Внешние ссылки начинаются с http:// или https://',
+
+    'view_index_sidebar_new_page' => 'Создать новую страницу',
+    'view_index_sidebar_drafts' => 'Шаблоны',
+    'view_index_sidebar_move' => 'Переместить',
+    
+    // view_update
+    
+    'view_update_drop_blocks' => 'Перетащите блок контента сюда',
+    'view_update_blockcontent' => 'Блок контента',
+    'view_update_config' => 'Дополнительные настройки',
+    'view_update_settings' => 'Настройки',
+    'view_update_btn_save' => 'Сохранить',
+    'view_update_btn_cancel' => 'Прервать',
+    'view_update_holder_state_on' => 'Скрыть',
+    'view_update_holder_state_off' => 'Раскрыть',
+    'view_update_is_draft_mode' => 'Редактировать как черновик',
+    'view_update_is_homepage' => 'Главная страница',
+    'view_update_properties_title' => 'Свойства страницы',
+    'view_update_no_properties_exists' => 'Не все свойства были сохранены для этой страницы.',
+    'view_update_draft_no_lang_error' => 'Черновики не имеют языковых харектиристик.',
+    'view_update_no_translations' => 'Эта страница еще не переведена.',
+    'view_update_page_is_module' => 'СТраница является <b>модулем</b>.',
+    'view_update_page_is_redirect_internal' => 'Эта страница имеет <b>внутреннюю переадресацию</b> на <show-internal-redirection nav-id="typeData.value" />.',
+    'view_update_page_is_redirect_external' => 'Эта страница имеет <b>внешнюю переадресацию</b> на <a ng-href="{{typeData.value}}">{{typeData.value}}</a>',
+    
+    // menu
+    
+    'menu_node_cms' => 'Контент страницы',
+    'menu_node_cmssettings' => 'CMS Настройки',
+    'menu_group_env' => 'Окружающая среда',
+    'menu_group_item_env_container' => 'Контейнеры',
+    'menu_group_item_env_layouts' => 'Шаблоны',
+    'menu_group_elements' => 'Элементы контента',
+    'menu_group_item_elements_blocks' => 'Управлять',
+    'menu_group_item_elements_group' => 'Группы',
+     
+    // global
+    
+    'btn_abort' => 'Отмена',
+    'btn_refresh' => 'Обновить',
+    'btn_save' => 'Сохранить',
+];
