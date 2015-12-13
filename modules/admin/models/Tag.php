a2de4916752f817bbe71a09eeee9a17822fe40a9
Marc Stampfli <marc.stampfli@zephir.ch>
2015-12-13 12:16:34 +0100

    Removed unnecessary code that was added for testing purposes

--- modules/admin/models/Tag.php
+++ modules/admin/models/Tag.php
@@ -2,8 +2,6 @@
 
 namespace admin\models;
 
-use admin\aws\TagActiveWindow;
-
 class Tag extends \admin\ngrest\base\Model
 {
     /* yii model properties */
@@ -46,8 +44,6 @@ class Tag extends \admin\ngrest\base\Model
         $config->create->copyFrom('list');
         $config->update->copyFrom('list');
         $config->delete = true;
-
-        $config->aw->register(new TagActiveWindow("admin_tag"), 'Tags');
 
         return $config;
     }
