5d949140780aea71d3e59da9853e2f9b90fee764
nadar <git@nadar.io>
2015-12-17 13:37:38 +0100

    added basic docs to storage system queries

--- modules/admin/storage/ItemTrait.php
+++ modules/admin/storage/ItemTrait.php
@@ -6,12 +6,27 @@ use Yii;
 
 trait ItemTrait
 {
+    /**
+     * @var array The array containing all data for the current item.
+     */
     public $itemArray = [];
 
+    /**
+     * Method to construct/build the item from Iterator or Query class.
+     * 
+     * @param array $itemArray The array data
+     * @return object Returns the specific item object (file, folder, image).
+     */
     public static function create(array $itemArray)
     {
         return Yii::createObject(['class' => self::className(), 'itemArray' => $itemArray]);
     }
     
+    /**
+     * Convert the current Object methods to an array whether the key is the property and value the method
+     * response for the given key.
+     * 
+     * @return array
+     */
     abstract public function toArray();
 }
--- modules/admin/storage/QueryTrait.php
+++ modules/admin/storage/QueryTrait.php
@@ -10,6 +10,11 @@ trait QueryTrait
     
     private $_where = [];
     
+    /**
+     * Singleton behavior for storage component getter.
+     * 
+     * @return admin\components\StorageContainer
+     */
     public function getStorage()
     {
         if ($this->_storage === null) {
@@ -19,6 +24,24 @@ trait QueryTrait
         return $this->_storage;
     }
     
+    /**
+     * Define the Query specific where values, all where conditions as provided with an assoc array, where the key is the 
+     * field and the value accords to the field value. Each Query object have different condition keys, see the specific query
+     * class to find all conditions.
+     * 
+     * ```php
+     * ->where(['id' => 1]); // WHERE id=1
+     * ```
+     * 
+     * ```php
+     * ->where(['id' => 1, 'folder_id' => 2]); // WHERE id=1 AND folder_id=2
+     * ```
+     * 
+     * All where conditions are defined as "equal" operators, so its *not possible* to set an not equal condition.
+     * 
+     * @param array $args Where "equal" conditions, multiple conditions are thread as "AND" statement.
+     * @return \admin\storage\QueryTrait|Object Current object, to make object chaining possible.
+     */
     public function where(array $args)
     {
         $this->_where = $args;
@@ -37,16 +60,30 @@ trait QueryTrait
         return true;
     }
     
+    /**
+     * Find All
+     * 
+     * @return admin\storage\IteratorAbstract|Object
+     */
     public function all()
     {
         return $this->createIteratorObject(array_filter($this->getDataProvider(), [$this, 'whereFilter']));
     }
     
+    /**
+     * 
+     * @return integer Amount of filtere data.
+     */
     public function count()
     {
         return count(array_filter($this->getDataProvider(), [$this, 'whereFilter']));
     }
     
+    /**
+     * Find One, if there are several items, it just takes the first one and does not throw an exception.
+     * 
+     * @return admin\storage\QueryTrait|Object
+     */
     public function one()
     {
         $data = array_filter($this->getDataProvider(), [$this, 'whereFilter']);
@@ -54,6 +91,12 @@ trait QueryTrait
         return (count($data) !== 0) ? $this->createItem(array_values($data)[0]): false;
     }
     
+    /**
+     * FindOne returns the specific item id
+     * 
+     * @param int $id The specific item id
+     * @return admin\storage\QueryTrait|Object
+     */
     public function findOne($id)
     {
         return ($itemArray = $this->getItemDataProvider($id)) ? $this->createItem($itemArray) : false;
