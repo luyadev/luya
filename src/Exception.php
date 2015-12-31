b2648bb2a9828736a7dbc99662092537d3f5114b
Nadar <git@nadar.io>
2015-12-31 10:54:44 +0100

    extend luya base exception

--- src/Exception.php
+++ src/Exception.php
@@ -3,11 +3,18 @@
 namespace luya;
 
 /**
- * Exception wrapper for yii\base\Exception
+ * Exception wrapper for yii\base\Exception, represents a generic exception for all purposes.
  * 
  * @author nadar
+ * @since 1.0.0-beta3
  */
 class Exception extends \yii\base\Exception
 {
-    
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return 'Luya Exception';
+    }
 }
