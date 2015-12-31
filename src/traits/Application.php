7543ea132583d6d99759ffd8250fdb4ad0c9d5f1
Nadar <git@nadar.io>
2015-12-31 11:55:11 +0100

    added application docs

--- src/traits/Application.php
+++ src/traits/Application.php
@@ -9,6 +9,8 @@ namespace luya\traits;
  */
 trait Application
 {
+    private $_webroot = null;
+    
     /**
      * @var string Title for the appliation used in differnt sections like Login screen
      */
@@ -25,12 +27,26 @@ trait Application
      */
     public $luyaLanguage = 'en';
     
+    /**
+     * @var string|boolean A token which will be used to collect data from a central host if you want to enable thise feature.
+     * Use https://www.random.org/strings/ to create complex strings. When you have enabled this feature you can collect to from 
+     * all your hosts with `example.com/admin/remote?token=Sha1EncodedRemoteToken`.
+     */
     public $remoteToken = false;
 
+    /**
+     * @var string The directory where your webroot represents, this is basically used to find the webroot directory
+     * in the console mode, cause some importer classes need thos variables.
+     */
     public $webrootDirectory = 'public_html';
     
-    private $_webroot = null;
-    
+    /**
+     * Read only property which is used in cli bootstrap process to set the @webroot alias
+     * 
+     * ```
+     * Yii::setAlias('@webroot', $app->webroot);
+     * ```
+     */
     public function getWebroot()
     {
         if ($this->_webroot === null) {
@@ -39,16 +55,20 @@ trait Application
         
         return $this->_webroot;
     }
-    
-    public $luyaCoreComponents = [
-        'mail' => ['class' => 'luya\components\Mail'],
-    ];
 
+    /**
+     * Add additional core components to the yii2 base core components.
+     */
     public function luyaCoreComponents()
     {
-        return array_merge(parent::coreComponents(), $this->luyaCoreComponents);
+        return array_merge(parent::coreComponents(), [
+            'mail' => ['class' => 'luya\components\Mail'],
+        ]);
     }
 
+    /**
+     * @return array Containing all module objects which are coreModules and isntance of luya\base\Module.
+     */
     public function getApplicationModules()
     {
         $modules = [];
