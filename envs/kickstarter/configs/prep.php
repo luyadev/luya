a22d3ddc1c23539c135999ee9dcd0c998d5c9b70
nadar <git@nadar.io>
2016-01-12 15:40:50 +0100

    added config documentation

--- envs/kickstarter/configs/prep.php
+++ envs/kickstarter/configs/prep.php
@@ -1,17 +1,63 @@
 <?php
 
-// enable or disable the debugging, if thos values are deleted YII_DEBUG is false and YII_ENV is prod.
+/*
+ * Enable or disable the debugging, if those values are deleted YII_DEBUG is false and YII_ENV is prod.
+ */
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 $config = [
-    'id' => 'myproject', // For best interoperability it is recommended that you use only alphanumeric characters when specifying an application ID
-    'siteTitle' => 'My Project', // The name of your site, will be display on the login screen
+    
+    /*
+     * For best interoperability it is recommended that you use only alphanumeric characters when specifying an application ID
+     */
+    'id' => 'myproject',
+    
+    /*
+     * The name of your site, will be display on the login screen
+     */
+    'siteTitle' => 'My Project',
+    
+    /*
+     * Set the administration area language. Available language short codes: "en", "de" and "ru"
+     */
+    'luyaLanguage' => 'en',
+    
+    /*
+     * Let the application know which module should be executed by default (if no url is set). This module must be included 
+     * in the modules section. In the most cases you are using the cms as default handler for your website. But the concept
+     * of LUYA is also that you can use the Website without the CMS module! 
+     */
+    'defaultRoute' => 'cms',
+	
+    /*
+     * Define the basePath of the project (Yii Configration Setup
+     */
     'basePath' => dirname(__DIR__),
-    'luyaLanguage' => 'en', // set the administration area language (currently available en, de)
-    'defaultRoute' => 'cms', // set the default module
-	'modules' => [
+    
+    'modules' => [
+        
+        /*
+         * This module is need to run Luya. It contains the different luya bootstraping proccess and components etc.
+         */
         'luya' => 'luya\Module',
+        
+        /*
+         * If you have other administration module (like cmsadmin) then you going to need this module. The Admin module provides
+         * a lot of functionalitiy like storage system etc. But the basic concept of LUYA is also that you can use LUYA without the
+         * admin module.
+         * 
+         * @secureLogin: (boolean) This will activated a two-way authentification method where u get a token sent by mail, for this feature
+         * you have to make sure the mail component is configured correctly, you can test with console command `./vendor/bin/luya health/mailer`.
+         */
+        'admin' => [
+            'class' => 'admin\Module',
+            'secureLogin' => false, // when enabling secure login, the mail component must be proper configured otherwise the auth token mail will not send.
+        ],
+        
+        /*
+         * You can use the cms module if you like.
+         */
         'cms' => [
             'class' => 'cms\Module',
             'assets' => [
@@ -19,15 +65,19 @@ $config = [
             ],
             'enableCompression' => true, // compressing the cms output (removing white spaces and newlines)
         ],
+        
+        /*
+         * This is the administration module for the `cms` module.
+         */
         'cmsadmin' => 'cmsadmin\Module',
-        'admin' => [
-            'class' => 'admin\Module',
-            'secureLogin' => false, // when enabling secure login, the mail component must be proper configured otherwise the auth token mail will not send.
-        ]
+        
     ],
     'components' => [
-        // add your smtp connection to the mail component to send mails (which is required for secure login), you can test your
-        // mail component with the luya console command ./vendor/bin/luya health/mailer.
+        
+        /*
+         * Add your smtp connection to the mail component to send mails (which is required for secure login), you can test your
+         * mail component with the luya console command ./vendor/bin/luya health/mailer.
+         */
     	'mail' => [
     		'host' => null,
     		'username' => null,
@@ -35,30 +85,41 @@ $config = [
     		'from' => null,
     		'fromName' => null,
     	],
+
+        /*
+         * ATTENTION:
+         * To help us improve our Software you can enable (true) this property to send all Exceptions directly to the luya developer team. The follwing informations will be transfered:
+         * - $_GET, $_POST
+         * - Exception Object (inlcuding stack trace, line, linenr, message, file)
+         *
+         * You can also create your own errorapi (zehir/luya-modul-errorapi) module to get notification
+         * about the errors from your projects.
+         */
     	'errorHandler' => [
-    		/*
-    		 * ATTENTION:
-    		 * To help us improve our Software you can enable (true) this property to send all Exceptions directly to the luya developer team. The follwing informations will be transfered:
-    		 * - $_GET, $_POST
-    		 * - Exception Object (inlcuding stack trace, line, linenr, message, file)
-    		 * 
-    		 * You can also create your own errorapi (zehir/luya-modul-errorapi) module to get notification
-    		 * about the errors from your projects.
-    		 */
             'transferException' => false,
         ],
-        // composition component handles all language issues
+        
+        /*
+         * The composition component handles your languages and they way your urls will look like. The composition componentn will
+         * automatically add the language prefix you have defined in `default` to your url (the language part in the url "example.com/EN/homepage").
+         * 
+         * hidden: (boolean) If this website is not multilingual you can hidde the composition, other whise you have to enable this.
+         */
         'composition' => [
             'hidden' => true, // you will not have languages in your url (most case for pages which are not multi lingual)
             'default' => ['langShortCode' => 'en'], // the default language for the composition should match your default language shortCode in the langauge table.
         ],
-        // when you are enabling the cache, luya will cache cms blocks and speed up the system in different arrays
-        // this should only be activated in the prod.php (productive) config.
+        
         /*
+         * When you are enabling the cache, luya will cache cms blocks and speed up the system in different ways. In the prep config
+         * we use the DummyCache to "fake" the caching behavior, but actually nothing gets cached, when your in production you should
+         * use caching which matches your hosting environment. In most cases yii\caching\FileCache will result in fast website.
+         * 
+         * http://www.yiiframework.com/doc-2.0/guide-caching-data.html#cache-apis
+         */
         'cache' => [
             'class' => 'yii\caching\FileCache', // choose between different caching systems, see yii2 documentation.
         ],
-        */
     ]
 ];
 
