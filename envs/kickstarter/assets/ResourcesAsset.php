29c89ee379d4133309eece91f9b6ac29d07ce880
nadar <git@nadar.io>
2016-01-12 15:18:27 +0100

    fixed fonts issue for resources assets

--- envs/kickstarter/assets/ResourcesAsset.php
+++ envs/kickstarter/assets/ResourcesAsset.php
@@ -7,7 +7,7 @@ class ResourcesAsset extends \luya\web\Asset
     public $sourcePath = '@app/resources';
     
     public $css = [
-        '//http://fonts.googleapis.com/css?family=Open+Sans:300,400,600',
+        '//fonts.googleapis.com/css?family=Open+Sans:300,400,600',
         '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
         'bootstrap/css/bootstrap.min.css',
         'bootstrap/css/bootstrap-theme.min.css',
