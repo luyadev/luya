40184f395e192ccc4b7ae4b15473149cc8d6b914
nadar <git@nadar.io>
2016-01-13 13:33:28 +0100

    luya beta 5 release version bump

--- src/Module.php
+++ src/Module.php
@@ -14,7 +14,7 @@ class Module extends \luya\base\Module
      *
      * @var string
      */
-    const VERSION = '1.0.0-beta4';
+    const VERSION = '1.0.0-beta5-dev';
 
     /**
      * Default url behavior if luya is included. first rule which will be picked.
