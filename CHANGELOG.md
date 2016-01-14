dac92cc982d1822c7684a8d42658f6d506e45851
nadar <git@nadar.io>
2016-01-14 09:36:16 +0100

    show is_deleted fils in files container closes #716

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ LUYA CHANGELOG
 
 **ISSUES**
 
+- `#716` Add is_deleted files to the storag container list.
 - `#715` CMS nav item model does now slugify the alias in the model before create/update to ensure correct alias paths.
 
 1.0.0-beta4 (13.1.2016)
--- modules/admin/components/StorageContainer.php
+++ modules/admin/components/StorageContainer.php
@@ -120,7 +120,7 @@ class StorageContainer extends \yii\base\Component
     public function getFilesArray()
     {
         if ($this->_filesArray === null) {
-            $this->_filesArray = $this->getQueryCacheHelper((new Query())->from('admin_storage_file')->select(['id', 'is_hidden', 'is_deleted', 'folder_id', 'name_original', 'name_new', 'name_new_compound', 'mime_type', 'extension', 'hash_name', 'upload_timestamp', 'file_size', 'upload_user_id'])->where(['is_deleted' => 0])->indexBy('id'), $this->fileCacheKey);
+            $this->_filesArray = $this->getQueryCacheHelper((new Query())->from('admin_storage_file')->select(['id', 'is_hidden', 'is_deleted', 'folder_id', 'name_original', 'name_new', 'name_new_compound', 'mime_type', 'extension', 'hash_name', 'upload_timestamp', 'file_size', 'upload_user_id'])->indexBy('id'), $this->fileCacheKey);
         }
         
         return $this->_filesArray;
