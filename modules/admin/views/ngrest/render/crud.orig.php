39fe050c843fa01259a8d81ad745e996813ec2cc
Nadar <git@nadar.io>
2016-01-16 15:12:24 +0100

    ngrest crud list minify

--- modules/admin/views/ngrest/render/crud.orig.php
+++ modules/admin/views/ngrest/render/crud.orig.php
@@ -1,201 +1,201 @@
 <script>
-activeWindowCallbackUrl = '<?= $activeWindowCallbackUrl;?>';
-ngrestConfigHash = '<?= $config->getNgRestConfigHash(); ?>';
-zaa.bootstrap.register('<?=$config->getNgRestConfigHash(); ?>', function($scope, $controller) {
-    /* extend class */
-    $.extend(this, $controller('CrudController', { $scope : $scope }));
-    /* local controller config */
-    $scope.config.apiListQueryString = '<?= $crud->apiQueryString('list'); ?>';
-    $scope.config.apiUpdateQueryString = '<?= $crud->apiQueryString('update'); ?>';
-    $scope.config.apiEndpoint = '<?=$config->getRestUrl();?>';
-    $scope.config.list = <?=json_encode($crud->getFields('list'));?>;
-    $scope.config.create = <?=json_encode($crud->getFields('create'));?>;
-    $scope.config.update = <?=json_encode($crud->getFields('update'));?>;
-    $scope.config.ngrestConfigHash = '<?= $config->getNgRestConfigHash(); ?>';
-    $scope.config.activeWindowCallbackUrl = '<?= $activeWindowCallbackUrl;?>';
-});
+    activeWindowCallbackUrl = '<?= $activeWindowCallbackUrl;?>';
+    ngrestConfigHash = '<?= $config->hash; ?>';
+    zaa.bootstrap.register('<?=$config->hash; ?>', function($scope, $controller) {
+        /* extend class */
+        $.extend(this, $controller('CrudController', { $scope : $scope }));
+        /* local controller config */
+        $scope.config.apiListQueryString = '<?= $this->context->apiQueryString('list'); ?>';
+        $scope.config.apiUpdateQueryString = '<?= $this->context->apiQueryString('update'); ?>';
+        $scope.config.apiEndpoint = '<?= $this->context->getRestUrl(); ?>';
+        $scope.config.list = <?= $this->context->getFieldsJson('list'); ?>;
+        $scope.config.create = <?= $this->context->getFieldsJson('create'); ?>;
+        $scope.config.update = <?= $this->context->getFieldsJson('update'); ?>;
+        $scope.config.ngrestConfigHash = '<?= $config->hash; ?>';
+        $scope.config.activeWindowCallbackUrl = '<?= $activeWindowCallbackUrl; ?>';
+        $scope.config.pk = '<?= $this->context->getPrimaryKey(); ?>';
+    });
 </script>
 
-<div ng-controller="<?=$config->getNgRestConfigHash(); ?>" ng-init="init()">
-<?php if ($canCreate): ?>
-<a ng-show="config.create.length" class="btn-floating btn-large waves-effect waves-light right red" style="margin:20px;" ng-click="openCreate()"><i class="mdi-content-add"></i></a>
-<?php endif; ?>
-<div class="card-panel" ng-show="loading">
-    <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-            <div class="circle-clipper left">
-                <div class="circle"></div>
-            </div>
-        </div>
-    </div>
-</div>
+<div ng-controller="<?=$config->hash; ?>" ng-init="init()">
+	<!-- This fake ui-view is used to render the detail item, which actuals uses the parent scope in the ui router controller. -->
+	<div style="visibility:hidden;" ui-view></div>
+    <div ng-switch="crudSwitchType">
 
-<!-- LIST -->
-<div class="card-panel" ng-show="!loading">
-
-    <div class="row">
-        <div class="input-field col s6">
-            <i class="mdi-action-search prefix"></i>
-            <input id="searchString" ng-model="searchString" type="text">
-            <label for="searchString">in der Tabelle <strong>{{currentMenuItem.alias}}</strong> suchen.</label>
-        </div>
-    </div>
-    <div ng-show="deleteErrors.length">
-        <div class="alert alert--danger">
+        <div class="tabs">
             <ul>
-                <li ng-repeat="e in deleteErrors">{{e}}</li>
+                <li class="tabs__item" ng-class="{'tabs__item--active' : crudSwitchType==0}">
+                    <a class="tabs__anchor" ng-click="switchTo(0)"><i class="material-icons tabs__icon">menu</i> <?= \admin\Module::t('ngrest_crud_btn_list'); ?></a>
+                </li>
+
+                <?php if ($canCreate && $config->getPointer('create')): ?>
+                    <li class="tabs__item" ng-class="{'tabs__item--active' : crudSwitchType==1}">
+                        <a class="tabs__anchor" style="" ng-click="switchTo(1)"><i class="material-icons tabs__icon">add_box</i> <?= \admin\Module::t('ngrest_crud_btn_add'); ?></a>
+                    </li>
+                <?php endif; ?>
+                
+                <li ng-show="crudSwitchType==2" class="tabs__item" ng-class="{'tabs__item--active' : crudSwitchType==2}">
+                    <a class="tabs__anchor" ng-click="switchTo(0)"><i class="material-icons tabs__icon">cancel</i> <?= \admin\Module::t('ngrest_crud_btn_close'); ?></a>
+                </li>
             </ul>
         </div>
-    </div>
-    <table class="striped hoverable responsive-table">
-    <thead>
-        <tr>
-            <?php foreach ($crud->list as $item): ?>
-                <th><?= $item['alias']; ?> <i ng-click="changeOrder('<?= $item['name']; ?>', '+')" ng-class="{'active-orderby' : isOrderBy('+<?= $item['name']; ?>') }" class="mdi-hardware-keyboard-arrow-up grid-sort-btn"></i> <i ng-click="changeOrder('<?= $item['name']; ?>', '-')" ng-class="{'active-orderby' : isOrderBy('-<?= $item['name']; ?>') }" class="mdi-hardware-keyboard-arrow-down grid-sort-btn"></i></th>
-            <?php endforeach; ?>
-            <?php if (count($crud->getButtons()) > 0): ?>
-                <th style="text-align:right;"><span class="grid-data-length">{{data.list.length}} Einträge</span></th>
-            <?php endif; ?>
-      </tr>
-    </thead>
-    <tbody>
-        <tr ng-repeat="item in data.list |  filter:searchString | orderBy:orderBy" >
-            <?php foreach ($crud->list as $item): ?>
-                <?php foreach ($crud->createElements($item, $crud::TYPE_LIST) as $element): ?>
-                    <td><?= $element['html']; ?></td>
-                <?php endforeach; ?>
-            <?php endforeach; ?>
-            <?php if (count($crud->getButtons()) > 0): ?>
-            <td style="text-align:right;">
-                <?php foreach ($crud->getButtons() as $item): ?>
-                <a class="waves-effect waves-light btn-flat" ng-click="<?= $item['ngClick']; ?>"><i class="<?=$item['icon']; ?>"></i><?=$item['label']; ?></a>
-                <?php endforeach; ?>
-            </td>
-            <?php endif; ?>
-        </tr>
-    </tbody>
-    </table>
-</div>
 
-<?php if ($canCreate): ?>
-<!-- CREATE MODAL -->
-<div id="createModal" class="modal">
-
-    <!-- MODAL HEADER -->
-    <div class="modal__header">
-        <div class="row">
-            <div class="col s12">
-                <h5>
-                    <i class="mdi-action-note-add"></i> Datensatz hinzufügen
-                    <i class="mdi-navigation-close right" ng-click="closeCreate()"></i>
-                </h5>
-            </div>
-        </div>
-    </div>
-    <!-- /MODAL HEADER -->
-    
-    <form name="formCreate" role="form" ng-submit="submitCreate()">
-    <!-- MODAL CONTENT -->
-    <div class="modal__content">
-        <?php foreach ($crud->create as $k => $item): ?>
-            <div class="row">
-                <?php foreach ($crud->createElements($item, $crud::TYPE_CREATE) as $element): ?>
-                    <?= $element['html']; ?>
-                <?php endforeach; ?>
-            </div>
-        <?php endforeach; ?>
-        
-        <div class="red lighten-2" style="color:white;" ng-show="createErrors.length">
-            <ul>
-                <li ng-repeat="error in createErrors" style="padding:6px;">- {{error.message}}</li>
-            </ul>
+        <div class="langswitch crud__langswitch" ng-if="crudSwitchType!==0">
+            <a ng-repeat="lang in AdminLangService.data" ng-click="AdminLangService.toggleSelection(lang)" ng-class="{'langswitch__item--active' : AdminLangService.isInSelection(lang.short_code)}" class="langswitch__item [ waves-effect waves-blue ][ btn-flat btn--small btn--bold ] ng-binding ng-scope">
+                <span class="flag flag--{{lang.short_code}}">
+                    <span class="flag__fallback">{{lang.name}}</span>
+                </span>
+            </a>
         </div>
         
-    </div>
-    <!-- /MODAL CONTENT -->
-
-    <!-- MODAL FOOTER -->
-    <div class="modal__footer">
-        <div class="row">
-            <div class="col s12">
-                <div class="right">
-                    <button class="btn waves-effect waves-light" type="submit" ng-disabled="createForm.$invalid">
-                        <i class="mdi-content-send"></i> Erstellen
-                    </button>
-                    <button class="btn waves-effect waves-light" type="button" ng-click="closeCreate()">
-                        <i class="mdi-navigation-cancel"></i> Abbrechen
-                    </button>
+        <!-- LIST -->
+        <div class="card-panel" ng-switch-default>
+            
+            
+            <div style="margin-bottom:30px;">
+                <div class="input input--vertical input--text">
+                    <div class="input__field-wrapper">
+                        <input class="input__field" id="searchString" ng-model="searchString" type="text" placeholder="<?= \admin\Module::t('ngrest_crud_search_text'); ?>" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <!-- /MODAL FOOTER -->
-    </form>
-    
-</div>
-<!-- /CREATE MODAL -->
-<?php endif; ?>
+            
 
-<?php if ($canUpdate): ?>
-<!-- UPDATE MODAL -->
-<div id="updateModal" class="modal">
-
-    <!-- MODAL HEADER -->
-    <div class="modal__header">
-        <div class="row">
-            <div class="col s12">
-                <h5>
-                    <i class="mdi-editor-mode-edit"></i> Bearbeiten
-                    <i class="mdi-navigation-close right" ng-click="closeUpdate()"></i>
-                </h5>
-            </div>
-        </div>
-    </div>
-    <!-- /MODAL HEADER -->
-    
-    <form name="formUpdate" role="form" ng-submit="submitUpdate()">
-    <!-- MODAL CONTENT -->
-    <div class="modal__content">
-        <?php foreach ($crud->update as $k => $item): ?>
-            <div class="row">
-                <?php foreach ($crud->createElements($item, $crud::TYPE_UPDATE) as $element): ?>
-                    <?= $element['html']; ?>
-                <?php endforeach; ?>
-            </div>
-        <?php endforeach; ?>
-        
-        <div class="red lighten-2" style="color:white;" ng-show="updateErrors.length">
-            <ul>
-                <li ng-repeat="error in updateErrors" style="padding:6px;">- {{error.message}}</li>
-            </ul>
-        </div>
-    </div>
-    <!-- /MODAL CONTENT -->
-
-    <!-- MODAL FOOTER -->
-    <div class="modal__footer">
-        <div class="row">
-            <div class="col s12">
-                <div class="right">
-                    <button class="btn waves-effect waves-light" type="submit" ng-disabled="updateForm.$invalid">
-                        <i class="mdi-content-send"></i> Speichern
-                    </button>
-                    <button class="btn waves-effect waves-light" type="button" ng-click="closeUpdate()">
-                        <i class="mdi-navigation-cancel"></i> Cancel
-                    </button>
+            <div ng-show="deleteErrors.length">
+                <div class="alert alert--danger">
+                    <ul>
+                        <li ng-repeat="e in deleteErrors">{{e}}</li>
+                    </ul>
                 </div>
             </div>
+            <table class="striped responsive-table hoverable">
+                <thead>
+                    <tr>
+                        <?php foreach ($config->getPointer('list') as $item): ?>
+                            <th><?= $item['alias']; ?> <i ng-click="changeOrder('<?= $item['name']; ?>', '+')" ng-class="{'active-orderby' : isOrderBy('+<?= $item['name']; ?>') }" class="material-icons grid-sort-btn">keyboard_arrow_up</i> <i ng-click="changeOrder('<?= $item['name']; ?>', '-')" ng-class="{'active-orderby' : isOrderBy('-<?= $item['name']; ?>') }" class="material-icons grid-sort-btn">keyboard_arrow_down</i></th>
+                        <?php endforeach; ?>
+                        <?php if (count($this->context->getButtons()) > 0): ?>
+                            <th style="text-align:right;"><span class="grid-data-length">{{data.list.length}} <?= \admin\Module::t('ngrest_crud_rows_count'); ?></span></th>
+                        <?php endif; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="(key, item) in data.list | srcbox:searchString" ng-class="{'crud__item-highlight': isHighlighted(item)}">
+                        <?php foreach ($config->getPointer('list') as $item): ?>
+                            <?php foreach ($this->context->createElements($item, \admin\ngrest\render\RenderCrud::TYPE_LIST) as $element): ?>
+                                <td><?= $element['html']; ?></td>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                        <?php if (count($this->context->getButtons()) > 0): ?>
+                        <td style="text-align:right;">
+                            <?php foreach ($this->context->getButtons() as $item): ?>
+                            <a class="waves-effect waves-light btn-flat btn--bordered" ng-click="<?= $item['ngClick']; ?>"><i class="material-icons<?php if (!empty($item['label'])): ?> left<?php endif; ?>"><?=$item['icon']; ?></i><?=$item['label']; ?></a>
+                            <?php endforeach; ?>
+                        </td>
+                        <?php endif; ?>
+                    </tr>
+                </tbody>
+            </table>
+            <div ng-show="data.list.length == 0" class="alert alert--info"><?= \admin\Module::t('ngrest_crud_empty_row'); ?></div>
         </div>
-    </div>
-    <!-- /MODAL FOOTER -->
-    </form>
+        <!-- /LIST -->
     
-</div>
-<!-- /UPDATE MODAL -->
-<?php endif; ?>
+        <div class="card-panel" ng-switch-when="1" zaa-esc="closeCreate()">
 
-<!-- activeWindow MODAL -->
-<div id="activeWindowModal" class="modal">
-    <div class="modal-content" compile-html ng-bind-html="data.aw.content"></div>
-</div>
+            <?php if ($canCreate && $config->getPointer('create')): ?>
+                <form name="formCreate" role="form" ng-submit="submitCreate()">
+
+                    <!-- MODAL CONTENT -->
+                    <div class="modal__content">
+                        
+                        <?php foreach ($config->getPointer('create') as $k => $item): ?>
+                            <div class="row">
+                                <?php foreach ($this->context->createElements($item, \admin\ngrest\render\RenderCrud::TYPE_CREATE) as $element): ?>
+                                    <?= $element['html']; ?>
+                                <?php endforeach; ?>
+
+                            </div>
+                        <?php endforeach; ?>
+
+                        <div class="red lighten-2" style="color:white;" ng-show="createErrors.length">
+                            <ul>
+                                <li ng-repeat="error in createErrors" style="padding:6px;">- {{error.message}}</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="modal__footer">
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="right">
+                                    <button class="btn waves-effect waves-light" type="submit" ng-disabled="createForm.$invalid">
+                                        <?= \admin\Module::t('ngrest_crud_btn_create'); ?> <i class="material-icons right">check</i>
+                                    </button>
+                                    <button class="btn waves-effect waves-light red" type="button" ng-click="closeCreate()">
+                                        <i class="material-icons left">cancel</i> <?= \admin\Module::t('button_abort'); ?>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            <?php endif; ?>
+        </div>
+    
+        <div class="card-panel" ng-switch-when="2" zaa-esc="closeUpdate()">
+            <?php if ($canUpdate && $config->getPointer('update')): ?>
+                <form name="formUpdate" role="form" ng-submit="submitUpdate()">
+                    <!-- MODAL CONTENT -->
+                    <div class="modal__content">
+                        <?php foreach ($config->getPointer('update') as $k => $item): ?>
+                            <div class="row">
+                                <?php foreach ($this->context->createElements($item, \admin\ngrest\render\RenderCrud::TYPE_UPDATE) as $element): ?>
+                                    <?= $element['html']; ?>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endforeach; ?>
+
+                        <div class="red lighten-2" style="color:white;" ng-show="updateErrors.length">
+                            <ul>
+                                <li ng-repeat="error in updateErrors" style="padding:6px;">- {{error.message}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /MODAL CONTENT -->
+
+                    <!-- MODAL FOOTER -->
+                    <div class="modal__footer">
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="right">
+                                    <button class="btn waves-effect waves-light" type="submit" ng-disabled="updateForm.$invalid">
+                                        <?= \admin\Module::t('button_save'); ?> <i class="material-icons right">check</i>
+                                    </button>
+                                    <button class="btn waves-effect waves-light red" type="button" ng-click="closeUpdate()">
+                                        <i class="material-icons left">cancel</i> <?= \admin\Module::t('button_abort'); ?>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /MODAL FOOTER -->
+                </form>
+            <?php endif; ?>
+        </div>
+
+    </div>
+
+    <!-- activeWindow MODAL -->
+    <div ng-show="activeWindowModal" class="modal__wrapper" zaa-esc="closeActiveWindow()">
+        <div class="modal">
+            <button class="btn waves-effect waves-light modal__close btn-floating red" type="button" ng-click="closeActiveWindow()">
+                <i class="material-icons">close</i>
+            </button>
+            <div class="modal-content" compile-html ng-bind-html="data.aw.content"></div>
+        </div>
+        <div class="modal__background"></div>
+    </div>
+
 </div>
