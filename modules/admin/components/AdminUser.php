411f20cd47de9552404552107ce3d0e6cf1c9298
nadar <git@nadar.io>
2016-02-01 17:36:48 +0100

    changed id param of admin user (fixed bug with different users), fixed issue with login url

--- modules/admin/components/AdminUser.php
+++ modules/admin/components/AdminUser.php
@@ -13,12 +13,14 @@ class AdminUser extends \yii\web\User
 {
     public $identityClass = '\admin\models\User';
 
-    public $loginUrl = 'admin/login';
+    public $loginUrl = ['/admin/login/index'];
 
     public $identityCookie = ['name' => '_adminIdentity', 'httpOnly' => true];
 
     public $enableAutoLogin = false;
 
+    public $idParam = '__luya_adminId';
+    
     public function init()
     {
         parent::init();
