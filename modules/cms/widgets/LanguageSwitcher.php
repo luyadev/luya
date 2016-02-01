7ee7785d53d29cd10467dd971ad4781829ea83f5
nadar <git@nadar.io>
2016-02-01 15:37:03 +0100

    added language switcher docu #755

--- modules/cms/widgets/LanguageSwitcher.php
+++ modules/cms/widgets/LanguageSwitcher.php
@@ -6,6 +6,34 @@ use Yii;
 use admin\models\Lang;
 use luya\web\Composition;
 
+/**
+ * Language Switcher for CMS
+ * 
+ * This widget will find all registered languages and display the corresponding like to the
+ * provided languages, if there as no like it will link to the home page of this language. The
+ * language switcher can event detect composition url rules for other languages based on the 
+ * current menu item.
+ * 
+ * You can always extend from this class and other proprties you can access trough the `$this->context`
+ * variable inside your view.
+ * 
+ * The following code is an example of how the template for this widget could look like:
+ * 
+ * ```html
+ * <div class="langnav">
+ *   <ul class="langnav__list">
+ *   <? foreach($items as $item): ?>
+ *       <li class="langnav__item">
+ *       	<a class="langnav__link <? if ($item['isCurrent']): ?>langnav__link--current<?endif; ?>" href="<?= $item['href']; ?>"><?= $item['language']['short_code']; ?></a>
+ *       </li>
+ *   <? endforeach; ?>
+ *   </ul>
+ * </div>
+ * ```
+ * 
+ * @author nadar
+ * @since 1.0.0-beta5
+ */
 class LanguageSwitcher extends \luya\base\Widget
 {
     public function run()
