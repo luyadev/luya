154fe235ff8766ae71a2435a5051a1e055c2507d
nadar <git@nadar.io>
2016-02-02 14:55:56 +0100

    Fixed bug where is_hidden state of file upload has not be assigned due to model validation error. closes #745

--- modules/admin/models/StorageFile.php
+++ modules/admin/models/StorageFile.php
@@ -22,6 +22,7 @@ class StorageFile extends \yii\db\ActiveRecord
         return [
             [['name_original', 'name_new', 'mime_type', 'name_new_compound', 'extension', 'hash_file', 'hash_name'], 'required'],
             [['folder_id', 'upload_timestamp', 'file_size', 'upload_user_id', 'upload_timestamp'], 'safe'],
+            [['is_hidden'], 'integer']
         ];
     }
     
