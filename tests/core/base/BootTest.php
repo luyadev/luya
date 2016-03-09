6cd4dc116f2cb9194224b6ca94c9d0f8c5abfb17
nadar <git@nadar.io>
2016-03-09 09:54:09 +0100

    added luya boot tests

---
+++ tests/core/base/BootTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace tests\core\base;
+
+use luya\Boot;
+
+class BootTest extends \tests\LuyaWebTestCase
+{
+    public function testBaseObject()
+    {
+        $boot = new Boot();
+        $this->assertEquals('cli', $boot->getSapiName());
+        
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testUnableToFindConfigFile()
+    {
+        $boot = new Boot();
+        $boot->run();
+    }
+    
+    /**
+     * @expectedException Exception
+     */
+    public function testWrongConfigFile()
+    {
+        $boot = new Boot();
+        $boot->configFile = __DIR__ . '/../../data/configs/wrongconfig.php';
+        $boot->run();
+    }
+    
+    /**
+     * @expectedException Exception
+     */
+    public function testYiiNotFound()
+    {
+        $boot = new Boot();
+        $boot->configFile = __DIR__ . '/../../data/configs/console.php';
+        $boot->setYiiPath('wrongPathToYii.php');
+        $boot->mockOnly = true;
+        $boot->applicationConsole();
+    }
+}
