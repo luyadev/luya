3ba3f80e493c76dd817162826bd1199b5e35b95d
nadar <git@nadar.io>
2016-03-10 10:19:47 +0100

    removed base exception test

--- tests/core/traits/ErrorHandlerTest.php
+++ tests/core/traits/ErrorHandlerTest.php
@@ -8,34 +8,23 @@ use luya\console\ErrorHandler;
 
 class ErrorHandlerTest extends LuyaWebTestCase
 {
-    public function testRenderException()
-    {
-        $stud = new ErrorHandler();
-        try {
-            $e = $stud->renderException(new Exception('foobar'));
-        } catch (Exception $err) {
-            $this->assertEquals('Error: foobar', $err->getMessage());
-        }
-    }
-
     public function testExceptionTrace()
     {
-        
         try {
-        $stud = new ErrorHandler();
-        $response = $stud->getExceptionArray(new Exception('foobar'));
-        
-        $this->assertArrayHasKey('message', $response);
-        $this->assertArrayHasKey('file', $response);
-        $this->assertArrayHasKey('line', $response);
-        $this->assertArrayHasKey('requestUri', $response);
-        $this->assertArrayHasKey('serverName', $response);
-        $this->assertArrayHasKey('date', $response);
-        $this->assertArrayHasKey('trace', $response);
-        $this->assertArrayHasKey('ip', $response);
-        $this->assertArrayHasKey('get', $response);
-        $this->assertArrayHasKey('post', $response);
-        $this->assertTrue(is_Array($response['trace']));
+            $stud = new ErrorHandler();
+            $response = $stud->getExceptionArray(new Exception('foobar'));
+            
+            $this->assertArrayHasKey('message', $response);
+            $this->assertArrayHasKey('file', $response);
+            $this->assertArrayHasKey('line', $response);
+            $this->assertArrayHasKey('requestUri', $response);
+            $this->assertArrayHasKey('serverName', $response);
+            $this->assertArrayHasKey('date', $response);
+            $this->assertArrayHasKey('trace', $response);
+            $this->assertArrayHasKey('ip', $response);
+            $this->assertArrayHasKey('get', $response);
+            $this->assertArrayHasKey('post', $response);
+            $this->assertTrue(is_Array($response['trace']));
         } catch(Exception $e) {
             $this->assertEquals('Error: foobar', $err->getMessage());
         }
