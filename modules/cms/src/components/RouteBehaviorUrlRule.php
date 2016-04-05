9b7cef13aa192d406ff69f87863db4fb85bef6ba
nadar <git@nadar.io>
2016-04-05 14:35:56 +0200

    changed yii info order

--- modules/cms/src/components/RouteBehaviorUrlRule.php
+++ modules/cms/src/components/RouteBehaviorUrlRule.php
@@ -17,15 +17,15 @@ class RouteBehaviorUrlRule extends \yii\web\UrlRule
     
     public function parseRequest($manager, $request)
     {
-        // add trace info
-        Yii::info('LUYA-CMS RouteBehaviorUrlRule is parsing the Request for path info \'' . $request->pathInfo .'\'', __METHOD__);
         // return the custom route
-        
         $parts = explode("/", $request->pathInfo);
         
         if (!isset($parts[0]) || !Yii::$app->hasModule($parts[0]) || $parts[0] === 'cms') {
             return false;   
         }
+        
+        // add trace info
+        Yii::info('LUYA-CMS RouteBehaviorUrlRule is parsing the Request for path info \'' . $request->pathInfo .'\'', __METHOD__);
         
         return parent::parseRequest($manager, $request);
     }
