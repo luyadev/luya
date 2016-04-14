2db92fbc6ab1f3bfcaee02eb5918e893759034c8
nadar <git@nadar.io>
2016-04-14 16:05:04 +0200

    added error action for console command

--- core/console/ErrorHandler.php
+++ core/console/ErrorHandler.php
@@ -10,4 +10,10 @@ namespace luya\console;
 class ErrorHandler extends \yii\console\ErrorHandler
 {
     use \luya\traits\ErrorHandler;
+
+    /**
+     * @var streing This propertie has been added in order to make sure console commands config
+     * does also work in console env.
+     */
+    public $errorAction = null;
 }
