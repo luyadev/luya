e23f2c012a49c1c519c4692a8fe3fa72cdc055d4
Nadar <git@nadar.io>
2016-05-03 11:31:45 +0200

    remved new helper method as its not well desgined in case of the context rendering.

--- core/helpers/ModuleHelper.php
+++ core/helpers/ModuleHelper.php
@@ -12,35 +12,7 @@ use luya\Exception;
  * @author Basil Suter <basil@nadar.io>
  */
 class ModuleHelper
-{
-	/**
-	 * Run a route of a module. The module will retrieved and the controller action
-	 * paramters will used to load the right action.
-	 * 
-	 * @param string $route The route you want to run `<module>/<controller>/<action>`
-	 * @param array $params Optional parameters for the action, the key is the method argument name.
-	 * @throws Exception
-	 * 
-	 * @since 1.0.0-beta7
-	 */
-	public static function runRoute($route, array $params = [])
-	{
-		$parts = explode("/", $route);		
-		
-		$module = Yii::$app->getModule($parts[0]);
-		$controller = (isset($parts[1])) ? $parts[1] : 'default';
-		$action = (isset($parts[2])) ? $parts[2] : 'index';
-		
-		if ($module) {
-			$module->context = 'cms';
-			$object = static::reflectionObject($module);
-			$object->defaultRoute($controller, $action, $params);
-			return $object->run();
-		}
-		
-		throw new Exception("Unable to find module '{$parts[0]}'");
-	}
-	
+{	
     /**
      * Create module reflection object based on a luya module.
      * 
