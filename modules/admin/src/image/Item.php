bbeda21dcf26ac966b1a8dd710ae25a36f5371f6
Basil Suter <git@nadar.io>
2016-05-10 14:48:02 +0200

    added getter/setter for caption property, added phpdocs

--- modules/admin/src/image/Item.php
+++ modules/admin/src/image/Item.php
@@ -4,15 +4,58 @@ namespace admin\image;
 
 use Yii;
 
+/**
+ * Image Item Detail Object.
+ * 
+ * Each image is represent as Item-Object class.
+ * 
+ * @property string $caption Image Caption
+ * @property integer $id The unique image identifier number.
+ * @property integer $fileId The file id where this image depends on.
+ * @property integer $filterId The applied filter id for this image
+ * @property string $source The source of the image where you can access the image by the web.
+ * @property string $serverSource The source to the image internal used on the Server.
+ * @property boolean $fileExists Return boolean whether the file server source exsits on the server or not.
+ * @property string $resolutionWidth Get the image resolution width.
+ * @property string $resolutionHeight Get the image resolution height.
+ * @property \admin\file\Item $file The file object where the image was created from.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ */
 class Item extends \yii\base\Object
 {
     use \admin\storage\ItemTrait;
     
     private $_file = null;
     
+    private $_caption = null;
+    
     /**
+     * Set caption for image item, override existings values
      * 
-     * @return mixed
+     * @param string $text The caption text for this image
+     * @since 1.0.0-beta7
+     */
+    public function setCaption($text)
+    {
+        $this->_caption = trim($text);
+    }
+    
+    /**
+     * Return the caption text for this image, if not defined or none give its null
+     * 
+     * @return string The caption text for this image
+     * @since 1.0.0-beta7
+     */
+    public function getCaption()
+    {
+        return $this->_caption;
+    }
+    
+    /**
+     * The unique image identifier number.
+     * 
+     * @return integer
      */
     public function getId()
     {
@@ -20,8 +63,9 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * The file id where this image depends on.
      * 
-     * @return mixed
+     * @return integer
      */
     public function getFileId()
     {
@@ -29,7 +73,9 @@ class Item extends \yii\base\Object
     }
 
     /**
+     * The applied filter id for this image
      * 
+     * @return integer
      */
     public function getFilterId()
     {
@@ -37,6 +83,7 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * The source of the image where you can access the image by the web.
      * 
      * @return string|boolean
      */
@@ -52,6 +99,7 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * The source to the image internal used on the Server.
      * 
      * @return string|boolean
      */
@@ -61,6 +109,7 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Return boolean value whether the file server source exsits on the server or not.
      * 
      * @return boolean
      */
@@ -70,6 +119,7 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Get the image resolution width.
      * 
      * @return mixed
      */
@@ -79,6 +129,7 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Get the image resolution height.
      * 
      * @return mixed
      */
@@ -88,7 +139,9 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Get image depending file object where the image was create from, its like the original Source
      * 
+     * @return \admin\file\Item
      */
     public function getFile()
     {
@@ -100,9 +153,10 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Apply a new filter for the original ussed file and return the new created image object.
      * 
-     * @param unknown $filterName
-     * @return boolean
+     * @param string $filterName The name of a filter like `tiny-thumbnail` or a custom filter you have defined in your filters list.
+     * @return boolean|\admin\image\Item Returns boolean or image item object if its found.
      */
     public function applyFilter($filterName)
     {
@@ -110,8 +164,9 @@ class Item extends \yii\base\Object
     }
     
     /**
+     * Get the image object informations as array.
      * 
-     * @return string[]|boolean[]|mixed[]
+     * @return array 
      */
     public function toArray()
     {
