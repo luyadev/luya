42eb733c0632b1a5017ec6a693055139fe1784c7
nadar <git@nadar.io>
2016-05-23 21:38:29 +0200

    preview offline pages by inject menu item #838

--- modules/cms/src/controllers/PreviewController.php
+++ modules/cms/src/controllers/PreviewController.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
 use cmsadmin\models\NavItem;
+use cms\menu\InjectItem;
 
 /**
  * CMS Preview Rendering
@@ -20,14 +21,30 @@ class PreviewController extends \cms\base\Controller
             throw new ForbiddenHttpException('Unable to see the preview page, session expired or not logged in.');
         }
 
-        $langShortCode = NavItem::findOne($itemId)->lang->short_code;
+        $navItem = NavItem::findOne($itemId);
+        $langShortCode = $navItem->lang->short_code;
 
         Yii::$app->composition['langShortCode'] = $langShortCode;
 
         $item = Yii::$app->menu->find()->where(['id' => $itemId])->with('hidden')->lang($langShortCode)->one();
 
+        // this item is still offline so we have to inject and fake it with the inject api
         if (!$item) {
-            throw new NotFoundHttpException("Unable to find the preview for this ID, maybe the page is still Offline?");
+        	// create new item to inject
+        	$inject = new InjectItem([
+        		'childOf' => Yii::$app->menu->home->id,
+        		'title' => $navItem->title,
+        		'alias' => $navItem->alias,
+        		'isHidden' => true,
+        	]);
+        	// inject item into menu component
+        	Yii::$app->menu->injectItem($inject);
+        	// find the inject menu item
+        	$item = Yii::$app->menu->find()->where(['id' => $inject->id])->with('hidden')->lang($langShortCode)->one();
+        	// something really went wrong while finding injected item
+        	if (!$item) {
+        		throw new NotFoundHttpException("Unable to find the preview for this ID, maybe the page is still Offline?");
+        	}
         }
 
         // set the current item, as it would be resolved wrong from the url manager / request path
