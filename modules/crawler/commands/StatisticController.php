b0de57130e248a048edc4117c1f5a26d0763456a
Basil Suter <git@nadar.io>
2016-05-25 10:05:46 +0200

    enhanced crawler stats mail. closes #845

--- modules/crawler/commands/StatisticController.php
+++ modules/crawler/commands/StatisticController.php
@@ -7,6 +7,17 @@ use luya\console\Command;
 use crawleradmin\models\Searchdata;
 use yii\console\Exception;
 
+/**
+ * Send E-Mail of search statistic for last 7 days.
+ * 
+ * Console command use:
+ * 
+ * ```
+ * ./vendor/bin/luya crawler/statistic
+ * ```
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ */
 class StatisticController extends Command
 {
     public function actionIndex()
@@ -32,23 +43,30 @@ class StatisticController extends Command
             }
         }
         
-        
-        $html = '<h1>Search Analtics from '. date("d.m.Y", $lastWeek) . ' until '.date("d.m.Y", time()).'</h1>';
+        $html = '<h1>Search analytics from '. date("d.m.Y", $lastWeek) . ' until '.date("d.m.Y", time()).'</h1>';
+        $html.= '<p>Page: ' . Yii::$app->siteTitle.'</p>';
         $html.= '<p>Number of searches: <strong>'.count($results).'</strong></p>';
-        $html.= '<p>Querys without results:</p><table border="1">';
-        foreach ($data['noresults'] as $item) {
-            $html.= '<tr><td>'.$item['query'].'</td><td>'.$item['language'].'</td><td>'. date("d.m.Y", $item['timestamp']).'</td></tr>';
+        
+        if (!empty($data['noresults'])) {
+            $html.= '<p>Querys <b>without</b> results:</p>';
+            $html.= '<table border="1" cellpadding="4" cellspacing="0"><tr><td>Query</td><td>Language</td><td>Date</td></tr>';
+            foreach ($data['noresults'] as $item) {
+                $html.= '<tr><td>'.$item['query'].'</td><td>'.$item['language'].'</td><td>'. date("d.m.Y", $item['timestamp']).'</td></tr>';
+            }
+            $html.='</table>';
         }
-        $html.='</table>';
         
-        $html.= '<p>Querys with results:</p><table border="1">';
-        foreach ($data['results'] as $item) {
-            $html.= '<tr><td>'.$item['query'].'</td><td>'.$item['results'].'</td><td>'.$item['language'].'</td><td>'. date("d.m.Y", $item['timestamp']).'</td></tr>';
+        if (!empty($data['results'])) {
+            $html.= '<p>Querys <b>with</b> results:</p>';
+            $html.= '<table border="1" cellpadding="4" cellspacing="0"><tr><td>Query</td><td>Results</td><td>Language</td><td>Date</td></tr>';
+            foreach ($data['results'] as $item) {
+                $html.= '<tr><td>'.$item['query'].'</td><td>'.$item['results'].'</td><td>'.$item['language'].'</td><td>'. date("d.m.Y", $item['timestamp']).'</td></tr>';
+            }
+            $html.='</table>';
         }
-        $html.='</table>';
         
-        Yii::$app->mail->compose('Search Analtics from '. date("d.m.Y", $lastWeek) . ' until '.date("d.m.Y", time()), $html)->addresses($this->module->statisticRecipients)->send();
+        Yii::$app->mail->compose('[' . Yii::$app->siteTitle.'] Search analytics from '. date("d.m.Y", $lastWeek) . ' until '.date("d.m.Y", time()), $html)->addresses($this->module->statisticRecipients)->send();
         
-        return $this->outputSuccess('statis sent!');
+        return $this->outputSuccess('Search analytics mail sent');
     }
 }
