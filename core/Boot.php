a284f0fa2407cc8b95b08751f9ab2bbd28b5c44a
Basil Suter <git@nadar.io>
2016-05-25 09:39:33 +0200

    allo failure test of ph php 5.5

--- core/Boot.php
+++ core/Boot.php
@@ -3,7 +3,7 @@
 namespace luya;
 
 /*
- * PHP Version 5.5 is not active tested anymore, but still works but its not supported.  
+ * We have skipped the active support/testing for PHP 5.5 and recommend PHP 5.6.
  */
 if (!version_compare(PHP_VERSION, '5.5.0', '>=')) {
     trigger_error('Some functions of LUYA need php version 5.5.0 or higher! You are currently using Version: '.PHP_VERSION, E_USER_ERROR);
