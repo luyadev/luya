53873f66651cb66ee5423c361aa976f568fbb9f1
Nadar <git@nadar.io>
2016-06-17 21:20:18 +0200

    added publish only options to asset in kickstater bundle #882

--- envs/kickstarter/assets/ResourcesAsset.php
+++ envs/kickstarter/assets/ResourcesAsset.php
@@ -6,11 +6,22 @@ class ResourcesAsset extends \luya\web\Asset
 {
     public $sourcePath = '@app/resources';
     
+    public $js = [
+    	'bootstrap/bootstrap.min.js',
+    ];
+    
     public $css = [
-        'bootstrap/css/bootstrap.min.css',
-        'bootstrap/css/bootstrap-theme.min.css',
+        'bootstrap/bootstrap.min.css',
+        'bootstrap/bootstrap-theme.min.css',
         '//fonts.googleapis.com/css?family=Open+Sans:300,400,600',
         '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
         'css/style.css'
+    ];
+    
+    public $publishOptions = [
+    	'only' => [
+    		'bootstrap/*',
+    		'css/*',
+    	]
     ];
 }
