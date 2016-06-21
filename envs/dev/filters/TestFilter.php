35a899f4e2e2bf51ad27ad292c7ea30e0e85a614
Basil Suter <git@nadar.io>
2016-06-21 08:46:06 +0200

    added envs dev test filter

---
+++ envs/dev/filters/TestFilter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\filters;
+
+use admin\base\Filter;
+use Imagine\Image\ManipulatorInterface;
+
+class TestFilter extends Filter
+{
+    public function identifier()
+    {
+        return 'test-filter';
+    }
+    
+    public function name()
+    {
+        return 'Test Filter';
+    }
+    
+    public function chain()
+    {
+        return [
+            [self::EFFECT_THUMBNAIL, ['width' => 150, 'height' => 70, 'mode' => ManipulatorInterface::THUMBNAIL_OUTBOUND, 'saveOptions' => ['quality' => 80]]],
+            //[self::EFFECT_CROP, ['width' => 100, 'height' => 100]],
+        ];
+    }
+}
