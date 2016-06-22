b155d360f678220348e36e688f960626876573b2
Basil Suter <git@nadar.io>
2016-06-22 13:19:33 +0200

    added angular helper class equals to Html helper class with angular tag. #905

--- modules/admin/src/helpers/Angular.php
+++ modules/admin/src/helpers/Angular.php
@@ -5,15 +5,22 @@ namespace admin\helpers;
 use yii\helpers\Html;
 
 /**
- * Helper Method to create admin angular directive inputs
+ * Helper Method to create angular tags.
+ * 
+ * The LUYA admin provides some default angular directives which are prefixed with `zaa`. In order to create custom
+ * NgRest Plugins sometimes you may want to reuse those. There is also a helper method called `directive` which 
+ * allows you the quickly generate a Html Tag for directives.
  * 
  * @author Basil Suter <basil@nadar.io>
  * @since 1.0.0-beta8
  */
-class AngularInput
+class Angular
 {
 
     /**
+     * Internal method to use to create the angular injector helper method like in angular context of directives.js
+     * 
+     * ```
      * "dir": "=",
      * "model": "=",
      * "options": "=",
@@ -23,6 +30,13 @@ class AngularInput
      * "fieldname": "@fieldname",
      * "placeholder": "@placeholder",
      * "initvalue": "@initvalue"
+     * ```
+     * 
+     * @param string $type
+     * @param string $ngModel
+     * @param string $label
+     * @param array $options
+     * @param array $mergeOptions Additonal attributes to be set for the tag $type.
      */
     protected static function injector($type, $ngModel, $label, $options = [], array $mergeOptions = [])
     {
@@ -32,34 +46,92 @@ class AngularInput
             'options' => $options,
         ]));
     }
+
+    /**
+     * Create a Angular Directive tag based on the name.
+     * 
+     * ```php
+     * Angular::directive('my-input', 'name');
+     * ```
+     * 
+     * would produce the my input directive tag:
+     * 
+     * ```html
+     * <my-input ng-model="name"></my-input>
+     * ```
+     * 
+     * @param unknown $name
+     * @param unknown $ngModel
+     * @param array $options
+     */
+    public static function directive($name, $ngModel, array $options = [])
+    {
+        return Html::tag($name, null, array_merge($options, ['ng-model' => $ngModel]));
+    }
     
+    /**
+     * zaaSortRelationArray directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $sourceData
+     * @param array $options
+     */
     public static function zaaSortRelationArray($ngModel, $label, array $sourceData, array $options = [])
     {
         return self::injector('zaa-sort-relation-array', $ngModel, $label, ['sourceData' => $sourceData], $options);
     }
         
+    /**
+     * zaaText directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaText($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-text', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaTextarea directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaTextarea($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-textarea', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaNumber directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaNumber($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-number', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaDecimal directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaDecimal($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-decimal', $ngModel, $label, [], $options);
     }
     
     /**
-     * Select Option
+     * Select directive
      * 
      * ```
      * return AngularInput::zaaSelect($ngModel, $this->alias, [['value' => 123, 'label' => 123123]]);
@@ -75,13 +147,20 @@ class AngularInput
         return self::injector('zaa-select', $ngModel, $label, $data, $options);
     }
     
+    /**
+     * zaaCheckbox directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaCheckbox($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-checkbox', $ngModel, $label, [], $options);
     }
     
     /**
-     * Checkbox Array
+     * zaaCheckboxArray directive
      * 
      * ```
      * AngularInput::zaaCheckboxArray($ngModel, $this->alias, [['value' => 123, 'label' => 123123], ['value' => 'A', 'label' => 'BCZ']]);
@@ -97,41 +176,97 @@ class AngularInput
         return self::injector('zaa-checkbox-array', $ngModel, $label, ['items' => $data], $options);
     }
     
+    /**
+     * zaaDate directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaDate($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-date', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaDatetime directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaDatetime($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-datetime', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaTable directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaTable($ngModel, $label, array $options = [])
     {
         return self::injector('zaa-table', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaListArray directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaListArray($ngModel, $label, array $options = [])
     {
         //return self::injector('zaa-table', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaFileArrayUpload directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaFileArrayUpload($ngModel, $label, array $options = [])
     {
         //return self::injector('zaa-table', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaImageArrayUpload directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaImageArrayUpload($ngModel, $label, array $options = [])
     {
         //return self::injector('zaa-table', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaImageUpload directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaImageUpload($ngModel, $label, array $options = [])
     {
         //return self::injector('zaa-table', $ngModel, $label, [], $options);
     }
     
+    /**
+     * zaaFileUpload directive
+     * 
+     * @param unknown $ngModel
+     * @param unknown $label
+     * @param array $options
+     */
     public static function zaaFileUpload($ngModel, $label, array $options = [])
     {
         //return self::injector('zaa-table', $ngModel, $label, [], $options);
