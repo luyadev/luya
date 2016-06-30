ac81fc6367cf1c63bea4c3604526cda44bedf4d8
Basil <git@nadar.io>
2016-06-30 22:23:35 +0200

    Update m160629_092417_cmspermissiontable.php

--- modules/cmsadmin/src/migrations/m160629_092417_cmspermissiontable.php
+++ modules/cmsadmin/src/migrations/m160629_092417_cmspermissiontable.php
@@ -4,16 +4,17 @@ use yii\db\Migration;
 
 class m160629_092417_cmspermissiontable extends Migration
 {
-    // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
         $this->createTable('cms_nav_permission', [
             'group_id' => 'int(11) NOT NULL',
             'nav_id' => 'int(11) NOT NULL',
+            'inheritance' => 'tinyint(1) default 0',
         ]);
     }
 
     public function safeDown()
     {
+        $this->dropTable('cms_nav_permission');
     }
 }
