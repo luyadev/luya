2e391cf169ec2ada9232d9099e989fae375c0943
Basil Suter <git@nadar.io>
2016-07-05 13:15:44 +0200

    float validator update

--- core/validators/FloatValidator.php
+++ core/validators/FloatValidator.php
@@ -9,14 +9,23 @@ use yii\validators\Validator;
  * 
  * @author Basil Suter <basil@nadar.io>
  * @author Martin Petrasch <martin.petrasch@zephir.ch>
+ * @since 1.0.0-beta8
  */
 class FloatValidator extends Validator
 {
+    public $message = '{attribute} must be a float or numeric value.';
+    
+    /**
+     * Validate the value if is_numeric or if not is_float
+     * {@inheritDoc}
+     * @see \yii\validators\Validator::validateAttribute()
+     */
     public function validateAttribute($model, $attribute)
     {
-        if (!is_numeric($attribute)) {
-            if (!is_float($model->$attribute)) {
-                return $model->addError($attribute, "Value must be float.");
+        $value = $model->$attribute;
+        if (!is_numeric($value)) {
+            if (!is_float($value)) {
+                return $model->addError($attribute, Yii::t('yii', $this->message, ['attribute' => $model->getAttributeLabel($attribute)]));
             }
         }
     }
