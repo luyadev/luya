4efbd64d5d7f2d53dc38aa19ede3ece1843598f4
Basil <git@nadar.io>
2016-08-03 15:17:15 +0200

    Update SoftDeleteTrait.php

--- modules/admin/src/traits/SoftDeleteTrait.php
+++ modules/admin/src/traits/SoftDeleteTrait.php
@@ -8,26 +8,28 @@ namespace admin\traits;
  * To override a match your custom field desribers (describers which is deleted and find state) you
  * can override the static `FieldStateDescriber()` method as described in the description of the method.
  * 
- * @author nadar
+ * @author Basil Suter <basil@nadar.io>
  */
 trait SoftDeleteTrait
 {
     /**
+     * Describes the fields shoudl be used to the perform the soft deleteion tasks.
      * 
+     * Examples value of the method:
+     * 
+     * ```php
+     * return [
+     *     'is_deleted' => [1, 0], // on delete sets `is_deleted = 1`; on find add where `where(['is_deleted' => 0]);`.
+     *     'is_inactive' => true, // on delete sets `is_inactive = true`; on find add where `where([is_inactive' => !true]);`.
+     * ];
+     * ```
+     * 
+     * @todo rename to fieldStateDescriber!
      * @return array Returns an array with a key which desribes what field should be updated on delete and observed on find,
      * the value for the corresponding field can be an array or a string/numeric
      * 
      * - array: The first value is the value for the delete command, the second for the find where
      * - string/nummeric: The value will be inverted with "!" opposite operator, this can lead into problems
-     * 
-     * Examples:
-     * 
-     * ```
-     * [
-     *     'is_deleted' => [1, 0], // on delete sets `is_deleted = 1`; on find add where `where(['is_deleted' => 0]);`.
-     *     'is_inactive' => true, // on delete sets `is_inactive = true`; on find add where `where([is_inactive' => !true]);`.
-     * ]
-     * ```
      * 
      * if you want to override the default implemenation to match your custom models you should always use the former type of state description.
      */
@@ -38,7 +40,12 @@ trait SoftDeleteTrait
         ];
     }
 
-    private static function internalAndWhere()
+    /**
+     * This method will be used to performe the where querys of `ngrestFind()` and `find()` of the ActiveRecord.
+     * 
+     * @return array Returns an array where the key is the field and value the correspoinding value for there where statments.
+     */
+    public static function internalAndWhere()
     {
         $query = [];
         
@@ -49,6 +56,30 @@ trait SoftDeleteTrait
         return $query;
     }
 
+    /**
+     * Overrides the ngRestFind() method of the ActiveRecord
+     */
+    public static function ngRestFind()
+    {
+        $where = static::internalAndWhere();
+        return (empty($where)) ? parent::ngrestFind() : parent::ngRestFind()->andWhere($where);
+    }
+    
+    /**
+     * Overrides the find() method of the ActiveRecord
+     */
+    public static function find()
+    {
+        $where = static::internalAndWhere();
+        return (empty($where)) ? parent::find() : parent::find()->andWhere($where);
+    }
+
+    public function delete()
+    {
+        $this->updateAttributes(static::internalUpdateValues());
+        return true;
+    }
+    
     private static function internalUpdateValues()
     {
         $update = [];
@@ -56,21 +87,5 @@ trait SoftDeleteTrait
             $update[$field] = (is_array($value)) ? $value[0] : $value;
         }
         return $update;
-    }
-    
-    public static function ngRestFind()
-    {
-        return parent::ngRestFind()->andWhere(static::internalAndWhere());
-    }
-    
-    public static function find()
-    {
-        return parent::find()->andWhere(static::internalAndWhere());
-    }
-
-    public function delete()
-    {
-        $this->updateAttributes(static::internalUpdateValues());
-        return true;
     }
 }
