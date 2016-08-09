b67ab3a73e82b9aa755f5ee777386c51c31361a6
Marc Stampfli <marc.stampfli@zephir.ch>
2016-08-09 17:37:43 +0200

    Updated permissions view

--- modules/cmsadmin/src/apis/MenuController.php
+++ modules/cmsadmin/src/apis/MenuController.php
@@ -11,7 +11,7 @@ use cmsadmin\models\NavContainer;
 
 /**
  * Menu Api provides commont tasks to retrieve cmsadmin menu data and cms group permissions setting tasks.
- * 
+ *
  * @author Basil Suter <basil@nadar.io>
  */
 class MenuController extends \admin\base\RestController
@@ -25,7 +25,7 @@ class MenuController extends \admin\base\RestController
             'hiddenCats' => ArrayHelper::typeCast(Yii::$app->adminuser->identity->setting->get("togglecat", [])),
         ];
     }
-    
+
     /**
      * Returns the full tree with groups, pages, is_inherit or not, does have rights or not as it was to hard to
      * implement does features directly with angular, now we just prepare anything withing php and delivers to angular only to display the data.
@@ -35,9 +35,11 @@ class MenuController extends \admin\base\RestController
         $data = [];
         // collect data
         foreach (NavContainer::find()->with('navs')->all() as $container) {
-            $data[] = [
-                'container' => $container,
-                'items' => $this->getItems($container),
+            $this->getItems($container);
+
+            $data['containers'][] = [
+                'containerInfo' => $container,
+                'items' => self::$_permissionItemData[$container->id],
             ];
         }
         // collect group informations
@@ -51,52 +53,54 @@ class MenuController extends \admin\base\RestController
         // return array with full data
         return $data;
     }
-    
+
     private function groupHasFullPermission(Group $group)
     {
         $count = (new Query())->select("*")->from("cms_nav_permission")->where(['group_id' => $group->id])->count();
-        
+
         if ($count > 0) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     private $_groups = null;
-    
+
     private function getGroups()
     {
         if ($this->_groups === null) {
             $this->_groups = Group::find()->all();
         }
-        
+
         return $this->_groups;
     }
-    
-    private function getItems(NavContainer $container, $parentNavId = 0, $parentGroup = [])
+
+    private static $_permissionItemData = [];
+
+    private function getItems(NavContainer $container, $parentNavId = 0, $parentGroup = [], $index = 1)
     {
         $navs = $container->getNavs()->andWhere(['parent_nav_id' => $parentNavId])->all();
-        
+
         $data = [];
-        
+
         foreach ($navs as $nav) {
             $array = $nav->toArray();
-            
+
             if (empty($nav->activeLanguageItem)) {
                 continue;
             }
             $array['title'] = $nav->activeLanguageItem->title;
-            
+
             foreach ($this->getGroups() as $key => $group) {
                 $isInheritedFromParent = false;
-                
+
                 if (isset($parentGroup[$key])) {
                     if ($parentGroup[$key]['isGroupPermissionInheritNode'] || $parentGroup[$key]['isInheritedFromParent']) {
                         $isInheritedFromParent = true;
                     }
                 }
-                
+
                 $array['groups'][$key] = [
                     'id' => $group->id,
                     'isGroupPermissionInheritNode' => $nav->isGroupPermissionInheritNode($group),
@@ -105,66 +109,72 @@ class MenuController extends \admin\base\RestController
                     'permissionCheckbox' => $nav->hasGroupPermissionSelected($group),
                     'groupFullPermission' => $this->groupHasFullPermission($group),
                 ];
+
+
             }
-            
-            $array['__children'] = $this->getItems($container, $nav->id, $array['groups']);
-            
-            $data[] = $array;
+
+            $array['nav_level'] = $index;
+
+            self::$_permissionItemData[$container->id][] = $array;
+
+            $this->getItems($container, $nav->id, $array['groups'], $index+1);
+
+            //$data[] = $array;
         }
-        
-        return $data;
+
+        //return $data;
     }
-    
+
     public function actionDataPermissionDelete()
     {
         $navId = Yii::$app->request->getBodyParam('navId');
         $groupId = Yii::$app->request->getBodyParam('groupId');
-        
+
         if (!empty($navId) && !empty($groupId)) {
             return Yii::$app->db->createCommand()->delete('cms_nav_permission', ['group_id' => $groupId, 'nav_id' => $navId])->execute();
         }
     }
-    
+
     public function actionDataPermissionInsert()
     {
         $navId = Yii::$app->request->getBodyParam('navId');
         $groupId = Yii::$app->request->getBodyParam('groupId');
-        
+
         if (!empty($navId) && !empty($groupId)) {
             return Yii::$app->db->createCommand()->insert('cms_nav_permission', ['group_id' => $groupId, 'nav_id' => $navId])->execute();
         }
     }
-    
+
     public function actionDataPermissionInsertInheritance()
     {
         $navId = Yii::$app->request->getBodyParam('navId');
         $groupId = Yii::$app->request->getBodyParam('groupId');
-        
+
         if (!empty($navId) && !empty($groupId)) {
             $one = (new Query())->select("*")->from("cms_nav_permission")->where(['group_id' => $groupId, 'nav_id' => $navId])->one();
-            
+
             if ($one) {
                 Yii::$app->db->createCommand()->delete('cms_nav_permission', ['group_id' => $groupId, 'nav_id' => $navId])->execute();
             }
-            
+
             return Yii::$app->db->createCommand()->insert('cms_nav_permission', ['group_id' => $groupId, 'nav_id' => $navId, 'inheritance' => 1])->execute();
         }
     }
-    
+
     public function actionDataPermissionDeleteInheritance()
     {
         $navId = Yii::$app->request->getBodyParam('navId');
         $groupId = Yii::$app->request->getBodyParam('groupId');
-        
+
         if (!empty($navId) && !empty($groupId)) {
             $one = (new Query())->select("*")->from("cms_nav_permission")->where(['group_id' => $groupId, 'nav_id' => $navId])->one();
-        
+
             if ($one) {
                 Yii::$app->db->createCommand()->delete('cms_nav_permission', ['group_id' => $groupId, 'nav_id' => $navId])->execute();
             }
         }
     }
-    
+
     public function actionDataPermissionGrantGroup()
     {
         $groupId = Yii::$app->request->getBodyParam('groupId');
--- modules/cmsadmin/src/views/permission/index.php
+++ modules/cmsadmin/src/views/permission/index.php
@@ -1,99 +1,108 @@
-<script type="text/ng-template" id="recursiveperm.html">
-    <li style="margin-left:15px;">
-        <table>
-        <tr ng-if="containerIndex==0 && innerIndex ==0">
-            <td>&nbsp</td>
-            <td ng-repeat="group in groupInjection" width="160">
-                <a class="btn btn-floating green" ng-if="group.fullPermission"><i class="material-icons">check_circle</i></a>
-                <a class="btn btn-floating grey" ng-click="grantFullPermission(group.id)" ng-if="!group.fullPermission"><i class="material-icons">check_circle</i></a>
-                <strong>{{group.name}}</strong>
-            </td>
-        </tr>
-        <tr>
-        <td width="100"><strong>{{ data.title }}</strong></td>
-        <td ng-repeat="group in data.groups" width="160">
-            <div ng-if="group.groupFullPermission">
-                <a class="btn btn-floating grey" ng-click="insertPermission(data.id, group.id)"><i class="material-icons">check</i></a>
-                <a class="btn btn-floating grey" ng-click="insertInheritance(data.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
-            </div>
-            <div ng-if="group.isInheritedFromParent && !group.groupFullPermission">
-                <a class="btn btn-floating grey disabled"><i class="material-icons">check</i></a>
-                <a class="btn btn-floating grey disabled"><i class="material-icons">playlist_add_check</i></a>
-            </div>
-            <div ng-if="!group.isInheritedFromParent && !group.groupFullPermission">
-                <a class="btn btn-floating green" ng-if="group.permissionCheckbox" ng-click="deletePermission(data.id, group.id)"><i class="material-icons">check</i></a>
-                <a class="btn btn-floating red" ng-if="!group.permissionCheckbox" ng-click="insertPermission(data.id, group.id)"><i class="material-icons">check</i></a>
-
-                <a class="btn btn-floating green" ng-if="group.isGroupPermissionInheritNode" ng-click="deleteInheritance(data.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
-                <a class="btn btn-floating red" ng-if="!group.isGroupPermissionInheritNode" ng-click="insertInheritance(data.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
-            </div>
-        </td>
-        </tr>
-        </table>
-        <ul style="margin:0px; padding:0px;" ng-repeat="data in data.__children" ng-include="'recursiveperm.html'" />
-    </li>
-</script>
 <script>
-zaa.bootstrap.register('PermissionController', function($scope, $http) {
+    zaa.bootstrap.register('PermissionController', function($scope, $http) {
 
-	$scope.$on('service:MenuData', function(event, data) {
-		$scope.loadPermissions();
-	});
-
-    $scope.data = null;
-
-    $scope.groupInjection = null;
-    
-	$scope.loadPermissions = function() {
-		$http.get('admin/api-cms-menu/data-permission-tree').then(function(response) {
-            $scope.data = response.data;
-            $scope.groupInjection = response.data.groups;
-		});
-	};
-
-    $scope.deletePermission = function(navId, groupId) {
-    	// delete request
-        $http.post('admin/api-cms-menu/data-permission-delete', {navId: navId, groupId: groupId}).then(function() {
-            $scope.loadPermissions();
-        });
-    };
-
-    $scope.insertPermission = function(navId, groupId) {
-    	$http.post('admin/api-cms-menu/data-permission-insert', {navId: navId, groupId: groupId}).then(function() {
-    	    $scope.loadPermissions();
+    	$scope.$on('service:MenuData', function(event, data) {
+    		$scope.loadPermissions();
     	});
-    };
 
-    $scope.deleteInheritance = function(navId, groupId) {
-    	$http.post('admin/api-cms-menu/data-permission-delete-inheritance', {navId: navId, groupId: groupId}).then(function() {
-    		$scope.loadPermissions();
-        });
-    };
-    
-    $scope.insertInheritance = function(navId, groupId) {
-    	$http.post('admin/api-cms-menu/data-permission-insert-inheritance', {navId: navId, groupId: groupId}).then(function() {
-    		$scope.loadPermissions();
-        });
-    };
+        $scope.data = null;
 
-    $scope.grantFullPermission = function(groupId) {
-        $http.post('admin/api-cms-menu/data-permission-grant-group', {groupId: groupId}).then(function() {
+        $scope.groupInjection = null;
+
+    	$scope.loadPermissions = function() {
+    		$http.get('admin/api-cms-menu/data-permission-tree').then(function(response) {
+                $scope.data = response.data;
+                $scope.groupInjection = response.data.groups;
+                console.dir($scope.data, $scope.groupInjection);
+    		});
+    	};
+
+        $scope.deletePermission = function(navId, groupId) {
+        	// delete request
+            $http.post('admin/api-cms-menu/data-permission-delete', {navId: navId, groupId: groupId}).then(function() {
+                $scope.loadPermissions();
+            });
+        };
+
+        $scope.insertPermission = function(navId, groupId) {
+        	$http.post('admin/api-cms-menu/data-permission-insert', {navId: navId, groupId: groupId}).then(function() {
+        	    $scope.loadPermissions();
+        	});
+        };
+
+        $scope.deleteInheritance = function(navId, groupId) {
+        	$http.post('admin/api-cms-menu/data-permission-delete-inheritance', {navId: navId, groupId: groupId}).then(function() {
+        		$scope.loadPermissions();
+            });
+        };
+
+        $scope.insertInheritance = function(navId, groupId) {
+        	$http.post('admin/api-cms-menu/data-permission-insert-inheritance', {navId: navId, groupId: groupId}).then(function() {
+        		$scope.loadPermissions();
+            });
+        };
+
+        $scope.grantFullPermission = function(groupId) {
+            $http.post('admin/api-cms-menu/data-permission-grant-group', {groupId: groupId}).then(function() {
+                $scope.loadPermissions();
+            });
+        }
+
+        $scope.init = function() {
             $scope.loadPermissions();
-        });
-    }
-    
-    $scope.init = function() {
-        $scope.loadPermissions();
-    };
+        };
 
-    $scope.init();
-});
+        $scope.init();
+    });
 </script>
 
 <div ng-controller="PermissionController" class="card-panel">
-    <h1>CMS Page Permissions</h1>
-    <div ng-repeat="container in data" ng-init="containerIndex=$index">
-        <h4>{{ container.container.name }}</h4>
-        <ul ng-repeat="data in container.items" ng-init="innerIndex=$index" ng-include="'recursiveperm.html'" />
+    <h3>CMS Page Permissions</h3>
+
+    <div class="permissions">
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <select>
+                            <option value="all" selected>Alle Container</option>
+                            <option ng-repeat="container in data.containers" value="{{container.containerInfo.id}}">{{ container.containerInfo.name }}</option>
+                        </select>
+                    </th>
+                    <th ng-repeat="group in data.groups">
+                        <a class="btn btn-floating green permissions__group-button" ng-if="group.fullPermission"><i class="material-icons">check_circle</i></a>
+                        <a class="btn btn-floating grey permissions__group-button" ng-click="grantFullPermission(group.id)" ng-if="!group.fullPermission"><i class="material-icons">check_circle</i></a>
+                        {{ group.name }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody ng-repeat="container in data.containers">
+                <tr><td class="permissions__container-spacing"></td></tr>
+                <tr class="permissions__container-row">
+                    <th colspan="{{data.groups.length + 1}}">{{ container.containerInfo.name }}</th>
+                </tr>
+                <tr ng-repeat="item in container.items" class="permissions__item-row">
+                    <th scope="row" style="padding-left: {{item.nav_level * 20}}px">{{ item.title }}</th>
+                    <td ng-repeat="group in item.groups">
+                        <!-- {{ group.name }} -->
+                        <div ng-if="group.groupFullPermission">
+                            <a class="btn btn-floating grey" ng-click="insertPermission(item.id, group.id)"><i class="material-icons">check</i></a>
+                            <a class="btn btn-floating grey" ng-click="insertInheritance(item.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
+                        </div>
+                        <div ng-if="group.isInheritedFromParent && !group.groupFullPermission">
+                            <a class="btn btn-floating green disabled"><i class="material-icons">check</i></a>
+                            <a class="btn btn-floating green disabled"><i class="material-icons">playlist_add_check</i></a>
+                        </div>
+                        <div ng-if="!group.isInheritedFromParent && !group.groupFullPermission">
+                            <a class="btn btn-floating green" ng-if="group.permissionCheckbox" ng-click="deletePermission(item.id, group.id)"><i class="material-icons">check</i></a>
+                            <a class="btn btn-floating red" ng-if="!group.permissionCheckbox" ng-click="insertPermission(item.id, group.id)"><i class="material-icons">check</i></a>
+
+                            <a class="btn btn-floating green" ng-if="group.isGroupPermissionInheritNode" ng-click="deleteInheritance(item.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
+                            <a class="btn btn-floating red" ng-if="!group.isGroupPermissionInheritNode" ng-click="insertInheritance(item.id, group.id)"><i class="material-icons">playlist_add_check</i></a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
