7533ce5e97f7a73bd6b92bd007ff7c0dce415105
Nadar <git@nadar.io>
2016-08-21 20:11:21 +0200

    cp bug fix #962

--- modules/admin/src/base/FilterInterface.php
+++ modules/admin/src/base/FilterInterface.php
@@ -12,5 +12,5 @@ interface FilterInterface
 	 *
 	 * @return string The identifier must match [a-zA-Z0-9\-]
 	 */
-	abstract public static function identifier();
+	 public static function identifier();
 }
