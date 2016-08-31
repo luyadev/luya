ff96a755d6fac8ed8057ed2ab525fd8f1b3a479a
Basil Suter <git@nadar.io>
2016-08-31 14:48:02 +0200

    removed unused modules

--- scripts/subsplit.sh
+++ scripts/subsplit.sh
@@ -16,17 +16,12 @@ git subsplit publish "
     envs/kickstarter:$BASE/luya-kickstarter.git
     modules/admin:$BASE/luya-module-admin.git
     modules/cms:$BASE/luya-module-cms.git
-    modules/cmsadmin:$BASE/luya-module-cmsadmin.git
     modules/news:$BASE/luya-module-news.git
-    modules/newsadmin:$BASE/luya-module-newsadmin.git
     modules/account:$BASE/luya-module-account.git
-    modules/accountadmin:$BASE/luya-module-accountadmin.git
     modules/errorapi:$BASE/luya-module-errorapi.git
     modules/exporter:$BASE/luya-module-exporter.git
     modules/gallery:$BASE/luya-module-gallery.git
-    modules/galleryadmin:$BASE/luya-module-galleryadmin.git
     modules/crawler:$BASE/luya-module-crawler.git
-    modules/crawleradmin:$BASE/luya-module-crawleradmin.git
     modules/styleguide:$BASE/luya-module-styleguide.git
     modules/frontendgroup:$BASE/luya-module-frontendgroup.git
     modules/remoteadmin:$BASE/luya-module-remoteadmin.git
