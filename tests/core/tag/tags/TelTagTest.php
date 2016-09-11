9ef083112da7c4c984ce73dc8d2adb26deea6269
Nadar <git@nadar.io>
2016-09-11 21:24:00 +0200

    fixed tel tag tests

--- tests/core/tag/tags/TelTagTest.php
+++ tests/core/tag/tags/TelTagTest.php
@@ -10,8 +10,8 @@ class TelTagTest extends LuyaWebTestCase
 	public function testTelTag()
 	{
 		$tag = new TelTag();
-		$this->assertSame('<a href="tel:+123">+123</a>', $tag->parse('+123'));
-		$this->assertSame('<a href="tel:+123">123</a>', $tag->parse('123'));
+		$this->assertSame('<a href="tel:+123">+123</a>', $tag->parse('+123', null));
+		$this->assertSame('<a href="tel:+123">123</a>', $tag->parse('123', null));
 		$this->assertSame('<a href="tel:+123">call</a>', $tag->parse('+123', 'call'));
 	}
 }
