9bf96fefdac71f4fabc48e88988e0290788011e6
Basil Suter <git@nadar.io>
2016-09-22 14:36:38 +0200

    added auto-grow directive and implemented to textarea #830

--- modules/admin/src/resources/js/zaa.js
+++ modules/admin/src/resources/js/zaa.js
@@ -144,6 +144,51 @@ function typeCastValue(value) {
 		};
 	});
 	
+	/**
+	 * Apply auto generated height for textareas based on input values
+	 */
+	zaa.directive('autoGrow', function() {
+		return function(scope, element, attr) {
+		    var minHeight = element[0].offsetHeight,
+		      paddingLeft = element.css('paddingLeft'),
+		      paddingRight = element.css('paddingRight');
+
+		    var $shadow = angular.element('<div></div>').css({
+		      position: 'absolute',
+		      top: -10000,
+		      left: -10000,
+		      width: element[0].offsetWidth - parseInt(paddingLeft || 0) - parseInt(paddingRight || 0),
+		      fontSize: element.css('fontSize'),
+		      fontFamily: element.css('fontFamily'),
+		      lineHeight: element.css('lineHeight'),
+		      resize:     'none'
+		    });
+		    angular.element(document.body).append($shadow);
+
+		    var update = function() {
+		      var times = function(string, number) {
+		        for (var i = 0, r = ''; i < number; i++) {
+		          r += string;
+		        }
+		        return r;
+		      }
+
+		      var val = element.val().replace(/</g, '&lt;')
+		        .replace(/>/g, '&gt;')
+		        .replace(/&/g, '&amp;')
+		        .replace(/\n$/, '<br/>&nbsp;')
+		        .replace(/\n/g, '<br/>')
+		        .replace(/\s{2,}/g, function(space) { return times('&nbsp;', space.length - 1) + ' ' });
+		      $shadow.html(val);
+
+		      element.css('height', Math.max($shadow[0].offsetHeight + 10 /* the "threshold" */, minHeight) + 'px');
+		    }
+
+		    element.bind('keyup keydown keypress change click', update);
+		    update();
+		  }
+		});
+	
 	zaa.directive('resizer', function($document) {
 
 		return {
