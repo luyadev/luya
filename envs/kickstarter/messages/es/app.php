8c6a6a7f6917885c3c976e138ada7ad5e908b213
Fernando Averanga <fernando.averanga@gmail.com>
2016-09-26 20:43:32 -0400

    Create app.php

---
+++ envs/kickstarter/messages/es/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'kickstarter_success' => '<h3>¡Estás listo!</h3>
+    <p>Sí, se ha instalado con éxito <i>LUYA</i> en su sistema. Ahora es capaz de iniciar sesión en el área de administración para crear sus páginas.</p>
+    <p><strong>Importante</strong><br />Después de iniciar sesión en el área de administración que tiene a la navegación en el &laquo;System&raquo; y habilitar todos los permisos para su grupo (Administrator).</p>',
+    'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-default">Ir al área de administración</a></p>',
+];
