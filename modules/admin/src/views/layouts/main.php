071e5c66ef523d7dbbaffd9ae46a56996b40766d
Marc Stampfli <marc.stampfli@zephir.ch>
2016-10-04 10:40:33 +0200

    Styled help-overlay; Closes #875

--- modules/admin/src/views/layouts/main.php
+++ modules/admin/src/views/layouts/main.php
@@ -558,10 +558,13 @@ $this->beginPage()
                     <ul class="collection with-header">
                     <?php foreach ($this->context->tags as $name => $object): ?>
                         <li class="collection-item" click-paste-pusher="<?= $object->example(); ?>" style="cursor: pointer;" ng-mouseenter="isHover['<?=$name;?>']=true" ng-mouseleave="isHover['<?=$name;?>']=false">
-                            <div ng-show="isHover['<?=$name;?>']" style="position:absolute;  right:20%; width:400px; position:fixed; background-color:black; color:white; z-index:9999999;">
-                                <h5 style="padding-left:2rem;"><?= $name; ?> <small> - <i><?= $object->example(); ?></i></small></h5>
-                                <?= Markdown::process($object->readme()); ?>
+
+                            <div class="help-overlay" ng-show="isHover['<?=$name;?>']">
+                                <h3 class="help-overlay__title"><?= $name; ?></h3>
+                                <code class="help-overlay__example-code"><?= $object->example(); ?></code>
+                                <div class="help-overlay__description"><?= Markdown::process($object->readme()); ?></div>
                             </div>
+
                             <div><?= $name; ?><a class="secondary-content"><i class="material-icons">content_paste</i></a></div>
                         </li>
                     <?php endforeach; ?>
