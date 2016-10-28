f33ee7c77d3dbf802e209f42bb6e422fb5d758ad
rainerCH <rainer.demmler@indielab.ch>
2016-10-28 15:56:48 +0200

    Update CmsPage.php

--- modules/admin/src/ngrest/plugins/CmsPage.php
+++ modules/admin/src/ngrest/plugins/CmsPage.php
@@ -30,7 +30,7 @@ class CmsPage extends Plugin
     public function onAfterFind($event)
     {
         $fieldValue = $event->sender->getAttribute($this->name);
-        $menuItem = (!empty($fieldValue)) ? Yii::$app->menu->find()->where(['nav_id' => $fieldValue])->with(['hidden'])->one() : $fieldValue;
+        $menuItem = (!empty($fieldValue)) ? Yii::$app->menu->find()->where(['nav_id' => $fieldValue])->with(['hidden'])->one() : false;
         $event->sender->setAttribute($this->name, $menuItem);
     }
 }
