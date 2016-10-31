8b3dc2b237fb1e30e6dd9cd02a598fb33084e0d1
Basil Suter <git@nadar.io>
2016-10-31 11:39:31 +0100

    phpdocs page tag.

--- modules/cms/src/tags/PageTag.php
+++ modules/cms/src/tags/PageTag.php
@@ -7,22 +7,48 @@ use luya\cms\models\Nav;
 use luya\cms\models\NavItem;
 
 /**
- * Page content tag.
+ * Get the Content of CMS Page.
+ * 
+ * Allows you to get the content of a page with the cmslayout or get the content of a placeholder inside the cmslayout of a page.
+ * 
+ * This allows you to Return the content of another page wherever your are. You can also use the PageTag in its Php from without the TagParser.
+ * 
+ * Example native call:
+ * 
+ * ```php
+ * echo (new \luya\cms\Tags\PageTag())->parse(1); // where 1 is the Nav ID
+ * ```
  *
  * @author Basil Suter <basil@nadar.io>
  */
 class PageTag extends BaseTag
 {
+    /**
+     * {@inheritDoc}
+     * @see \luya\tag\TagInterface::example()
+     */
     public function example()
     {
         return 'page[1](placeholder)';
     }
     
+    /**
+     * {@inheritDoc}
+     * @see \luya\tag\TagInterface::readme()
+     */
     public function readme()
     {
         return 'Get the content of a full page or just a placeholder of the page. The first param is the page it (which you get by hovering over the menu tree in the administration area) `page[1]` if you just want to get the content of a placeholder of the cmslayout used the second parameter `page[1](placeholderName)`.';
     }
     
+    /**
+     * Get the content of Nav for the current active language with cmslayout or placeholder.
+     * 
+     * @param string $value The value of the Nav ID e.g 1 (hover the cms menu to see the ID).
+     * @param string|null $sub If null this parameter will be ignored otherwise its the name of the placeholder inside this cmslayout.
+     * @return string The content rendered with the cmslayout, or if $sub is provided and not null with its placeholder name.
+     * @see \luya\tag\TagInterface::parse()
+     */
     public function parse($value, $sub)
     {
         $page = Nav::findOne($value);
