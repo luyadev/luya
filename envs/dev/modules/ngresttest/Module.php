e1b471432038fa2e4cb3a4a98e2c8fe0c7c882c6
Nadar <git@nadar.io>
2016-11-16 19:41:17 +0100

    add ngrest test

--- envs/dev/modules/ngresttest/Module.php
+++ envs/dev/modules/ngresttest/Module.php
@@ -6,16 +6,16 @@ use luya\admin\components\AdminMenuBuilder;
 
 class Module extends \luya\admin\base\Module
 {
-    public $apis = [
-        'api-ngresttest-myadminuser' => 'ngresttest\apis\MyAdminUserController',
-    ];
-    
-    public function getMenu()
-    {
-        return (new \luya\admin\components\AdminMenuBuilder($this))
-        ->node('MyAdminUser', 'extension')
-        ->group('Group')
-        ->itemApi('MyAdminUser', 'ngresttest/my-admin-user/index', 'label', 'api-ngresttest-myadminuser');
-    }
+	public $apis = [
+			'api-ngresttest-table' => 'ngresttest\apis\TableController',
+	];
+	
+	public function getMenu()
+	{
+		return (new \luya\admin\components\AdminMenuBuilder($this))
+		->node('Table', 'extension')
+		->group('Group')
+		->itemApi('Table', 'ngresttest/table/index', 'label', 'api-ngresttest-table');
+	}
     
 }
