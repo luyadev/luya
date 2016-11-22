2a8c1b6e91e52994e678697ccc17869f9d50f996
Basil Suter <git@nadar.io>
2016-11-22 15:13:11 +0100

    use cms module for dev environment in order to allow unit tests for link plugin

--- modules/admin/tests/admin/ngrest/plugins/LinkTest.php
+++ modules/admin/tests/admin/ngrest/plugins/LinkTest.php
@@ -27,7 +27,7 @@ class LinkTest extends AdminTestCase
 
         $plugin->onFind($event);
 
-        $this->assertSame('link', $user->firstname);
+        $this->assertInstanceOf('luya\web\ExternalLink', $user->firstname);
     }
     
     public function testExternalLinkI18n()
@@ -47,6 +47,6 @@ class LinkTest extends AdminTestCase
     
         $plugin->onFind($event);
     
-        $this->assertSame('link', $user->firstname);
+        $this->assertInstanceOf('luya\web\ExternalLink', $user->firstname);
     }
 }
