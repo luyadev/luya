5ad427557d8c7b454584016a0e29136410db7e6f
Basil Suter <git@nadar.io>
2016-12-05 12:51:57 +0100

    added cacheable tests

---
+++ tests/core/traits/CacheableTraitTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace luyatests\core\traits;
+
+use Yii;
+use luyatests\LuyaWebTestCase;
+use luya\traits\CacheableTrait;
+use yii\caching\FileDependency;
+
+class CacheStub
+{
+    use CacheableTrait;
+}
+
+class CacheableTraitTest extends LuyaWebTestCase
+{
+    public function testNotCacheable()
+    {
+        $cache = new CacheStub();
+        $this->assertFalse($cache->isCachable());
+        $this->assertFalse($cache->setHasCache('not', 'available'));
+        $this->assertFalse($cache->deleteHasCache('not'));
+        $this->assertFalse($cache->getHasCache('not'));
+    }
+    
+    public function testCacheable()
+    {
+        Yii::$app->set('cache', ['class' => 'yii\caching\DummyCache']);
+        $cache = new CacheStub();
+        $this->assertTrue($cache->isCachable());
+        $this->assertTrue($cache->setHasCache('foo', 'bar'));
+        $this->assertTrue($cache->setHasCache('baz', 'bar', ['class' => 'yii\caching\FileDependency', 'fileName' => 'foobar.txt']));
+        $this->assertTrue($cache->setHasCache('qux', 'bar', (new FileDependency(['fileName' => 'foobar.txt']))));
+        $this->assertTrue($cache->deleteHasCache('foo'));
+    }
+}
