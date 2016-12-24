a754570e6b306978ef82481e2ebc35aa03d6dd59
Stas <asyou1985@gmail.com>
2016-12-24 16:53:32 +0200

    Translate Ukraine (#1123)
    * Admin Module translate
    
    * Translate CMS frontend
    
    * Translate kick,crawler,errorapi,news
    
    * Translate cms admin

---
+++ modules/news/src/admin/messages/ua/newsadmin.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'news' => 'Новини',
+    'news_administrate' => 'Адміністрування',
+    'article' => 'Новина',
+    'article_cat_id' => 'Категорія',
+    'article_title' => 'Назва',
+    'article_text' => 'Контент',
+    'article_image_id' => 'Зображення',
+    'article_timestamp_create' => 'Дата',
+    'article_timestamp_display_from' => 'Відображати з ',
+    'article_timestamp_display_until' => 'Відображати до',
+    'article_is_display_limit' => 'Межа показів',
+    'article_image_list' => 'Список зображень',
+    'article_file_list' => 'Список файлів',
+    'article_tag' => 'Тег',
+    'cat' => 'Категорії',
+    'cat_title' => 'Кмтегория',
+    'cat_delete_error' => 'В цій категорії до сих пір використовується одина або кілька новин і вона не може бути видалена.',
+    'cat_title_create_error' => 'Будь ласка виберіть назву категорії.',
+    'tag' => 'Теги',
+    'tag_title' => 'Тег',
+    'tag_title_create_error' => 'Будьласка віберіть назву тега.',
+    
+// 1.0.0
+    
+    'teaser_text' => 'Текст тизера',
+];
