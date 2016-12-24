44bea8dc8bb48db4454d7e566324b607f7ed7eba
Nadar <git@nadar.io>
2016-12-24 12:10:35 +0100

    added crawler messages test

---
+++ modules/crawler/tests/MessageFileTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace admintests;
+
+use Yii;
+use luyatests\data\MessageFileComperatorTrait;
+use PHPUnit\Framework\TestCase;
+
+class MessageFileTest extends TestCase
+{
+	use MessageFileComperatorTrait;
+	
+	public function testFiles()
+	{
+		$this->compare(__DIR__ . '/../src/admin/messages', 'en');
+	}
+}
