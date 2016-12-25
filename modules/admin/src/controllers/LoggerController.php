1c6e96af0e0d5e41d15fc68a395e261574f969ee
Basil <git@nadar.io>
2016-12-25 12:35:42 +0100

    Update LoggerController.php

--- modules/admin/src/controllers/LoggerController.php
+++ modules/admin/src/controllers/LoggerController.php
@@ -4,6 +4,9 @@ namespace luya\admin\controllers;
 
 use luya\admin\ngrest\base\Controller;
 
+/**
+ * NgRest Logger Controller
+ */
 class LoggerController extends Controller
 {
     public $modelClass = 'luya\admin\models\Logger';
