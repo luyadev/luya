f4e645ede368a787563e2b1d8e0441899b259f23
Basil Suter <git@nadar.io>
2017-01-01 16:15:57 +0100

    add getter/setter test for config

--- tests/core/base/BootTest.php
+++ tests/core/base/BootTest.php
@@ -42,4 +42,11 @@ class BootTest extends \luyatests\LuyaWebTestCase
         $boot->mockOnly = true;
         $boot->applicationConsole();
     }
+    
+    public function testSetterGetterConfig()
+    {
+    	$boot = new Boot();
+    	$boot->setConfigArray(['foo' => 'bar']);
+    	$this->assertSame(['foo' => 'bar'], $boot->getConfigArray());
+    }
 }
