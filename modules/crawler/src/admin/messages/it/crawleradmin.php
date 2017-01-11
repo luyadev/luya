d43c38ae4af6d3da2940c97e7b066ea635f81f02
agaplus <messina.emanuele@gmail.com>
2017-01-11 21:02:17 +0100

    Italian translation for crawleradmin.php

---
+++ modules/crawler/src/admin/messages/it/crawleradmin.php
@@ -0,0 +1,20 @@
+<?php
+return [
+// added translation in 1.0.0-beta5:
+    'crawler' => 'Crawler',
+    'crawler_indexing' => 'Indexing',
+    //builderindex
+    'crawler_index' => 'Index pagina',
+    'builderindex_url' => 'Url',
+    'builderindex_title' => 'Titolo',
+    'builderindex_language_info' => 'Info Lingua',
+    'builderindex_content' => 'Content',
+    'builderindex_url_found' => 'Url trovato nella Pagina',
+    //index
+    'crawler_builderindex' => 'Pagina cache',
+    'index_url' => 'Url',
+    'index_title' => 'Titolo',
+    'index_language_info' => 'Info lingua',
+    'index_content' => 'Contenuto',
+    'index_url_found' => 'Url trovato nella Pagina',
+];
