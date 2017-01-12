b3388d65bf89c38ad33cef8a455374e6cc943537
agaplus <messina.emanuele@gmail.com>
2017-01-12 09:23:46 +0100

    Italian translation for newsadmin.php file (#1153)

---
+++ modules/news/src/admin/messages/it/newsadmin.php
@@ -0,0 +1,29 @@
+<?php
+return [
+// added translation in 1.0.0-beta5:
+    'news' => 'Notizie',
+    'news_administrate' => 'Amministrazione',
+    'article' => 'Articoli',
+    'article_cat_id' => 'Categoria',
+    'article_title' => 'Titolo',
+    'article_text' => 'Contenuto',
+    'article_image_id' => 'Immagine',
+    'article_timestamp_create' => 'Data',
+    'article_timestamp_display_from' => 'Visualizza dal ',
+    'article_timestamp_display_until' => 'Visualizza fino al ',
+    'article_is_display_limit' => 'Limita visualizzazione',
+    'article_image_list' => 'Lista immagini',
+    'article_file_list' => 'Lista file',
+    'article_tag' => 'Tag',
+    'cat' => 'Categorie',
+    'cat_title' => 'Categoria',
+    'cat_delete_error' => 'Questa categoria è attualmente utilizzata da uno o più articoli e quindi non può essere cancellata.',
+    'cat_title_create_error' => 'Inserisci un titolo per la categoria',
+    'tag' => 'Tags',
+    'tag_title' => 'Tag',
+    'tag_title_create_error' => 'Inserisci un titolo per il tag.',
+    
+// 1.0.0
+    
+    'teaser_text' => 'Sottotitolo',
+];
