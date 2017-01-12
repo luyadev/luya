47930f67fac3a450e2c334597a609b9fa983132b
agaplus <messina.emanuele@gmail.com>
2017-01-12 09:20:33 +0100

    italian translation for app.php (#1149)

---
+++ envs/kickstarter/messages/it/app.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'kickstarter_success' => '<h1>Installazione effettuata!</h1>
+    <p>Si, hai installato <i>LUYA</i> nel tuo Sistema. Puoi effettuare il login nell\'area di amministrazione per creare le tue pagine.</p>
+    <p><strong>Importante</strong><br />Dopo il login nell\area di amministrazione puoi navigare nel &laquo;System&raquo; e abilitare i permessi per il tuo gruppo (Adminstrator).</p>',
+    'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Apri l\'area di amministrazione</a></p>',
+];
