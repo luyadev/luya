4d94ae117646d839c80cefdbe79287499efbb598
Basil Suter <git@nadar.io>
2017-01-30 11:44:44 +0100

    fixed image source property

--- modules/admin/src/proxy/ClientTransfer.php
+++ modules/admin/src/proxy/ClientTransfer.php
@@ -83,7 +83,7 @@ class ClientTransfer extends Object
                 if (!$curl->error) {
                     if (FileHelper::writeFile($image->serverSource, $curl->response)) {
                         $imageCount++;
-                        $this->build->command->outputInfo('[+] Image ' . $image->systemFileName.' downloaded.');
+                        $this->build->command->outputInfo('[+] Image ' . $image->source.' downloaded.');
                     }
                 }
             }
