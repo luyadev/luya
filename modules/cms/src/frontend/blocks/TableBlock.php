e6599dc63dec10c20a28724357941aa14b03350c
Basil Suter <git@nadar.io>
2017-02-05 14:42:26 +0100

    refactoring table block #1124

--- modules/cms/src/frontend/blocks/TableBlock.php
+++ modules/cms/src/frontend/blocks/TableBlock.php
@@ -2,31 +2,46 @@
 
 namespace luya\cms\frontend\blocks;
 
-use luya\cms\frontend\Module;
 use luya\TagParser;
-use luya\cms\base\TwigBlock;
+use luya\cms\frontend\Module;
+use luya\cms\base\PhpBlock;
 
 /**
  * Table Block.
  *
  * @author Basil Suter <basil@nadar.io>
  */
-final class TableBlock extends TwigBlock
+final class TableBlock extends PhpBlock
 {
+	/**
+	 * @inheritdoc
+	 */
     public $module = 'cms';
     
+    /**
+     * @inheritdoc
+     */
     public $cacheEnabled = true;
 
+    /**
+     * @inheritdoc
+     */
     public function name()
     {
         return Module::t('block_table_name');
     }
 
+    /**
+     * @inheritdoc
+     */
     public function icon()
     {
         return 'border_all';
     }
 
+    /**
+     * @inheritdoc
+     */
     public function config()
     {
         return [
@@ -44,6 +59,9 @@ final class TableBlock extends TwigBlock
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getFieldHelp()
     {
         return [
@@ -51,28 +69,40 @@ final class TableBlock extends TwigBlock
         ];
     }
 
+    /**
+     * Get the table data for the table generator.
+     *
+     * @return array
+     */
     public function getTableData()
     {
+    	$hasHeader = $this->getCfgValue('header', 0);
         $table = [];
         $i = 0;
-        foreach ($this->getVarValue('table', []) as $key => $row) {
+        foreach ($this->getVarValue('table', []) as $row) {
             ++$i;
-
-            if ($this->getCfgValue('header', 0) == 1 && $i == 1) {
+			// whether the header data can be skipped or not
+            if ($hasHeader == 1 && $i == 1) {
                 continue;
             }
-
+			// if markdown enabled parse field values
             if ($this->getCfgValue('parseMarkdown', false)) {
-                foreach ($row as $k => $v) {
-                    $row[$k] = TagParser::convertWithMarkdown($v);
+                foreach ($row as $field => $value) {
+                    $row[$field] = TagParser::convertWithMarkdown($value);
                 }
             }
             
             $table[] = $row;
         }
+        
         return $table;
     }
 
+    /**
+     * Return the table header array data.
+     * 
+     * @return array
+     */
     public function getHeaderRow()
     {
         $data = $this->getVarValue('table', []);
@@ -80,6 +110,9 @@ final class TableBlock extends TwigBlock
         return (count($data) > 0) ? array_values($data)[0] : [];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function extraVars()
     {
         return [
@@ -87,41 +120,18 @@ final class TableBlock extends TwigBlock
             'headerData' => $this->getHeaderRow(),
         ];
     }
-
-    public function twigFrontend()
-    {
-        return  '{% if extras.table is not empty %}'.
-                '<div class="{{cfgs.divCssClass}}"><table class="table{% if cfgs.stripe %} table-striped{% endif %}{% if cfgs.border %} table-bordered{% endif %}">'.
-                    '{% if cfgs.header %}'.
-                    '<thead>'.
-                        '<tr>'.
-                            '{% for column in extras.headerData %}'.
-                            '<th>{{ column }}</th>{% endfor %}'.
-                        '</tr>'.
-                    '</thead>'.
-                    '{% endif %}'.
-                    '<tbody>'.
-                        '{% for row in extras.table %}'.
-                        '<tr>'.
-                            '{% for column in row %}'.
-                            '<td {% if cfgs.equaldistance %}class="col-md-{{ (12/(row|length))|round }}"{% endif %}>{{ column }}</td>'.
-                            '{% endfor %}'.
-                        '</tr>'.
-                        '{% endfor %}'.
-                    '</tbody>'.
-                '</table></div>'.
-                '{% endif %}';
-    }
-
-    public function twigAdmin()
+    
+    /**
+     * @inheritdoc
+     */
+    public function admin()
     {
         return  '<p>{% if extras.table is empty %}<span class="block__empty-text">' . Module::t('block_table_no_table') . '</span>{% else %}'.
-                '<table>'.
+                '<table class="bordered striped" style="border:1px solid #d0d0d0">'.
                     '{% if cfgs.header %}'.
                     '<thead>'.
                         '<tr>'.
-                            '{% for column in extras.headerData %}'.
-                            '<th>{{ column }}</th>{% endfor %}'.
+                            '{% for column in extras.headerData %}<th>{{ column }}</th>{% endfor %}'.
                         '</tr>'.
                     '</thead>'.
                     '{% endif %}'.
