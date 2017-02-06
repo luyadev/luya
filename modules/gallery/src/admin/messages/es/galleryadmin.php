6ab9bed4c520d13247af878795c8f38359be5229
Fernando Averanga <nandes.ingsistemas@gmail.com>
2017-02-06 09:37:38 -0400

    ES galleryadmin

---
+++ modules/gallery/src/admin/messages/es/galleryadmin.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'gallery' => 'Galerías',
+    'gallery_administrate' => 'Administrar',
+
+    //album
+    'album' => 'Álbumes',
+    'album_title' => 'Título',
+    'album_description' => 'Descripción',
+    'album_cover_image_id' => 'Imagen de portada',
+    'album_cat_id' => 'Categoría',
+    'album_sort_index' => 'Ordenar',
+    'album_is_highlight' => 'Realce',
+    'album_upload' => 'Subir y editar imágenes',
+    'album_category_error' => 'Por favor elija una categoría.',
+    'album_title_create_error' => 'Por favor, elija un título.',
+
+    //cat
+    'cat' => 'Categorías',
+    'cat_title' => 'Categoría',
+    'cat_description' => 'Descripción',
+    'cat_cover_image_id' => 'Imagen de portada',
+    'cat_title_create_error' => 'Por favor, elija un título.',
+    'cat_delete_error' => 'Esta categoría todavía está siendo utilizada por una o varias galerías y no se puede eliminar',
+];
