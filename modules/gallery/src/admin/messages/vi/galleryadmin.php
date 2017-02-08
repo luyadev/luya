d7471b12edd44689b3775be0f986bf54daead841
Vuong Minh <vuongxuongminh@gmail.com>
2017-02-09 02:33:37 +0700

    Add Vietnamese translate (#1205)
    + Add Vietnamese translate:
    + Admin Module
    + CMS Admin Module
    + Add translate module News Admin
    + Add translate module Gallery Admin
    * Add translate module Crawler Admin

---
+++ modules/gallery/src/admin/messages/vi/galleryadmin.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'gallery' => 'Thư viện ảnh',
+    'gallery_administrate' => 'Quản lý thư viện ảnh',
+
+    //album
+    'album' => 'Albums',
+    'album_title' => 'Tên album',
+    'album_description' => 'Mô tả',
+    'album_cover_image_id' => 'Ảnh cover',
+    'album_cat_id' => 'Danh mục',
+    'album_sort_index' => 'Sắp xếp',
+    'album_is_highlight' => 'Đánh dấu',
+    'album_upload' => 'Tải lên và chỉnh sửa ảnh',
+    'album_category_error' => 'Vui lòng nhập danh mục.',
+    'album_title_create_error' => 'Vui lòng nhập tên.',
+
+    //cat
+    'cat' => 'Danh mục',
+    'cat_title' => 'Tên danh mục',
+    'cat_description' => 'Mô tả',
+    'cat_cover_image_id' => 'Ảnh cover',
+    'cat_title_create_error' => 'Vui lòng nhập tên danh mục',
+    'cat_delete_error' => 'Danh mục này hiện đang chứa một hoặc nhiều album hình ảnh không thể xóa!',
+];
---
+++ modules/news/src/admin/messages/vi/newsadmin.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'news' => 'Bài viết',
+    'news_administrate' => 'Quản lý bài viết',
+    'article' => 'Bài viết mới',
+    'article_cat_id' => 'Danh mục',
+    'article_title' => 'Tiêu đề',
+    'article_text' => 'Nội dung',
+    'article_image_id' => 'Hình ảnh',
+    'article_timestamp_create' => 'Thời gian',
+    'article_timestamp_display_from' => 'Hiển thị tại ',
+    'article_timestamp_display_until' => 'Hiển thị đến ',
+    'article_is_display_limit' => 'Giới hạn hiển thị',
+    'article_image_list' => 'Danh sách hình ảnh',
+    'article_file_list' => 'Danh sách tệp tin',
+    'article_tag' => 'Tag',
+    'cat' => 'Danh mục',
+    'cat_title' => 'Danh mục',
+    'cat_delete_error' => 'Danh mục này hiện đang có một hoặc nhiều bài viết bên trong. Không thể xóa!',
+    'cat_title_create_error' => 'Vui lòng nhập tên danh mục!',
+    'tag' => 'Thẻ tag',
+    'tag_title' => 'Tên thẻ',
+    'tag_title_create_error' => 'Vui lòng nhập tên thẻ!',
+    
+// 1.0.0
+    
+    'teaser_text' => 'Teaser Text',
+];
