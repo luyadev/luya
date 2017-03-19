10d05a0c935c9caf2b92122864c7979833cd64f7
Rodrigo Z. Armond <rodzadra@gmail.com>
2017-03-19 10:05:10 -0300

    Brazilian translations (#1236)
    * Brazilian Portuguese translation
    * Brazilia Portuguese to newsadmin.php file
    * Correcting for a typo. Brazilia Portuguese to newsadmin.php file
    * Brazilian Portuguese translation
    * Brazilia Portuguese to newsadmin.php file
    * Correcting for a typo. Brazilia Portuguese to newsadmin.php file
    * Complements in the Brazilian Portuguese translation.
    * kickstarter portuguese translation
    * Brazilian translations: crawleradmin.php, errorapi.php, galleryadmin.php and newsadmin.php

---
+++ modules/news/src/admin/messages/pt/newsadmin.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'news' => 'Notícias', 
+    'news_administrate' => 'Administrar',
+    'article' => 'Entrada de notícias',
+    'article_cat_id' => 'Categoria',
+    'article_title' => 'Título',
+    'article_text' => 'Conteúdo',
+    'article_image_id' => 'Imagem',
+    'article_timestamp_create' => 'Data',
+    'article_timestamp_display_from' => 'Exibir de ',
+    'article_timestamp_display_until' => 'Exibir até',
+    'article_is_display_limit' => 'Limitar a exibição',
+    'article_image_list' => 'Lista de imagens',
+    'article_file_list' => 'Lista de arquivos',
+    'article_tag' => 'Tag',
+    'cat' => 'Categorias',
+    'cat_title' => 'Categoria',
+    'cat_delete_error' => 'Esta categoria ainda está sendo usada por uma ou várias notícias e não pode ser excluída.',
+    'cat_title_create_error' => 'Por favor, escolha um título para a categoria.',
+    'tag' => 'Tags',
+    'tag_title' => 'Tag',
+    'tag_title_create_error' => 'Por favor, escolha um título para a tag.',
+    
+// 1.0.0
+    
+    'teaser_text' => 'Texto de chamada',
+];
