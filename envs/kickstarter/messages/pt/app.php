8b08339722b61edab6dbcc323fc5746490f67279
Rodrigo Z. Armond <rodzadra@gmail.com>
2017-04-11 11:29:36 -0300

    RC3 and typos (#1261)

--- envs/kickstarter/messages/pt/app.php
+++ envs/kickstarter/messages/pt/app.php
@@ -3,6 +3,6 @@
 return [
     'kickstarter_success' => '<h1>Você terminou!</h1>
     <p>Sim! Você instalou o <i>LUYA</i> no seu sistema com sucesso. Agora você pode fazer login na área administrativa para criar suas páginas.</p>
-    <p><strong>Importante</strong><br />Depois de entrar na área administrativa você deve navegar até o menu "Sistema" e ativar todas as permissões para o seu grupo (Adminstrador).</p>',
+    <p><strong>Importante</strong><br />Depois de entrar na área administrativa você deve navegar até o menu "Sistema" e ativar todas as permissões para o seu grupo (Administrador).</p>',
     'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Abrir a área administrativa</a></p>',
 ];
