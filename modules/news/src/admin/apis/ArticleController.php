1530faae1621ccf8286ffc7f561294c17bb11176
Marc Stampfli <marc.stampfli@zephir.ch>
2017-04-11 10:46:02 +0200

    Removed test setting that shouldn't be commited

--- modules/news/src/admin/apis/ArticleController.php
+++ modules/news/src/admin/apis/ArticleController.php
@@ -4,7 +4,5 @@ namespace luya\news\admin\apis;
 
 class ArticleController extends \luya\admin\ngrest\base\Api
 {
-    public $pagination = ['pageSize' => 10];
-
     public $modelClass = 'luya\news\models\Article';
 }
