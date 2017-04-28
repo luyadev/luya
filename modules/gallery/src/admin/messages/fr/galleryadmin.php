a0f913ee403022ce84f9c5a450ced24038e0a913
JDL <j.david.legrand@gmail.com>
2017-04-28 20:45:04 +0200

    Create galleryadmin.php

---
+++ modules/gallery/src/admin/messages/fr/galleryadmin.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    // added translation in 1.0.0-beta5:
+    'gallery' => 'Galeries',
+    'gallery_administrate' => 'Gérer',
+
+    //album
+    'album' => 'Albums',
+    'album_title' => 'Titre',
+    'album_description' => 'Description',
+    'album_cover_image_id' => 'Image de couverture',
+    'album_cat_id' => 'Catégorie',
+    'album_sort_index' => 'Trier',
+    'album_is_highlight' => 'Mettre en évidence',
+    'album_upload' => 'Télécharger et éditer des images',
+    'album_category_error' => 'Veuillez choisir une catégorie',
+    'album_title_create_error' => 'Veuillez entrer un titre',
+
+    //cat
+    'cat' => 'Catégories',
+    'cat_title' => 'Catégorie',
+    'cat_description' => 'Description',
+    'cat_cover_image_id' => 'Image de couverture',
+    'cat_title_create_error' => 'Veuillez entrer un titre',
+    'cat_delete_error' => 'Cette catégorie est utilisée par une ou plusieurs galeries et ne peut pas être supprimée',
+];
