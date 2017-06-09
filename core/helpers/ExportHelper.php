049ecefe54ce93ac110297783ce089c6ab12292d
Basil Suter <git@nadar.io>
2017-06-09 11:11:21 +0200

    added exporter tests #1332

--- core/helpers/ExportHelper.php
+++ core/helpers/ExportHelper.php
@@ -1,70 +1,110 @@
 <?php
 
-namespace app\helpers;
+namespace luya\helpers;
 
 use yii\db\ActiveQueryInterface;
 use yii\db\ActiveRecordInterface;
 use luya\helpers\ArrayHelper;
 use yii\helpers\Html;
+use luya\Exception;
 
 /**
- * Exporting into other Formats.
- * 
+ * Exporting into Formats.
+ *
  * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
  */
 class ExportHelper
 {
     /**
      *
      * @param unknown $input
-     * @return unknown
+     * @param array $keys
+     * @param string $header
+     * @return string
      */
-    public static function csv($input, array $properties = [])
+    public static function csv($input, array $keys = [], $header = true)
     {
         if ($input instanceof ActiveQueryInterface) {
             $input = $input->all();
         }
 
-        return self::generateContent($input, ',', $properties);
+        return self::generateContent($input, ',', $keys, $header);
     }
 
     /**
-     * 
+     *
      * @param array $contentRows
      * @param unknown $delimiter
-     * @param unknown $properties
+     * @param unknown $keys
      * @param string $generateHeader
-     * @return NULL|string
+     * @return string
      */
-    protected static function generateContent(array $contentRows, $delimiter, $properties, $generateHeader = true)
+    protected static function generateContent($contentRows, $delimiter, $keys, $generateHeader = true)
     {
+        if (is_scalar($contentRows)) {
+            throw new Exception("Content must be either an array, object or Travarsable");
+        }
+        
+        $attributeKeys = $keys;
         $header = [];
         $rows = [];
         $i = 0;
-        foreach ($contentRows as $key => $content) {
+        foreach ($contentRows as $content) {
+            // handle rows content
+            if (!empty($keys) && is_array($content)) {
+                foreach ($content as $k => $v) {
+                    if (!in_array($k, $keys)) {
+                        unset($content[$k]);
+                    }
+                }
+            } elseif (!empty($keys) && is_object($content)) {
+                $attributeKeys[get_class($content)] = $keys;
+            }
+            $rows[$i] = ArrayHelper::toArray($content, $attributeKeys, false);
+            
             // handler header
-            if ($i == 0) {
+            if ($i == 0 && $generateHeader) {
                 if ($content instanceof ActiveRecordInterface) {
-                    $header[] = $content->getAttributeLabel($key);
+                    foreach ($content as $k => $v) {
+                        if (empty($keys)) {
+                            $header[] = $content->getAttributeLabel($k);
+                        } elseif (in_array($k, $keys)) {
+                            $header[] = $content->getAttributeLabel($k);
+                        }
+                    }
                 } else {
-                    $header[] = $key;
+                    $header = array_keys($rows[0]);
                 }
             }
-
-            $rows[] = ArrayHelper::toArray($content, $properties, false);
-            // handle content rows
+            
             $i++;
         }
 
-        $html = null;
+        $output = null;
+        if ($generateHeader) {
+            $output.= self::generateRow($header, $delimiter, '"');
+        }
         foreach ($rows as $row) {
-            array_walk($row, function(&$item) {
-                $item = '"'.Html::encode($item).'"';
-            });
-
-            $html.= implode($delimiter, $row) . PHP_EOL;
+            $output.= self::generateRow($row, $delimiter, '"');
         }
 
-        return $html;
+        return $output;
+    }
+
+    /**
+     *
+     * @param array $row
+     * @param unknown $delimiter
+     * @param unknown $enclose
+     * @return string
+     */
+    protected static function generateRow(array $row, $delimiter, $enclose)
+    {
+        array_walk($row, function (&$item) use ($enclose) {
+            $item = $enclose.Html::encode($item).$enclose;
+        });
+        
+        return implode($delimiter, $row) . PHP_EOL;
     }
 }
