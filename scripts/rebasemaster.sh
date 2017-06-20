93d82e5448f9fbaf89ebcfa5ed1d5354c5296aac
Silvan <silvan@dev7.ch>
2017-06-20 09:58:34 +0200

    improve rebasemaster.sh script

--- scripts/rebasemaster.sh
+++ scripts/rebasemaster.sh
@@ -4,6 +4,14 @@ if [ "$1" = "init" ];  then
 	git remote add upstream https://github.com/luyadev/luya.git
 fi
 
-git checkout master
+# simply type ./scripts/rebasemaster.sh branch **your-branch-name** in terminal
+
+BRANCH="master"
+
+if [ "$1" = "branch" ];  then
+	BRANCH=$2
+fi
+
+git checkout $BRANCH
 git fetch upstream
-git rebase upstream/master
+git rebase upstream/$BRANCH $BRANCH
