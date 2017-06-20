56bd166bfb4ba3fb44c30634b039eebf428fa2f0
Silvan <silvan@dev7.ch>
2017-06-20 10:30:36 +0200

    added file comment (#1339)

--- scripts/rebasemaster.sh
+++ scripts/rebasemaster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# When running the rebasemaster on the first time, use the init command.
+
 if [ "$1" = "init" ];  then
 	git remote add upstream https://github.com/luyadev/luya.git
 fi
