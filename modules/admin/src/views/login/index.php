2ea93b15700e04aa1a26b66a73c783980ac31842
Silvan <silvan@dev7.ch>
2017-07-22 17:08:39 +0200

    add input field labels and login screen again (#1359)

--- modules/admin/src/views/login/index.php
+++ modules/admin/src/views/login/index.php
@@ -1,94 +1,71 @@
 <?php
 use luya\admin\Module;
-
 ?>
-<div class="container login">
-    <div class="row">
-        <div class="col s12 offset-s0 m6 offset-m3">
 
-            <div class="card hidden" id="success">
-                <div class="login-success">
-                    <i class="material-icons login-success-icon green-text">check_circle</i>
+<div class="login-frame">
+    <div class="login-panel-left">
+        <div class="login-logo"></div>
+        <div class="login-system-info">
+            <span class="login-version">
+                <?= Module::t('login_version'); ?>
+            </span>
+        </div>
+    </div>
+    <div class="login-panel-right">
+        <!-- normal login in form -->
+        <div class="login-status-success-wrapper hidden" id="success">
+            <i class="login-status-success material-icons">check_circle</i>
+        </div>
+        <form class="login-form" method="post" id="loginForm">
+            <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken; ?>" />
+            <div class="login-inputs">
+                <h1 class="login-title"><?= Module::t('login_pre_title', ['title' => Yii::$app->siteTitle]); ?></h1>
+                <div class="login-status-wrapper">
+                    <div class="login-status" id="errorsContainer" style="display: none"></div>
+                </div>
+                <div class="login-form-field">
+                    <input class="login-input" id="email" name="login[email]" type="email" tabindex="1" required />
+                    <label for="email" class="login-input-label"><?= Module::t('login_mail'); ?></label>
+                </div>
+                <div class="login-form-field">
+                    <input class="login-input" id="password" name="login[password]" type="password" tabindex="2" required />
+                    <label for="password" class="login-input-label"><?= Module::t('login_password'); ?></label>
+                    <a href="#" class="login-reset login-link">
+                        Passwort vergessen?
+                    </a>
+                </div>
+                <div class="login-buttons">
+                    <button class="login-btn" type="submit"  tabindex="3">
+                        <?= Module::t('login_btn_login'); ?>
+                    </button>
                 </div>
             </div>
-
-            <!- Normal login ->
-            <form class="card" method="post" id="loginForm">
-                <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken; ?>" />
-                <div class="card-content clearfix">
-                    <span class="card-title black-text"><?= Module::t('login_pre_title', ['title' => Yii::$app->siteTitle]); ?></span>
-
-                    <br />
-                    <br />
-
-                    <div class="row">
-                        <div class="input input-text input-vertical col s12">
-                            <label class="input-label" for="email"><?= Module::t('login_mail'); ?></label>
-                            <div class="input-field-wrapper">
-                                <input class="input-field" id="email" name="login[email]" type="email" tabindex="1" />
-                            </div>
-                        </div>
+        </form>
+        <!-- end of normal login in form -->
+        <!-- secure login in form -->
+        <form class="login-form hidden" method="post" id="secureForm">
+            <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken; ?>" />
+            <div class="login-inputs">
+                <h1 class="login-title"><?= Module::t('login_pre_title', ['title' => Yii::$app->siteTitle]); ?></h1>
+                <div class="login-status-wrapper">
+                    <div class="login-status" id="errorsContainer" style="display: none">
+                        <span class="login-status-message">blah</span>
                     </div>
-
-                    <div class="row">
-                        <div class="input input-text input-vertical col s12">
-                            <label class="input-label" for="password"><?= Module::t('login_password'); ?></label>
-                            <div class="input-field-wrapper">
-                                <input class="input-field" id="password" name="login[password]" type="password" tabindex="2" />
-                            </div>
-                        </div>
-                    </div>
- 
-                    <br />
-
                 </div>
-
-                <div class="card-action">
-                    <button class="btn right color green white-text" type="submit"  tabindex="3">
-                        <?= Module::t('login_btn_login'); ?> <i class="material-icons right submit-icon">keyboard_arrow_right</i>
-                        <div class="preloader-wrapper login-spinner right small active hidden spinner"><div class="spinner-layer"><div class="circle-clipper left"><div class="circle"></div></div></div></div>
+                <div class="login-form-field">
+                    <label class="input-label" for="secure_token"><?= Module::t('login_securetoken_info'); ?></label>
+                    <input class="login-input" id="secure_token" name="secure_token" type="text" tabindex="1" placeholder="<?= Module::t('login_securetoken'); ?>" />
+                </div>
+                <div class="login-buttons">
+                    <button class="login-btn login-btn-50" type="submit"  tabindex="2">
+                        <?= Module::t('button_send'); ?>
                     </button>
-                    <div class="clearfix"></div>
-                </div>
-            </form>
-            <!- /Normal login ->
-
-            <!- Token ->
-            <form class="card hidden" method="post" id="secureForm">
-                <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken; ?>" />
-                <div class="card-content clearfix">
-                    <span class="card-title black-text"><?= Module::t('login_pre_title', ['title' => Yii::$app->siteTitle]); ?></span>
-
-                    <br />
-                    <br />
-
-                    <div class="row">
-                        <div class="input input-text input-vertical col s12">
-                            <label class="input-label" for="secure_token"><?= Module::t('login_securetoken'); ?></label>
-                            <div class="input-field-wrapper">
-                                <input class="input-field" name="secure_token" id="secure_token" type="text" tabeindex="1" />
-                                <small><?= Module::t('login_securetoken_info'); ?></small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <br />
-
-                </div>
-
-                <div class="card-action">
-                    <button class="btn right green white-text" type="submit">
-                        <?= Module::t('button_send'); ?> <i class="material-icons right submit-icon">keyboard_arrow_right</i>
-                        <div class="preloader-wrapper login-spinner right small active hidden spinner"><div class="spinner-layer"><div class="circle-clipper left"><div class="circle"></div></div></div></div>
+                    <button class="login-btn login-btn-50" id="abortToken" type="submit"  tabindex="3">
+                        <?= Module::t('button_abort'); ?>
                     </button>
-                    <button class="btn left red white-text" type="button" id="abortToken"><i class="material-icons left">cancel</i> <?= Module::t('button_abort'); ?></button>
-                    <div class="clearfix"></div>
                 </div>
-            </form>
-            <!- /Token ->
-
-            <div class="card-panel red lighten-1 white-text" id="errorsContainer" style="display:none;"></div>
-
-        </div>
+            </div>
+        </form>
+        <!-- end of secure login in form -->
     </div>
 </div>
