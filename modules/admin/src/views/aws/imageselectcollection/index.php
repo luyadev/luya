cca14b0b540ec99e83936bc7b4f18dea5e8b96d5
Basil Suter <git@nadar.io>
2017-08-14 09:25:03 +0200

    fix file selection views

--- modules/admin/src/views/aws/imageselectcollection/index.php
+++ modules/admin/src/views/aws/imageselectcollection/index.php
@@ -1,23 +1,19 @@
 <?php
 use luya\admin\Module;
-
 ?>
-<div ng-controller="ActiveWindowGalleryController">
-    <div class="col s8">
+<div ng-controller="ActiveWindowGalleryController" class="row">
+    <div class="col-md-8">
     	<storage-file-manager selection="true" only-images="true" />
     </div>
-    <div class="col s4" style="background-color:#F0F0F0;">
-        <div style="padding:10px;">
-            <h5><?= Module::t('aws_gallery_images')?></h5>
-            <div ng-show="isEmptyObject(files)">
-                <p><?= Module::t('aws_gallery_empty')?></p>
+    <div class="col-md-4">
+        <h5><?= Module::t('aws_gallery_images')?></h5>
+            <div ng-show="isEmptyObject(files)" class="alert alert-info">
+                <?= Module::t('aws_gallery_empty')?>
             </div>
-            <div class="col s3" ng-repeat="file in files">
-                <div class="aws-gallery__image-wrapper">
-                    <div class="aws-gallery__image-index">{{ $index+1 }}</div>
-                    <img class="aws-gallery__image" ng-src="{{file.source}}" height="auto" width="100%" />
-                    <button class="aws-gallery__image-btn" type="button" ng-click="remove(file)"><i class="material-icons">delete</i></button>
-                </div>
+        <div class="row">
+            <div class="col-md-2" ng-repeat="file in files">
+                <button class="btn btn-outline-danger btn-sm" type="button" ng-click="remove(file)" style="position:absolute;"><i class="material-icons">delete</i></button>
+                <img class="img-fluid img-thumbnail" ng-src="{{file.source}}" /><br />
             </div>
         </div>
     </div>
