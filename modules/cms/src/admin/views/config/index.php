3c42e3b2ae27d430807ee13651cd849db9a248b0
Silvan <silvan@dev7.ch>
2017-08-29 10:29:40 +0200

    update btn docs (#1416)
    * update btn docs
    
    * btn corrections config/index

--- modules/cms/src/admin/views/config/index.php
+++ modules/cms/src/admin/views/config/index.php
@@ -11,6 +11,6 @@ use luya\cms\admin\Module;
                 <td><menu-dropdown class="menu-dropdown" nav-id="data.httpExceptionNavId" /></td>
             </tr>
         </table>
-        <a ng-click="save()" class="btn btn-symbol btn-icon btn-icon-save"></a>
+        <button type="button" ng-click="save()" class="btn btn-icon btn-save"><?= Module::t('btn_save'); ?></button>
     </div>
 </div>
