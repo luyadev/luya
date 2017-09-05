6e46f048dd6018defcc7254d2b68e51e87cf72d8
dev7ch <silvan@dev7.ch>
2017-09-05 16:02:55 +0200

    toolbar positioning

--- modules/cms/src/admin/views/page/update.php
+++ modules/cms/src/admin/views/page/update.php
@@ -35,15 +35,15 @@ use luya\cms\admin\Module;
                     </label>
                 </div>
                 <div class="toolbar-item toolbar-item-lang" ng-class="{'ml-auto':$first}" ng-repeat="lang in AdminLangService.data" ng-click="AdminLangService.toggleSelection(lang)" ng-show="AdminLangService.data.length > 1">
-                    <button class="toolbar-button toolbar-button-flag" ng-class="{'active' : AdminLangService.isInSelection(lang.short_code)}" >
+                    <button class="btn-toolbar flag-btn" ng-class="{'active' : AdminLangService.isInSelection(lang.short_code)}" >
                         <span class="flag flag-{{lang.short_code}}">
                             <span class="flag-fallback">{{lang.name}}</span>
                         </span>
                     </button>
                 </div>
                 <div class="toolbar-item" ng-class="{'ml-auto': AdminLangService.data.length <= 1}">
-                    <div class="btn-group" ng-class="{'show': toggleSettings}"  ng-click="toggleSettings=!toggleSettings">
-                        <button class="toolbar-button btn-sm">
+                    <div class="dropdown" ng-class="{'show': toggleSettings}"  ng-click="toggleSettings=!toggleSettings">
+                        <button class="btn btn-toolbar">
                             <i class="material-icons">more_vert</i>
                         </button>
                         <div class="dropdown-menu dropdown-menu-right" ng-class="{'show': toggleSettings}">
