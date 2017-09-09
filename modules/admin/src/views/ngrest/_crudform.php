d3b47b620adabbb048183b7da5d3c1466db3eeb3
Basil <git@nadar.io>
2017-09-09 14:50:42 +0200

    Remove short open tags #1469

--- modules/admin/src/views/ngrest/_crudform.php
+++ modules/admin/src/views/ngrest/_crudform.php
@@ -6,7 +6,7 @@ use luya\admin\Module;
 /* @var boolean $isInline */
 ?>
 <div class="tab-pane tab-padded" role="tabpanel" ng-if="crudSwitchType==<?= $type; ?>" ng-class="{'active' : crudSwitchType==<?= $type; ?>}" <?php if (!$isInline): ?>zaa-esc="closeUpdate()"<?php endif; ?>>
-    <form name="formCreate" class="js-form-side-by-side" ng-submit="<?php if ($type==2):?>submitUpdate()<?else:?>submitCreate()<?endif;?>">
+    <form name="formCreate" class="js-form-side-by-side" ng-submit="<?php if ($type==2):?>submitUpdate()<?php else: ?>submitCreate()<?php endif; ?>">
         <?php foreach ($this->context->forEachGroups($renderer) as $key => $group): ?>
             <?php foreach ($group['fields'] as $field => $fieldItem): ?>
                     <?php foreach ($this->context->createElements($fieldItem, $renderer) as $element): ?>
