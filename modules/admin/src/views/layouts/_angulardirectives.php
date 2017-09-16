98835018946135d5b8dd33588d6f1536438d252c
Silvan <silvan@dev7.ch>
2017-09-16 09:23:52 +0200

    adding vertical scrolling and corrections of table headers

--- modules/admin/src/views/layouts/_angulardirectives.php
+++ modules/admin/src/views/layouts/_angulardirectives.php
@@ -236,49 +236,32 @@ use luya\admin\Module as Admin;
                 <table class="table table-hover table-responsive table-striped table-align-middle mt-4">
                     <thead class="thead-default">
                         <tr>
-                            <th ng-hide="allowSelection == 'true'">
-                                <span class="filemanager-check-all" ng-click="toggleSelectionAll()">
+                            <th>
+                                <span ng-hide="allowSelection == 'true'" class="filemanager-check-all" ng-click="toggleSelectionAll()" tooltip tooltip-position="right" tooltip-text="{{ (filesData | filemanagerfilesfilter:currentFolderId:onlyImages:searchQuery | filter:searchQuery).length }} files">
                                     <i class="material-icons">done_all</i>
-                                    <span class="filemanager-files-counter">({{ (filesData | filemanagerfilesfilter:currentFolderId:onlyImages:searchQuery | filter:searchQuery).length }} files)</span>
                                 </span>
                             </th>
                             <th ng-if="selectedFileFromParent" style="width:15px;"></th>
                             <th></th><!-- image thumbnail / file icon -->
                             <th>
                                 <span><?= Admin::t('layout_filemanager_col_name'); ?></span>
-                                <div class="table-sorter table-sorter-up">
-                                    <i class="material-icons">keyboard_arrow_up</i>
-                                </div>
-                                <div class="table-sorter table-sorter-down">
-                                    <i class="material-icons">keyboard_arrow_down</i>
-                                </div>
+                                <!-- <div class="table-sorter-wrapper">
+                                    <div class="table-sorter table-sorter-up">
+                                        <i class="material-icons">keyboard_arrow_up</i>
+                                    </div>
+                                    <div class="table-sorter table-sorter-down">
+                                        <i class="material-icons">keyboard_arrow_down</i>
+                                    </div>
+                                </div> -->
                             </th>
                             <th>
                                 <span><?= Admin::t('layout_filemanager_col_type'); ?></span>
-                                <div class="table-sorter table-sorter-up">
-                                    <i class="material-icons">keyboard_arrow_up</i>
-                                </div>
-                                <div class="table-sorter table-sorter-down">
-                                    <i class="material-icons">keyboard_arrow_down</i>
-                                </div>
                             </th>
                             <th>
                                 <span><?= Admin::t('layout_filemanager_col_date'); ?></span>
-                                <div class="table-sorter table-sorter-up">
-                                    <i class="material-icons">keyboard_arrow_up</i>
-                                </div>
-                                <div class="table-sorter table-sorter-down">
-                                    <i class="material-icons">keyboard_arrow_down</i>
-                                </div>
                             </th>
                             <th>
                                 <span><?= Admin::t('layout_filemanager_col_size'); ?></span>
-                                <div class="table-sorter table-sorter-up">
-                                    <i class="material-icons">keyboard_arrow_up</i>
-                                </div>
-                                <div class="table-sorter table-sorter-down">
-                                    <i class="material-icons">keyboard_arrow_down</i>
-                                </div>
                             </th>
                             <th class="tab-padding-right text-right filemanager-actions-column"></th>
                         </tr>
@@ -293,16 +276,16 @@ use luya\admin\Module as Admin;
                                 <div class="filemanager-fakebox" ng-class="{'filemanager-fakebox-active': inSelection(file)}"></div>
                             </th>
 
-                            <td class="text-center" ng-click="openFileDetail(file)" tooltip tooltip-image-url="{{file.thumbnailMedium.source}}" tooltip-disabled="!file.isImage">
+                            <td class="text-center" ng-click="toggleSelection(file)" tooltip tooltip-image-url="{{file.thumbnailMedium.source}}" tooltip-disabled="!file.isImage">
                                 <span ng-if="file.isImage"><img class="responsive-img filmanager-thumb" ng-src="{{file.thumbnail.source}}" /></span>
                                 <span ng-if="!file.isImage"><i class="material-icons">attach_file</i></span>
                             </td>
-                            <td ng-click="openFileDetail(file)">{{file.name | truncateMiddle: 30}}</td>
-                            <td ng-click="openFileDetail(file)">{{file.extension}}</td>
+                            <td ng-click="toggleSelection(file)">{{file.name | truncateMiddle: 30}}</td>
+                            <td ng-click="toggleSelection(file)">{{file.extension}}</td>
                             <td ng-click="openFileDetail(file)">{{file.uploadTimestamp * 1000 | date:"short"}}</td>
                             <td ng-click="openFileDetail(file)">{{file.sizeReadable}}</td>
                             <td class="text-right">
-                                <button type="button" class="btn btn-sm btn-symbol" ng-click="openFileDetail(file)">
+                                <button type="button" class="btn btn-sm" ng-click="openFileDetail(file)">
                                     <i class="material-icons">zoom_in</i>
                                 </button>
                             </td>
