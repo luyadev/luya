79d3d2e2f138a61bd2359dd5dcdebb3b09476299
Basil Suter <git@nadar.io>
2017-10-03 15:05:32 +0200

    remove welcome text

--- modules/admin/src/views/default/dashboard.php
+++ modules/admin/src/views/default/dashboard.php
@@ -1,6 +1,4 @@
 <div class="luya-content">
-    <h1><?php echo \luya\admin\Module::t('dashboard_title'); ?></h1>
-    <p><?php echo \luya\admin\Module::t('dashboard_text'); ?></p>
     <div class="row">
     <?php foreach ($items as $dashboard): /* @var $dashboard \luya\admin\base\DashboardObjectInterface */ ?>
     <div class="col-md-4">
