5b39b0d6a61ff9c6875714708866f9ec0bf9e3a2
Basil Suter <git@nadar.io>
2017-10-04 17:34:04 +0200

    set default null in order to fix inserts when upgrading to newer version and use admin proxy command

--- modules/admin/src/apis/ProxyController.php
+++ modules/admin/src/apis/ProxyController.php
@@ -32,7 +32,7 @@ class ProxyController extends Controller
 	 * @var array A list of tables which will be ignored and can not be synced with the proxy command.
 	 */
     protected $ignoreTables = [
-        'migration', 'admin_proxy_build', 'admin_proxy_machine', 'admin_config', 'admin_user_login',
+        'migration', 'admin_proxy_build', 'admin_proxy_machine', 'admin_config',
     ];
     
     public function actionIndex($identifier, $token)
--- modules/admin/src/migrations/m170926_144137_add_admin_user_session_id_column.php
+++ modules/admin/src/migrations/m170926_144137_add_admin_user_session_id_column.php
@@ -6,7 +6,7 @@ class m170926_144137_add_admin_user_session_id_column extends Migration
 {
     public function safeUp()
     {
-        $this->addColumn('admin_user_login', 'session_id', $this->string()->notNull());
+        $this->addColumn('admin_user_login', 'session_id', $this->string()->notNull()->defaultValue(null));
     }
 
     public function safeDown()
