be6d34cb1eea8304a089bc6c417720ee66b77b27
Silvan <silvan@dev7.ch>
2017-11-23 22:19:07 +0100

    adjust dropdown icon sort relation, fix #1570

--- modules/admin/src/resources/js/directives.js
+++ modules/admin/src/resources/js/directives.js
@@ -835,9 +835,9 @@
                                             '<i class="material-icons">add_circle</i><span>{{ option.label }}</span>' +
                                         '</li>' +
                                     '</ul>' +
-                                    '<div class="float-right">' +
-                                        '<i ng-click="dropdownOpen=!dropdownOpen" class="material-icons" ng-show="dropdownOpen">arrow_drop_down</i>' +
-                                        '<i ng-click="dropdownOpen=!dropdownOpen" class="material-icons" ng-show="!dropdownOpen">arrow_drop_up</i>' +
+                                    '<div class="list-chevron">' +
+                                        '<i ng-click="dropdownOpen=!dropdownOpen" class="material-icons" ng-show="dropdownOpen">arrow_drop_up</i>' +
+                                        '<i ng-click="dropdownOpen=!dropdownOpen" class="material-icons" ng-show="!dropdownOpen">arrow_drop_down</i>' +
                                     '</div>' +
                                 '</div>' +
                             '</div>' +
