8cc0ce4a6a4b06008cecc5f1f4a041196f849a41
ilbassa <ilbassa@users.noreply.github.com>
2017-12-05 10:57:54 +0100

    New it tranlsation (#1704)
    * Completed IT translation of admin module
    
    * Completed IT translation of cms module (admin)
    
    * Completed IT translation of cms module (frontend)
    
    * Completed IT translation of remoteadmin module
    
    * Escaped some character

--- modules/admin/src/messages/it/admin.php
+++ modules/admin/src/messages/it/admin.php
@@ -259,9 +259,9 @@ return [
     'upload_err_message_6' => 'Manca una cartella temporanea.',
     'upload_err_message_7' => 'Impossibile salvare il file su disco.',
     'upload_err_message_8' => 'Un\'estensione PHP ha stoppato l\'upload del file.',
-    'model_config_atr_name' => 'Identifier',
-    'model_config_atr_name_hint' => 'Unique identifier for this variable. You can access system config with Config::get(identifier) in your application.',
-    'model_config_atr_value' => 'Value',
-    'model_config_atr_is_system' => 'Is system',
-    'menu_system_item_config' => 'Config variables'
+    'model_config_atr_name' => 'Identificativo',
+    'model_config_atr_name_hint' => 'Identificativo unico per questa variabile. Puoi accedere alle configurazioni di sistema con Config::get(identifier) nell\'applicazione.',
+    'model_config_atr_value' => 'Valore',
+    'model_config_atr_is_system' => 'Di sistema',
+    'menu_system_item_config' => 'Variabili di configurazione'
 ];
