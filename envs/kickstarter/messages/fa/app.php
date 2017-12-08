a9ef102deec167babc362b4bf1d9e76a9bdf3777
masihfathi <masihfathi@gmail.com>
2017-12-08 13:51:59 +0330

    add persian language translate to luya framework (#1719)
    * Add files via upload
    
    add Persian language translate
    
    * add Persian module cms translate
    
    * add cms admin module translate
    
    * add kickstarter Persian translate

---
+++ envs/kickstarter/messages/fa/app.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'kickstarter_success' => '<h1>You are done!</h1>
+    <p>Yes, you have successful installed <i>LUYA</i> on your System. You are now able to login into the administration area to create your pages.</p>
+    <p><strong>Important</strong><br />After loging into the administration area you have to navigation to &laquo;System&raquo; and enable all permissions for your group (Adminstrator).</p>',
+    'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Open administration area</a></p>',
+];
