b1a465d42bc861cbc0b66e8763139982a756d108
Basil Suter <git@nadar.io>
2017-12-12 11:19:57 +0100

    add missing languages

--- modules/admin/src/Module.php
+++ modules/admin/src/Module.php
@@ -52,7 +52,7 @@ final class Module extends \luya\admin\base\Module implements CoreModuleInterfac
     
     /**
      * @var string The default language for the admin interrace (former known as luyaLanguage).
-     * Currently supported: "en", "de", "fr", "es", "ru", "it", "ua", "el".
+     * Currently supported: en, de, ru, es, fr, ua, it, el, vi, pt, fa
      */
     public $interfaceLanguage = 'en';
     
