b69921fb59b98e9e9b78d7a535c7fee507a90797
Basil Suter <git@nadar.io>
2017-12-12 18:50:08 +0100

    subsplit scripts

--- scripts/subsplit.sh
+++ scripts/subsplit.sh
@@ -13,14 +13,4 @@ fi
 
 git subsplit publish "
     core:$BASE/luya-core.git
-    envs/kickstarter:$BASE/luya-kickstarter.git
-    modules/admin:$BASE/luya-module-admin.git
-    modules/cms:$BASE/luya-module-cms.git
-    modules/news:$BASE/luya-module-news.git
-    modules/errorapi:$BASE/luya-module-errorapi.git
-    modules/gallery:$BASE/luya-module-gallery.git
-    modules/crawler:$BASE/luya-module-crawler.git
-    modules/styleguide:$BASE/luya-module-styleguide.git
-    modules/frontendgroup:$BASE/luya-module-frontendgroup.git
-    modules/remoteadmin:$BASE/luya-module-remoteadmin.git
 " --heads=master -q
