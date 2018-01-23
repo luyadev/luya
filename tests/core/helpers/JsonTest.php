29f0dc4874e2ab296432f528c9a9dfa31727ca56
Basil Suter <git@nadar.io>
2018-01-23 08:45:40 +0100

    cs fix

--- tests/core/helpers/JsonTest.php
+++ tests/core/helpers/JsonTest.php
@@ -7,17 +7,17 @@ use luya\helpers\Json;
 
 class JsonTest extends LuyaWebTestCase
 {
-	public function testIsJson()
-	{
-		// not a json
-		$this->assertFalse(Json::isJson(['foo' => 'bar']));
-		$this->assertFalse(Json::isJson('12312312'));
-		$this->assertFalse(Json::isJson(12312312));
-		$this->assertFalse(Json::isJson('luya{"123":123}'));
-		// is a json
-		$this->assertTrue(Json::isJson('{"123":"456"}'));
-		$this->assertTrue(Json::isJson('{"123":456}'));
-		$this->assertTrue(Json::isJson('[{"123":"456"}]'));
-		$this->assertTrue(Json::isJson('[{"123":"456"}]'));
-	}
+    public function testIsJson()
+    {
+        // not a json
+        $this->assertFalse(Json::isJson(['foo' => 'bar']));
+        $this->assertFalse(Json::isJson('12312312'));
+        $this->assertFalse(Json::isJson(12312312));
+        $this->assertFalse(Json::isJson('luya{"123":123}'));
+        // is a json
+        $this->assertTrue(Json::isJson('{"123":"456"}'));
+        $this->assertTrue(Json::isJson('{"123":456}'));
+        $this->assertTrue(Json::isJson('[{"123":"456"}]'));
+        $this->assertTrue(Json::isJson('[{"123":"456"}]'));
+    }
 }
