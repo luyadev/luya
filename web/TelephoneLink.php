8b8e1aec1fb03458f24b318e6e0e5dbe01224e64
boehsermoe <boehsermoe@me.com>
2018-05-30 22:33:31 +0200

    TelephoneLink support

---
+++ web/TelephoneLink.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace luya\web;
+
+use luya\cms\models\NavItemPageBlockItem;
+use luya\tag\tags\TelTag;
+use yii\base\BaseObject;
+use yii\validators\RegularExpressionValidator;
+
+/**
+ * Telephone Link.
+ *
+ * Represent a {{luya\web\LinkInterface}} of an telephone link.
+ *
+ * @author Bennet Klarhölter <boehsermoe@me.com>
+ * @since 1.0.9
+ */
+class TelephoneLink extends BaseObject implements LinkInterface
+{
+    use LinkTrait;
+
+    private $_telephone;
+
+    /**
+     * Setter method for telephone number.
+     *
+     * If no valid telephone is provided, not value is set.
+     *
+     * @param string $telephone The telephone number which should be used for the tel link.
+     */
+    public function setTelephone($telephone)
+    {
+        /**
+         * Hack to support leading + sign
+         * @see ArrayHelper::typeCast in NavItemPageBlockItem::rules()
+         */
+        $telephone = ltrim($telephone, '\\');
+
+        $validator = new RegularExpressionValidator([
+            'pattern' => '#^(?:0|\+[0-9]{2})[\d- ]+$#'
+        ]);
+        if ($validator->validate($telephone, $error)) {
+            $this->_telephone = $telephone;
+        }
+    }
+
+    /**
+     * Getter method for the telephone.
+     *
+     * @return string Returns the telephone from the setter method, if telephone is not valid null is returned.
+     */
+    public function getTelephone()
+    {
+        return $this->_telephone;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getHref()
+    {
+        return empty($this->getTelephone()) ?: 'tel:' . $this->getTelephone();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTarget()
+    {
+        return '_self';
+    }
+}
