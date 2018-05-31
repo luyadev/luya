46c6c1932214e1d05e78680f72cf6d217f487c35
boehsermoe <boehsermoe@me.com>
2018-05-31 15:33:13 +0200

    TelephoneLink support for LinkButtonBlock (#1815)
    * TelephoneLink support
    * Changelog and refactoring #1815
    * TelephoneLink support for LinkButtonBlock closes #1815
    * Telephonelink remove non-digits from href
    * UnitTest for Telephonelink

---
+++ core/web/TelephoneLink.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace luya\web;
+
+use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
+use yii\validators\RegularExpressionValidator;
+
+/**
+ * Telephone Link.
+ *
+ * Represent a {{luya\web\LinkInterface}} of an telephone link.
+ *
+ * @property $telephone
+ *
+ * @author Bennet Klarhölter <boehsermoe@me.com>
+ * @since 1.0.9
+ */
+class TelephoneLink extends BaseObject implements LinkInterface
+{
+    use LinkTrait;
+
+    private $_telephone;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if ($this->telephone === null) {
+            throw new InvalidConfigException('The telephone attribute can not be empty and must be set trough configuration array.');
+        }
+    }
+
+    /**
+     * Setter method for telephone number.
+     *
+     * If no valid telephone is provided, not value is set.
+     *
+     * @param string $telephone The telephone number which should be used for the tel link.
+     */
+    public function setTelephone($telephone)
+    {
+        /**
+         * Hack to support leading + sign
+         * @see \luya\cms\models\NavItemPageBlockItem::rules()
+         * @link https://github.com/luyadev/luya/pull/1815
+         */
+        $telephone = ltrim($telephone, '\\');
+
+        $validator = new RegularExpressionValidator([
+            'pattern' => '#^(?:0|\+[0-9]{2})[\d- ()]+$#'
+        ]);
+        if ($validator->validate($telephone, $error)) {
+            $this->_telephone = $telephone;
+        }
+    }
+
+    /**
+     * Getter method for the telephone.
+     *
+     * @return string Returns the telephone from the setter method, if telephone is not valid null is returned.
+     */
+    public function getTelephone()
+    {
+        return $this->_telephone;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getHref()
+    {
+        $href = null;
+
+        if (!empty($this->getTelephone())) {
+            // Remove all chars expect digits and "+"
+            $number = preg_replace('#[^\d+]#', '', $this->getTelephone());
+            $href = 'tel:' . $number;
+        }
+
+        return $href;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTarget()
+    {
+        return '_self';
+    }
+}
---
+++ tests/core/web/TelephoneLinkTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace luyatests\core\web;
+
+use luya\web\TelephoneLink;
+use luyatests\LuyaWebTestCase;
+
+class TelephoneLinkTest extends LuyaWebTestCase
+{
+    public function testLinkable()
+    {
+        $link = new TelephoneLink(['telephone' => '+000 (9) 123-456']);
+
+        $this->assertInstanceOf('luya\web\LinkInterface', $link);
+
+        $this->assertSame('_self', $link->getTarget());
+        $this->assertSame('+000 (9) 123-456', $link->getTelephone());
+        $this->assertSame('tel:+0009123456', $link->getHref());
+        $this->assertSame('tel:+0009123456', $link->__toString());
+    }
+
+    public function testMissingHrefException()
+    {
+        $this->expectException('yii\base\InvalidConfigException');
+        $link = new TelephoneLink();
+    }
+
+    public function testPrefixWithBackslash()
+    {
+        $link = new TelephoneLink(['telephone' => '\+49 123456']);
+
+        $this->assertContains('+49 123456', $link->getTelephone());
+        $this->assertContains('tel:+49123456', $link->getHref());
+    }
+}
