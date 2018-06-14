ffc984c18433e71a1cec2c9d5c79a9ac2da3c6d9
Iskender TOTOĞLU <iskender@altivebir.com>
2018-06-14 23:15:57 +0300

    Turkish messages (#1823)

---
+++ core/messages/tr/luya.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{attribute}, minimum {length} karakter uzunluğunda olmalıdır.',
+    '{attribute} must include at least one special character.' => '{attribute} en az bir özel karakter içermelidir.',
+    '{attribute} must include at least one digit.' => '{attribute} en az bir rakam içermelidir.',
+    '{attribute} must include at least one letter.' => '{attribute} en az bir harf içermelidir.',
+    '{attribute} must include at least one uppercase letter.' => '{attribute}, en az bir büyük harf içermelidir.',
+    '{attribute} must include at least one lowercase letter.' => '{attribute}, en az bir küçük harf içermelidir.',
+    '{attribute} must be a float or numeric value.' => '{attribute}, bir float veya sayısal değer olmalıdır.'
+];
