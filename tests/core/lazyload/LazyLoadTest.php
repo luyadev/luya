3c0cab57d4d72029ff505372b03a6939b1ad3211
Basil Suter <git@nadar.io>
2018-07-06 10:00:52 +0200

    Lazyload widget asset registration issue fixed when used in nested context. closes #1830

--- tests/core/lazyload/LazyLoadTest.php
+++ tests/core/lazyload/LazyLoadTest.php
@@ -9,7 +9,6 @@ class LazyLoadTest extends LuyaWebTestCase
 {
     public function testWidget()
     {
-        LazyLoad::$counter=2;
         $this->assertSame('<img class="lazy-image add" data-src="abc.jpg"><noscript><img class="lazy-image add" src="abc.jpg" /></noscript>', LazyLoad::widget(['src' => 'abc.jpg', 'extraClass' => 'add']));
     }
 }
