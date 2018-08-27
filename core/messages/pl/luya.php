dd33b6869db0a79d0e14da3bd8e9fadfa63afe10
vave <fiolekcs@o2.pl>
2018-08-27 16:53:55 +0200

    Polish translation (#1841)

---
+++ core/messages/pl/luya.php
@@ -0,0 +1,10 @@
+<?php
+ return [
+    '{attribute} must have a minimum length of {length} characters.' => '{attribute} musi mieć co najmniej {length} znaków.',
+    '{attribute} must include at least one special character.' => '{attribute} musi zawierać przynajmniej jeden znak specjalny.',
+    '{attribute} must include at least one digit.' => '{attribute} musi zawierać co najmniej jedną cyfrę.',
+    '{attribute} must include at least one letter.' => '{attribute} musi zawierać co najmniej jedną literę.',
+    '{attribute} must include at least one uppercase letter.' => '{attribute} musi zawierać co najmniej jedną wielką literę.',
+    '{attribute} must include at least one lowercase letter.' => '{attribute} musi zawierać co najmniej jedną małą literę.',
+    '{attribute} must be a float or numeric value.' => '{attribute} musi być wartością zmiennoprzecinkową lub liczbową.'
+];
