a4d60f6b1f89927d7d0d1630e1786f77794d183a
boehsermoe <boehsermoe@me.com>
2018-09-01 20:54:41 +0200

    Viewpath refactoring

--- tests/core/web/ControllerTest.php
+++ tests/core/web/ControllerTest.php
@@ -10,7 +10,6 @@ class StubController extends \luya\web\Controller
 
 class ControllerTest extends \luyatests\LuyaWebTestCase
 {
-    /** @var StubController */
     public $controller;
 
     public function setUp()
