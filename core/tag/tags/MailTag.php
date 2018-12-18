64fbfdb26e7cb970f0c21b92aa95e8ab4eb83f63
Basil Suter <git@nadar.io>
2018-12-18 11:32:35 +0100

    add email tag obfuscation #1895

--- core/tag/tags/MailTag.php
+++ core/tag/tags/MailTag.php
@@ -49,6 +49,38 @@ EOT;
      */
     public function parse($value, $sub)
     {
-        return Html::mailto((!empty($sub)) ? $sub : $value, $value);
+        return Html::tag('a', Html::encode($sub) ?: $this->obfuscate($value), [
+            'rel' => 'nofollow',
+            'href' => $this->obfuscate('mailto:'.$value),
+            'encoding' => false,
+        ]);
+    }
+
+    /**
+     * Obfucscate email adresse
+     *
+     * @param string $email
+     * @return string
+     * @see https://stackoverflow.com/a/12592364/4611030
+     */
+    public function obfuscate($email)
+    {
+        $alwaysEncode = ['.', ':', '@'];
+        $result = null;
+        // Encode string using oct and hex character codes
+        for ($i = 0; $i < strlen($email); $i++) {
+            // Encode 25% of characters including several that always should be encoded
+            if (in_array($email[$i], $alwaysEncode) || mt_rand(1, 100) < 25) {
+                if (mt_rand(0, 1)) {
+                    $result .= '&#' . ord($email[$i]) . ';';
+                } else {
+                    $result .= '&#x' . dechex(ord($email[$i])) . ';';
+                }
+            } else {
+                $result .= $email[$i];
+            }
+        }
+    
+        return $result;
     }
 }
