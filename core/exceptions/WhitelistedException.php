865addba4f088971838cceda0da0e2a6e3d40475
Basil Suter <git@nadar.io>
2019-09-09 12:56:23 +0200

    add php doc

--- core/exceptions/WhitelistedException.php
+++ core/exceptions/WhitelistedException.php
@@ -4,7 +4,14 @@ namespace luya\exceptions;
 
 use luya\Exception;
 
+/**
+ * An exception which is Whitelisted and therefore expect.
+ * 
+ * > Whitelisted Exception won't be transmitted to the LUYA error api.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.21
+ */
 class WhitelistedException extends Exception
 {
-    
 }
