64f329af024fb6b0c11b534d0abbc243ff37bf50
boehsermoe <boehsermoe@me.com>
2019-09-21 09:01:52 +0200

    Theme layout fix

--- core/base/Module.php
+++ core/base/Module.php
@@ -189,10 +189,6 @@ abstract class Module extends \yii\base\Module
                 throw new InvalidConfigException(sprintf('The required component "%s" is not registered in the configuration file', $component));
             }
         }
-        
-        if (Yii::$app->themeManager->hasActiveTheme) {
-            $this->layout = Yii::$app->themeManager->activeTheme->layout;
-        }
 
         static::onLoad();
     }
@@ -211,6 +207,9 @@ abstract class Module extends \yii\base\Module
      */
     public function luyaBootstrap(Application $app)
     {
+        if (Yii::$app->themeManager->hasActiveTheme) {
+            $this->layout = Yii::$app->themeManager->activeTheme->layout;
+        }
     }
 
     /**
