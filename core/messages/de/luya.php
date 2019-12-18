b8b69d8e39bdb6423ec5c9c8cf83239eafc5b604
Basil Suter <git@nadar.io>
2019-12-18 15:34:44 +0100

    add german translations

--- core/messages/de/luya.php
+++ core/messages/de/luya.php
@@ -8,7 +8,7 @@ return [
     '{attribute} must include at least one uppercase letter.' => '{attribute} muss mindestens einen Grossbuchstaben enthalten.',
     '{attribute} must include at least one lowercase letter.' => '{attribute} muss mindestens einen Kleinbuchstaben enthalten.',
     '{attribute} must be a float or numeric value.' => '{attribute} muss ein Numerischer Wert sein.',
-    'Invalid phone number.' => '',
-    'The phone number does not match the required type {name}' => '',
-    'Invalid phone number, ensure it starts with the correct country code.' => '',
+    'Invalid phone number.' => 'Ungültige Telefonnummer.',
+    'The phone number does not match the required type {name}.' => 'Die Telefonnummer entspricht nicht dem erwarteten Typen {name}.',
+    'Invalid phone number, ensure it starts with the correct country code.' => 'Ungültige Telefonnummer; stellen Sie sicher, dass sie mit dem korrekten Ländercode beginnt.',
 ];
