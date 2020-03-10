fb41a3fd4f0ed6e0f237eec97cb0fe2e006137d3
Basil <git@nadar.io>
2020-03-10 06:41:47 +0100

    fix php tests

--- core/texttospeech/TextToSpeechWidget.php
+++ core/texttospeech/TextToSpeechWidget.php
@@ -132,7 +132,7 @@ class TextToSpeechWidget extends Widget
 
         $config = Json::htmlEncode([
             'text' => $this->text ? $this->text : '', // must be an empty string as it will checked with .length
-            'language' => Yii::$app->composition->langShortCode,
+            'language' => Yii::$app->language,
         ]);
 
         $this->view->registerJs("
