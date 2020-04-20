4a0fdfe7f61c50da7091018b0cd760656911579b
Basil <git@nadar.io>
2020-04-20 12:15:21 +0200

    add offers option (#2015)
    * add offers option
    
    * add changelog
    
    * add offer test
    
    * add exception test

--- core/web/jsonld/OfferTrait.php
+++ core/web/jsonld/OfferTrait.php
@@ -12,4 +12,58 @@ namespace luya\web\jsonld;
 trait OfferTrait
 {
     use ThingTrait;
+
+    private $_availability;
+
+    private $_price;
+
+    /**
+     * Price Setter
+     *
+     * @param PriceValue $price
+     * @return static
+     * @since 1.2.2
+     */
+    public function setPrice(PriceValue $price)
+    {
+        $this->_price = $price->getValue();
+        return $this;
+    }
+
+    /**
+     * Get Price
+     *
+     * @return mixed
+     * @since 1.2.2
+     */
+    public function getPrice()
+    {
+        return $this->_price;
+    }
+
+    private $_priceCurrency;
+
+    /**
+     * Price Currency Setter.
+     *
+     * @param CurrencyValue $currencyValue
+     * @return static
+     * @since 1.2.2
+     */
+    public function setPriceCurrency(CurrencyValue $currencyValue)
+    {
+        $this->_priceCurrency = $currencyValue->getValue();
+        return $this;
+    }
+
+    /**
+     * Get Price Currency
+     *
+     * @return string
+     * @since 1.2.2
+     */
+    public function getPriceCurrency()
+    {
+        return $this->_priceCurrency;
+    }
 }
