302a4f5884819dbe2cad8a1bdf902917c846b366
Basil Suter <git@nadar.io>
2020-06-08 11:49:50 +0200

    string template helper

--- tests/core/helpers/StringHelperTest.php
+++ tests/core/helpers/StringHelperTest.php
@@ -222,4 +222,13 @@ EOT;
         $this->assertFalse(StringHelper::filterMatch('hello', '!hello'));
         $this->assertFalse(StringHelper::filterMatch('hello', '!hello', 'hello'));
     }
+
+    public function testTemplate()
+    {
+        $this->assertSame('<p>bar</p>', StringHelper::template('<p>{{foo}}</p>', ['foo' => 'bar']));
+        $this->assertSame('<p>bar</p>', StringHelper::template('<p>{{ foo }}</p>', ['foo' => 'bar']));
+
+        $this->assertSame('<p>bar {{unknown}}</p>', StringHelper::template('<p>{{ foo }} {{unknown}}</p>', ['foo' => 'bar']));
+        $this->assertSame('<p>bar {{unknown}}</p>', StringHelper::template('<p>{{ foo }} {{unknown}}</p>', ['foo' => 'bar', 'xyz' => 'abc']));
+    }
 }
