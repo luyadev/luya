6b1039dc1f0890b369613b9728c4a1b53ab15b61
NidgetGod <nidgetgod@hotmail.com>
2020-10-25 20:48:51 +0800

    Add Traditional Chinese of Taiwan translation

---
+++ core/messages/zh-TW/luya.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{attribute} 必須至少有 {length} 個字元。',
+    '{attribute} must include at least one special character.' => '{attribute} 必須至少包含一個特殊字元。',
+    '{attribute} must include at least one digit.' => '{attribute} 必須至少包含一個數字。',
+    '{attribute} must include at least one letter.' => '{attribute} 必須至少包含一個字母。',
+    '{attribute} must include at least one uppercase letter.' => '{attribute} 必須至少包含一個大寫字母。',
+    '{attribute} must include at least one lowercase letter.' => '{attribute} 必須至少包含一個小寫字母。',
+    '{attribute} must be a float or numeric value.' => '{attribute} 必須是浮點數或數字。',
+    'Invalid phone number.' => '無效的電話號碼。',
+    'The phone number does not match the required type {name}.' => '電話號碼與要求的 {name} 類型不符。',
+    'Invalid phone number, ensure it starts with the correct country code.' => '無效的電話號碼，請確保以正確的國家地區代碼開頭。',
+];
