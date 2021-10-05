dda7bd36a29df4f03607c28e3aa28f760ed08a98
Wermeille Bastien <bastien.wermeille@gmail.com>
2021-10-05 08:00:00 +0200

    Add french translation

---
+++ core/messages/fr/luya.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{attribute} doit avoir une longueur minimale de {length} charactères.',
+    '{attribute} must include at least one special character.' => '{attribute} doit comporter au moins un charactère spécial.',
+    '{attribute} must include at least one digit.' => '{attribute} doit comporter au moins un chiffre.',
+    '{attribute} must include at least one letter.' => '{attribute} doit comporter au moins une lettre.',
+    '{attribute} must include at least one uppercase letter.' => '{attribute} doit comporter au moins une lettre majuscule.',
+    '{attribute} must include at least one lowercase letter.' => '{attribute} doit comporter au moins une lettre minuscule.',
+    '{attribute} must be a float or numeric value.' => '{attribute} doit être un nombre à virgule ou une valeur numérique.',
+    'Invalid phone number.' => 'Numéro de téléphone non valide.',
+    'The phone number does not match the required type {name}.' => 'Le numéro de téléphone ne correspond pas au type requis {name}.',
+    'Invalid phone number, ensure it starts with the correct country code.' => 'Numéro de téléphone non valide, assurez-vous qu\'il commence par le bon code de pays.',
+];
