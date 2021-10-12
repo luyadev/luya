8b8a81228dd7f1627902299492016c06091cfb76
Szántó Zoltán <mrbig00@gmail.com>
2021-10-12 09:41:26 +0300

    Add romanian translation (#2108)

---
+++ core/messages/ro/luya.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{atribut} trebuie să aibă o lungime minimă de {lungime} caractere.',
+    '{attribute} must include at least one special character.' => '{atribut} trebuie să includă cel puțin un caracter special.',
+    '{attribute} must include at least one digit.' => '{atribut} trebuie să includă cel puțin o cifră.',
+    '{attribute} must include at least one letter.' => '{atribut} trebuie să includă cel puțin o literă.',
+    '{attribute} must include at least one uppercase letter.' => '{atribut} trebuie să includă cel puțin o literă mare.',
+    '{attribute} must include at least one lowercase letter.' => '{atribut} trebuie să includă cel puțin o literă mică.',
+    '{attribute} must be a float or numeric value.' => '{atribut} trebuie să fie o valoare numerică sau float.',
+    'Invalid phone number.' => 'Numar de telefon invalid.',
+    'The phone number does not match the required type {name}.' => 'Numărul de telefon nu se potrivește cu tipul necesar {name}.',
+    'Invalid phone number, ensure it starts with the correct country code.' => 'Număr de telefon nevalid, asigurați-vă că începe cu codul de țară corect.',
+];
