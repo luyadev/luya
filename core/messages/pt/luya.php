b8c99f081f3756964d9798866afa96ba944b4ab6
Andre Gomes <andre@appdev.com.br>
2021-10-19 12:11:27 -0300

    Added portuguese translation

---
+++ core/messages/pt/luya.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{attribute} deve ter o mínimo de {length} caracteres.',
+    '{attribute} must include at least one special character.' => '{attribute} deve incluir no mínimo um caracter especial.',
+    '{attribute} must include at least one digit.' => '{attribute} deve incluir no mínimo um dígito.',
+    '{attribute} must include at least one letter.' => '{attribute} deve incluir no mínimo uma letra.',
+    '{attribute} must include at least one uppercase letter.' => '{attribute} deve incluir no mínimo uma letra maiúscula.',
+    '{attribute} must include at least one lowercase letter.' => '{attribute} deve incluir no mínimo uma letra minúscula.',
+    '{attribute} must be a float or numeric value.' => '{attribute} deve ser um valor númerico (com ou sem núneros após vírgula).',
+    'Invalid phone number.' => 'Número de telefone inválido',
+    'The phone number does not match the required type {name}.' => 'O número de telefone não corresponde com tipo {name} requerido.',
+    'Invalid phone number, ensure it starts with the correct country code.' => 'Número de telefone inválido, assegure-se que ele inicia com o código correto do país.',
+];
