ea30dd209824a9509024d66b1ceb06a1c2e5a24a
testt23 <rekstomi@abv.bg>
2021-12-01 10:11:59 +0000

    Create luya.php (#2112)

---
+++ core/messages/bg/luya.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '{attribute} must have a minimum length of {length} characters.' => '{атрибутът} трябва да има поне {length} знака.',
+    '{attribute} must include at least one special character.' => '{attribute} трябва да съдържа поне един специален знак. ',
+    '{attribute} must include at least one digit.' => '{attribute} трябва да съдържа поне едно число.',
+    '{attribute} must include at least one letter.' => '{attribute} трябва да съдържа поне една буква.',
+    '{attribute} must include at least one uppercase letter.' => '{attribute} трябва да съдържа поне една главна буква.',
+    '{attribute} must include at least one lowercase letter.' => '{attribute} трябва да съдържа поне една малка буква.',
+    '{attribute} must be a float or numeric value.' => '{attribute} трябва да е числова стойност.',
+    'Invalid phone number.' => 'Невалиден телефонен номер.',
+    'The phone number does not match the required type {name}.' => 'Телефонният номер не съответства на очаквания тип {name}.',
+    'Invalid phone number, ensure it starts with the correct country code.' => 'Невалиден телефонен номер; уверете се, че започва с правилния код на държавата.',
+];
