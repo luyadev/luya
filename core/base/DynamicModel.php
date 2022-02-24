52b21275e471dfe6673624b0781903ed65c08d7b
Basil Suter <git@nadar.io>
2022-02-24 14:58:53 +0000

    remove attribute labels

--- core/base/DynamicModel.php
+++ core/base/DynamicModel.php
@@ -11,8 +11,7 @@ use Yii;
  *
  * ```php
  * $model = new DynamicModel(['query']);
- * $model->attributeLabels = ['query' => 'Search term'];
- * $model->attributeHints = ['query' => 'Enter a search term in order to find articles.'];
+ * $model->setAttributeHints(['query' => 'Enter a search term in order to find articles.']);
  * $model->addRule(['query'], 'string');
  * $model->addRule(['query'], 'required');
  * ```
@@ -26,24 +25,30 @@ class DynamicModel extends \yii\base\DynamicModel
      * @var array An array with key value pairing where key is the attribute and value the label.
      * @since 1.0.15
      */
-    public $attributeHints = [];
+    public $_attributeHints = [];
 
     /**
-     * @var array Assignable attributes by array where key is the label key value the label for the key.
+     * Sets the attribute hints in a massive way.
+     *
+     * @param array $hints Array of attribute hints
+     * @return $this
+     * @since 2.0.3
      */
-    public $attributeLabels = [];
-    
-    /**
-     * {@inheritDoc}
-     */
-    public function attributeLabels()
+    public function setAttributeHints(array $hints)
     {
-        $labels = [];
-        foreach ($this->attributeLabels as $key => $value) {
-            $labels[$key] = Yii::t('app', $value);
-        }
-        
-        return $labels;
+        $this->_attributeHints = $hints;
+        return $this;
+    }
+
+    /**
+     * Get all hints for backwards compatibility.
+     * 
+     * @return array
+     * @since 2.0.3
+     */
+    public function getAttributeHints()
+    {
+        return $this->_attributeHints;
     }
 
     /**
